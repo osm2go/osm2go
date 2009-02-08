a982e6c5184d5573c063548cec5397ef56097b40
achadwick <achadwick@33a88ea1-378e-42d5-b688-df424f1c5956>
2009-02-08 20:38:33 +0000

    Trunk portability: allow building without a debian/
    git-svn-id: https://vcs.maemo.org/svn/osm2go/trunk@58 33a88ea1-378e-42d5-b688-df424f1c5956

---
+++ version.mk
@@ -0,0 +1,15 @@
+
+# Set the version number pertaining to the trunk here.
+
+TRUNK_VERSION=0.6.12
+
+# Packagers can append or prepend whatever magic strings through 
+# environment vars.
+
+VERSION=$${VERSION_PREFIX}$(TRUNK_VERSION)$${VERSION_SUFFIX}
+
+version.h: FORCE
+	grep -q "\"$(VERSION)\"" $@ || echo "#define VERSION \"$(VERSION)\"" >$@
+
+FORCE:
+
