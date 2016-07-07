37668f3886916b92716391cce585abac8ad82248
Rolf Eike Beer <eike@sf-mail.de>
2016-07-08 00:19:02 +0200

    make g_thread_create() error checking more readable

--- src/net_io.c
+++ src/net_io.c
@@ -302,7 +302,7 @@ static gboolean net_io_do(GtkWidget *parent, net_io_request_t *request) {
 
   /* create worker thread */
   request->refcount = 2;   // master and worker hold a reference
-  if(!g_thread_create(&worker_thread, request, FALSE, NULL) != 0) {
+  if(g_thread_create(&worker_thread, request, FALSE, NULL) == NULL) {
     g_warning("failed to create the worker thread");
 
     /* free request and return error */
