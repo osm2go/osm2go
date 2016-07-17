6495b32ab656b642a04f7e86515e86ab34646fd4
Rolf Eike Beer <eike@sf-mail.de>
2016-07-18 00:13:51 +0200

    split liblocation code out of gps.c
    The whole file is just a single #ifdef, and both halves share only very few
    includes. Separate the liblocation code out into it's own file and only
    include the one that is needed.

--- src/gps.c
+++ src/gps.c
@@ -20,130 +20,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
-#include "appdata.h"
-
-#ifdef ENABLE_LIBLOCATION
-
-#include <location/location-gps-device.h>
-
-gboolean gps_get_pos(appdata_t *appdata, pos_t *pos, float *alt) {
-  if(!appdata->settings || !appdata->settings->enable_gps)
-    return FALSE;
-
-  gps_state_t *gps_state = appdata->gps_state;
-
-  if(!gps_state->fix)
-    return FALSE;
-
-  if(pos) {
-    pos->lat = gps_state->latitude;
-    pos->lon = gps_state->longitude;
-  }
-
-  if(alt)
-    *alt = gps_state->altitude;
-
-  return TRUE;
-}
-
-static void
-location_changed(LocationGPSDevice *device, gps_state_t *gps_state) {
-
-  gps_state->fix =
-    (device->fix->fields & LOCATION_GPS_DEVICE_LATLONG_SET);
-
-  if(gps_state->fix) {
-    gps_state->latitude = device->fix->latitude;
-    gps_state->longitude = device->fix->longitude;
-  }
-
-  if(device->fix->fields & LOCATION_GPS_DEVICE_ALTITUDE_SET)
-    gps_state->altitude = device->fix->altitude;
-  else
-    gps_state->altitude = NAN;
-
-  if(gps_state->cb)
-    if(!gps_state->cb(gps_state->data))
-      gps_state->cb = NULL;
-}
-
-void gps_init(appdata_t *appdata) {
-  gps_state_t *gps_state = appdata->gps_state = g_new0(gps_state_t, 1);
-
-  printf("GPS init: Using liblocation\n");
-
-  gps_state->device = g_object_new(LOCATION_TYPE_GPS_DEVICE, NULL);
-  if(!gps_state->device) {
-    printf("Unable to connect to liblocation\n");
-    return;
-  }
-
-  gps_state->idd_changed =
-    g_signal_connect(gps_state->device, "changed",
-		     G_CALLBACK(location_changed), gps_state);
-
-#ifdef LL_CONTROL_GPSD
-  gps_state->control = location_gpsd_control_get_default();
-#endif
-}
-
-void gps_release(appdata_t *appdata) {
-  gps_state_t *gps_state = appdata->gps_state;
-
-  if(!gps_state->device) return;
-
-#ifdef LL_CONTROL_GPSD
-  if(gps_state->control
-#if MAEMO_VERSION_MAJOR < 5
-     && gps_state->control->can_control
-#endif
-     ) {
-    printf("Having control over GPSD and its running, stopping it\n");
-    if(appdata->gps_state->gps_is_on)
-      location_gpsd_control_stop(gps_state->control);
-  }
-#endif
-
-  /* Disconnect signal */
-  g_signal_handler_disconnect(gps_state->device, gps_state->idd_changed);
-
-  g_free(appdata->gps_state);
-  appdata->gps_state = NULL;
-}
-
-void gps_enable(appdata_t *appdata, gboolean enable) {
-  if(appdata->settings) {
-    gps_state_t *gps_state = appdata->gps_state;
-
-    if(enable != appdata->gps_state->gps_is_on) {
-      if(gps_state->device && gps_state->control
-#if MAEMO_VERSION_MAJOR < 5
-	 && gps_state->control->can_control
-#endif
-	 ) {
-	if(enable) {
-	  printf("starting gpsd\n");
-	  location_gpsd_control_start(gps_state->control);
-	} else {
-	  printf("stopping gpsd\n");
-	  location_gpsd_control_stop(gps_state->control);
-	}
-	appdata->gps_state->gps_is_on = enable;
-      }
-    }
-
-    appdata->settings->enable_gps = enable;
-  }
-}
-
-#else  // ENABLE_LIBLOCATION
-
 #ifdef ENABLE_GPSBT
 #include <gpsbt.h>
 #include <gpsmgr.h>
 #include <errno.h>
 #endif
+
+#include "appdata.h"
 
 /* maybe user configurable later on ... */
 #define GPSD_HOST "127.0.0.1"
@@ -394,5 +277,3 @@ void gps_release(appdata_t *appdata) {
   g_free(appdata->gps_state);
   appdata->gps_state = NULL;
 }
-
-#endif // ENABLE_LIBLOCATION
