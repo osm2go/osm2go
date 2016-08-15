b193e9eea2ae7d6110e977ceeb0bc6b6da6368ae
Rolf Eike Beer <eike@sf-mail.de>
2016-08-16 00:28:30 +0200

    simplify mktarball.sh

--- mktarball.sh
+++ mktarball.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
 VERSION="$1"
-if test "x$VERSION" = "x"; then
-    echo "usage: $0 VERSION"
-    exit 1
+if [ -z "${VERSION}" ]; then
+	VERSION=$(git describe --tags)
 fi
 
-workdir=`pwd`
-stem="osm2go-$VERSION"
-tarball="$stem.tar.gz"
-tmpdir="/tmp/mktarball-$USER-$$"
-
-echo "creating ../$tarball ..."
-mkdir -p "$tmpdir"
-ln -s "$workdir" "$tmpdir/$stem"
-(cd "$tmpdir" && tar -c --exclude=.svn --dereference -pzf "$workdir/../$tarball" "$stem/")
-rm -fr "$tmpdir"
-
-
-
+git archive --format=tar -o osm2go-${VERSION}.tar --prefix=osm2go-${VERSION}/ HEAD
+gzip --best osm2go-${VERSION}.tar
