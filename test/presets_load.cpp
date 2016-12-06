aec9c5766839e9e7a3cb3d465dfefc33f5ab57c0
Rolf Eike Beer <eike@sf-mail.de>
2016-12-06 22:38:16 +0100

    add loads of new icons from JOSM
    Also extend the presets_load testcase to check for missing icons. Do not fail on
    missing icons yet as they are far from complete.

--- test/presets_load.cpp
+++ test/presets_load.cpp
@@ -1,15 +1,80 @@
 #include <josm_presets.h>
 #include <josm_presets_p.h>
 
-#include <errno.h>
+#include <algorithm>
+#include <cstdlib>
+#include <cerrno>
 #include <iostream>
-#include <stdlib.h>
+#include <set>
+#include <string>
 
-int main(void)
+static std::vector<std::string> basedirs;
+
+struct check_icon {
+  const std::string &filename;
+  check_icon(const std::string &fn) : filename(fn) {}
+  bool operator()(const std::string &dir);
+};
+
+static std::set<std::string> missingIcons;
+
+bool check_icon::operator()(const std::string &dir)
+{
+  const char *icon_exts[] = { ".svg", ".gif", ".png", ".jpg", NULL };
+  gint idx;
+  std::string path = dir + "/icons/" + filename;
+
+  if(g_file_test(path.c_str(), G_FILE_TEST_IS_REGULAR) == TRUE)
+    return true;
+
+  for (idx = 0; icon_exts[idx]; idx++) {
+    std::string name = path + icon_exts[idx];
+
+    if(g_file_test(name.c_str(), G_FILE_TEST_IS_REGULAR) == TRUE)
+      return true;
+  }
+  return false;
+}
+
+static void err(const std::string &filename)
+{
+  std::cerr << filename << std::endl;
+}
+
+/**
+ * @brief check icons of the given item
+ * @param item the item to check
+ * @returns if a missing icon was detected
+ * @retval false all icons below this nodes were found (if any)
+ */
+static void checkItem(const presets_item_t *item)
+{
+  const presets_item_visible * const vis = dynamic_cast<const presets_item_visible *>(item);
+  if(!vis)
+    return;
+
+  if(vis->icon) {
+    const std::string filename(reinterpret_cast<const char *>(vis->icon));
+    const std::vector<std::string>::const_iterator it = std::find_if(
+                      basedirs.begin(), basedirs.end(), check_icon(filename));
+    if(it == basedirs.end())
+      missingIcons.insert(filename);
+  }
+
+  const presets_item_group * const group = dynamic_cast<const presets_item_group *>(vis);
+  if(group) {
+    std::for_each(group->items.begin(), group->items.end(), checkItem);
+  }
+}
+
+int main(int argc, char **argv)
 {
   struct presets_items *presets;
 
   xmlInitParser();
+
+  for(int i = 1; i < argc; i++)
+    basedirs.push_back(argv[i]);
 
   presets = josm_presets_load();
 
@@ -25,7 +90,15 @@ int main(void)
 
   std::cout << presets->items.size() << " top level items found" << std::endl;
 
+  std::for_each(presets->items.begin(), presets->items.end(), checkItem);
+
+  if(!missingIcons.empty()) {
+    std::cerr << missingIcons.size() << " icons missing" << std::endl;
+    std::for_each(missingIcons.begin(), missingIcons.end(), err);
+  }
+
   josm_presets_free(presets);
   xmlCleanupParser();
+
   return 0;
 }
