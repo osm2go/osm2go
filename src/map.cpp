osm2go/osm2go

commit bfaf2cb947b8d15664104740e1529df046dcfa3f

do not store empty tag list as "last" preset

src/map.cpp
@@ -430,18 +430,20 @@ static void map_object_select(appdata_t *appdata, object_t &object) {
 void map_item_deselect(appdata_t *appdata) {
 
   /* save tags for "last" function in info dialog */
-  if(appdata->map->selected.object.type == NODE) {
-    std::vector<tag_t> &v = appdata->map->last_node_tags;
-    std::for_each(v.begin(), v.end(), osm_tag_members_free);
+  if(appdata->map->selected.object.obj->tag) {
+    std::vector<tag_t> clear;
+    if(appdata->map->selected.object.type == NODE) {
+      clear.swap(appdata->map->last_node_tags);
 
-    appdata->map->last_node_tags =
-      osm_tags_list_copy(appdata->map->selected.object.obj->tag);
-  } else if(appdata->map->selected.object.type == WAY) {
-    std::vector<tag_t> &v = appdata->map->last_way_tags;
-    std::for_each(v.begin(), v.end(), osm_tag_members_free);
+      appdata->map->last_node_tags =
+        osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+    } else if(appdata->map->selected.object.type == WAY) {
+      clear.swap(appdata->map->last_way_tags);
 
-    appdata->map->last_way_tags =
-      osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+      appdata->map->last_way_tags =
+        osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+    }
+    std::for_each(clear.begin(), clear.end(), osm_tag_members_free);
   }
 
   /* remove statusbar message */

commit 91330381c8a0b8b60c42f70974844a861716f972

fix crash when trying to deselect and nothing was selected
Broken in commit bfaf2cb947b8d15664104740e1529df046dcfa3f.

src/map.cpp
@@ -431,7 +431,7 @@ static void map_object_select(appdata_t *appdata, object_t &object) {
 void map_item_deselect(appdata_t *appdata) {
 
   /* save tags for "last" function in info dialog */
-  if(appdata->map->selected.object.obj->tag) {
+  if(appdata->map->selected.object.obj && appdata->map->selected.object.obj->tag) {
     std::vector<tag_t> clear;
     if(appdata->map->selected.object.type == NODE) {
       clear.swap(appdata->map->last_node_tags);