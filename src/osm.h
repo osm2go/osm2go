osm2go/osm2go

commit 8aa76a25d0fabc5a98a6cee16b1e9059b7aca99d

convert osm_tag_find() to tag_t::find() with convenience overloads

src/osm.h
@@ -70,6 +70,11 @@ typedef struct tag_t {
   { }
 
   bool is_creator_tag() const;
+  tag_t *find(const char *key);
+  inline tag_t *find(const xmlChar *key)
+  { return find(reinterpret_cast<const xmlChar *>(key)); }
+  inline const tag_t *find(const char *key) const
+  { return const_cast<tag_t *>(this)->find(key); }
 #endif
 } tag_t;
 
@@ -293,9 +298,9 @@ struct osm_t {
 
   struct icon_t **icons;
 
+#ifdef __cplusplus
   bounds_t rbounds;
 
-#ifdef __cplusplus
   std::map<item_id_t, node_t *> nodes;
   std::map<item_id_t, way_t *> ways;
   std::map<item_id_t, relation_t *> relations;
@@ -336,7 +341,6 @@ void osm_free(osm_t *osm);
 
 void osm_tag_free(tag_t *tag);
 void osm_tags_free(tag_t *tag);
-tag_t *osm_tag_find(tag_t* tag, const char* key);
 const char *osm_tag_get_by_key(const tag_t *tag, const char *key);
 gboolean osm_tag_key_and_value_present(const tag_t *haystack, const tag_t *tag);
 gboolean osm_tag_key_other_value_present(const tag_t *haystack, const tag_t *tag);

src/josm_presets.cpp
@@ -529,7 +529,7 @@ static void presets_item_dialog(presets_context_t *context,
 
     for(widget_cnt = widget_skip; it != itEnd; it++, widget_cnt++) {
       /* check if there's a value with this key already */
-      tag_t *otag = osm_tag_find(*orig_tag, (char*)(*it)->key);
+      tag_t *otag = (*orig_tag)->find((*it)->key);
       const char *preset = otag ? otag->value : NULL;
 
       switch((*it)->type) {
@@ -710,7 +710,7 @@ static void presets_item_dialog(presets_context_t *context,
       case WIDGET_TYPE_KEY:
 	g_assert(!gtk_widgets[widget_cnt]);
 	g_assert(!otag);
-	otag = osm_tag_find(*orig_tag, (char*)(*it)->key);
+	otag = (*orig_tag)->find((*it)->key);
 
 	changed |= store_value(*it, last, (char*)(*it)->key_w.value);
 	break;

src/osm.cpp
@@ -1078,9 +1078,10 @@ gboolean osm_sanity_check(GtkWidget *parent, const osm_t *osm) {
 
 /* ------------------------- misc access functions -------------- */
 
-tag_t *osm_tag_find(tag_t* tag, const char* key) {
+tag_t *tag_t::find(const char* key) {
   if(!key) return NULL;
 
+  tag_t *tag = this;
   while(tag) {
     if(strcasecmp(tag->key, key) == 0)
       return tag;
@@ -1092,7 +1093,7 @@ tag_t *osm_tag_find(tag_t* tag, const char* key) {
 }
 
 const char *osm_tag_get_by_key(const tag_t* tag, const char* key) {
-  const tag_t *t = osm_tag_find(const_cast<tag_t*>(tag), key);
+  const tag_t *t = tag->find(key);
 
   if (t)
     return t->value;

commit 074ee2752a6ca4926091900b92d3bdcc5f2d0fac

fix endless loop when calling tag_t::find(xmlChar *)
This was broken in commit 8aa76a25d0fabc5a98a6cee16b1e9059b7aca99d.

src/osm.h
@@ -72,7 +72,7 @@ typedef struct tag_t {
   bool is_creator_tag() const;
   tag_t *find(const char *key);
   inline tag_t *find(const xmlChar *key)
-  { return find(reinterpret_cast<const xmlChar *>(key)); }
+  { return find(reinterpret_cast<const char *>(key)); }
   inline const tag_t *find(const char *key) const
   { return const_cast<tag_t *>(this)->find(key); }
   const char *get_by_key(const char *key) const;