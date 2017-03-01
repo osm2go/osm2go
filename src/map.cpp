osm2go/osm2go

commit bfaf2cb947b8d15664104740e1529df046dcfa3f

do not store empty tag list as "last" preset

src/map.cpp
@@ -430,18 +430,20 @@ static void map_object_select(appdata_t *appdata, object_t &object) {
 void map_item_deselect(appdata_t *appdata) {
 
   /* save tags for "last" function in info dialog */
-  if(appdata->map->selected.object.type == NODE) {
-    std::vector<tag_t> &v = appdata->map->last_node_tags;
-    std::for_each(v.begin(), v.end(), osm_tag_members_free);
+  if(appdata->map->selected.object.obj->tag) {
+    std::vector<tag_t> clear;
+    if(appdata->map->selected.object.type == NODE) {
+      clear.swap(appdata->map->last_node_tags);
 
-    appdata->map->last_node_tags =
-      osm_tags_list_copy(appdata->map->selected.object.obj->tag);
-  } else if(appdata->map->selected.object.type == WAY) {
-    std::vector<tag_t> &v = appdata->map->last_way_tags;
-    std::for_each(v.begin(), v.end(), osm_tag_members_free);
+      appdata->map->last_node_tags =
+        osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+    } else if(appdata->map->selected.object.type == WAY) {
+      clear.swap(appdata->map->last_way_tags);
 
-    appdata->map->last_way_tags =
-      osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+      appdata->map->last_way_tags =
+        osm_tags_list_copy(appdata->map->selected.object.obj->tag);
+    }
+    std::for_each(clear.begin(), clear.end(), osm_tag_members_free);
   }
 
   /* remove statusbar message */

commit 81e98a57a1a686296d8ead4db25e96c84d2d3482

fix crash when deselecting an invalid item
My way to reproduce this was: select something, download OSM data, click
somewhere. The problem was that the object pointer was checked, not the object
type. There are several things where an item may be invalid but has a random
pointer still set.

This was broken in commit bfaf2cb947b8d15664104740e1529df046dcfa3f and only
partially fixed in 91330381c8a0b8b60c42f70974844a861716f972.

src/map.cpp
@@ -437,7 +437,7 @@ static void map_object_select(appdata_t *appdata, object_t &object) {
 void map_item_deselect(appdata_t *appdata) {
 
   /* save tags for "last" function in info dialog */
-  if(appdata->map->selected.object.obj && appdata->map->selected.object.obj->has_tag()) {
+  if(appdata->map->selected.object.is_real() && appdata->map->selected.object.obj->has_tag()) {
     std::vector<tag_t> clear;
     if(appdata->map->selected.object.type == NODE) {
       clear.swap(appdata->map->last_node_tags);