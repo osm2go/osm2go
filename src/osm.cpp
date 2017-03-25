osm2go/osm2go

commit 368477310bd129bc18f94eb14b7997d5df69874b

store the node icons in the style instead of node_t
Most nodes do not have an icon attached to then so this should save some memory.

src/osm.cpp
@@ -393,19 +393,12 @@ void tag_t::update_value(const char *nvalue)
 
 /* ------------------- node handling ------------------- */
 
-void node_t::cleanup(osm_t *osm) {
-  if(icon_buf)
-    icon_free(osm->icons, icon_buf);
-
-  /* there must not be anything left in this chain */
-  g_assert(!map_item_chain);
-
-  tags.clear();
-}
-
 void osm_t::node_free(node_t *node) {
   nodes.erase(node->id);
-  node->cleanup(this);
+
+  /* there must not be anything left in this chain */
+  g_assert(!node->map_item_chain);
+
   delete node;
 }
 
@@ -413,7 +406,6 @@ struct nodefree {
   osm_t * const osm;
   nodefree(osm_t *o) : osm(o) {}
   void operator()(std::pair<item_id_t, node_t *> pair) {
-    pair.second->cleanup(osm);
     delete pair.second;
   }
 };
@@ -2220,7 +2212,6 @@ node_t::node_t()
   : base_object_t()
   , ways(0)
   , zoom_max(0.0)
-  , icon_buf(0)
   , map_item_chain(0)
 {
   memset(&pos, 0, sizeof(pos));
@@ -2233,7 +2224,6 @@ node_t::node_t(item_id_t ver, const lpos_t &lp, const pos_t &p)
   , lpos(lp)
   , ways(0)
   , zoom_max(0.0)
-  , icon_buf(0)
   , map_item_chain(0)
 {
 }

src/josm_elemstyles.cpp
@@ -447,11 +447,19 @@ struct condition_not_matches_obj {
   }
 };
 
+static void node_icon_unref(style_t *style, node_t *node) {
+  std::map<item_id_t, GdkPixbuf *>::iterator it = style->node_icons.find(node->id);
+  if(it != style->node_icons.end()) {
+    icon_free(style->iconP, it->second);
+    style->node_icons.erase(it);
+  }
+}
+
 struct colorize_node {
-  const style_t * const style;
+  style_t * const style;
   node_t * const node;
   bool &somematch;
-  colorize_node(const style_t *s, node_t *n, bool &m) : style(s), node(n), somematch(m) {}
+  colorize_node(style_t *s, node_t *n, bool &m) : style(s), node(n), somematch(m) {}
   void operator()(elemstyle_t *elemstyle);
 };
 
@@ -470,20 +478,20 @@ void colorize_node::operator()(elemstyle_t *elemstyle)
                            elemstyle->icon.filename, NULL);
 
     /* free old icon if there's one present */
-    if(node->icon_buf) {
-      icon_free(style->iconP, node->icon_buf);
-      node->icon_buf = NULL;
-    }
+    node_icon_unref(style, node);
 
-    node->icon_buf = icon_load(style->iconP, name);
+    GdkPixbuf *buf = icon_load(style->iconP, name);
     g_free(name);
+
+    if(buf)
+      style->node_icons[node->id] = buf;
 
     if (elemstyle->zoom_max > 0)
       node->zoom_max = elemstyle->zoom_max;
   }
 }
 
-void josm_elemstyles_colorize_node(const style_t *style, node_t *node) {
+void josm_elemstyles_colorize_node(style_t *style, node_t *node) {
   node->zoom_max = style->node.zoom_max;
 
   bool somematch = false;
@@ -491,15 +499,13 @@ void josm_elemstyles_colorize_node(const style_t *style, node_t *node) {
   std::for_each(style->elemstyles.begin(), style->elemstyles.end(), fc);
 
   /* clear icon for node if not matched at least one rule and has an icon attached */
-  if (!somematch && node->icon_buf) {
-    icon_free(style->iconP, node->icon_buf);
-    node->icon_buf = NULL;
-  }
+  if(!somematch)
+    node_icon_unref(style, node);
 }
 
 struct josm_elemstyles_colorize_node_functor {
-  const style_t * const styles;
-  josm_elemstyles_colorize_node_functor(const style_t *s) : styles(s) {}
+  style_t * const style;
+  josm_elemstyles_colorize_node_functor(style_t *s) : style(s) {}
   void operator()(std::pair<item_id_t, node_t *> pair);
 };
 
@@ -507,12 +513,9 @@ void josm_elemstyles_colorize_node_functor::operator()(std::pair<item_id_t, node
   node_t * const node = pair.second;
   /* remove all icon references that may still be there from */
   /* an old style */
-  if(node->icon_buf) {
-    icon_free(styles->iconP, node->icon_buf);
-    node->icon_buf = NULL;
-  }
+  node_icon_unref(style, node);
 
-  josm_elemstyles_colorize_node(styles, node);
+  josm_elemstyles_colorize_node(style, node);
 }
 
 static int line_mod_apply(gint width, const elemstyle_width_mod_t *mod) {
@@ -666,7 +669,7 @@ void josm_elemstyles_colorize_way(const style_t *style, way_t *way) {
   f(way);
 }
 
-void josm_elemstyles_colorize_world(const style_t *styles, osm_t *osm) {
+void josm_elemstyles_colorize_world(style_t *styles, osm_t *osm) {
 
   printf("preparing colors\n");
 

src/josm_elemstyles.h
@@ -53,9 +53,9 @@ bool parse_color(xmlNode *a_node, const char *name, elemstyle_color_t &color);
 
 struct style_t;
 
-void josm_elemstyles_colorize_node(const style_t *style, node_t *node);
+void josm_elemstyles_colorize_node(style_t *style, node_t *node);
 void josm_elemstyles_colorize_way(const style_t *style, way_t *way);
-void josm_elemstyles_colorize_world(const style_t *style, osm_t *osm);
+void josm_elemstyles_colorize_world(style_t *style, osm_t *osm);
 
 #endif
 

src/map.cpp
@@ -190,9 +190,10 @@ static void map_node_select(appdata_t *appdata, node_t *node) {
   new_map_item->highlight = TRUE;
 
   float radius = 0;
-  if(node->icon_buf && map->style->icon.enable) {
-    gint w = gdk_pixbuf_get_width(map_item->object.node->icon_buf);
-    gint h = gdk_pixbuf_get_height(map_item->object.node->icon_buf);
+  std::map<item_id_t, GdkPixbuf *>::iterator it = map->style->node_icons.find(node->id);
+  if(it != map->style->node_icons.end() && map->style->icon.enable) {
+    gint w = gdk_pixbuf_get_width(it->second);
+    gint h = gdk_pixbuf_get_height(it->second);
 
     /* icons are technically square, so a radius slightly bigger */
     /* than sqrt(2)*MAX(w,h) should fit nicely */
@@ -521,12 +522,14 @@ static canvas_item_t *map_node_new(map_t *map, node_t *node, gint radius,
   map_item_t *map_item = g_new0(map_item_t, 1);
   map_item->object = node;
 
-  if(!node->icon_buf || !map->style->icon.enable)
+  const std::map<item_id_t, GdkPixbuf *>::const_iterator it = map->style->node_icons.find(node->id);
+
+  if(it == map->style->node_icons.end() || !map->style->icon.enable)
     map_item->item = canvas_circle_new(map->canvas, CANVAS_GROUP_NODES,
        node->lpos.x, node->lpos.y, radius, width, fill, border);
   else
     map_item->item = canvas_image_new(map->canvas, CANVAS_GROUP_NODES,
-      node->icon_buf, node->lpos.x, node->lpos.y,
+      it->second, node->lpos.x, node->lpos.y,
 		      map->state->detail * map->style->icon.scale,
 		      map->state->detail * map->style->icon.scale);
 

src/osm.h
@@ -32,7 +32,6 @@ extern "C" {
 
 #include <math.h>
 #include <glib.h>
-#include <gtk/gtk.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
@@ -329,15 +328,10 @@ public:
   int ways;
   float zoom_max;
 
-  /* icon */
-  GdkPixbuf *icon_buf;
-
   /* a link to the visual representation on screen */
   struct map_item_chain_t *map_item_chain;
 
   xmlChar *generate_xml(item_id_t changeset);
-
-  void cleanup(osm_t *osm);
 };
 
 struct item_id_chain_t {

src/style.cpp
@@ -423,11 +423,21 @@ style_t::style_t()
   background.color = 0xffffffff; // white
 }
 
+struct unref_icon {
+  icon_t ** const icons;
+  unref_icon(icon_t **i) : icons(i) {}
+  void operator()(const std::pair<item_id_t, GdkPixbuf *> &pair) {
+    icon_free(icons, pair.second);
+  }
+};
+
 style_t::~style_t()
 {
   printf("freeing style\n");
 
   josm_elemstyles_free(elemstyles);
+
+  std::for_each(node_icons.begin(), node_icons.end(), unref_icon(iconP));
 
   g_free(name);
   g_free(icon.path_prefix);

src/style.h
@@ -28,6 +28,8 @@
 
 #include <vector>
 
+class node_t;
+
 struct style_t {
   style_t();
   ~style_t();
@@ -89,6 +91,8 @@ struct style_t {
   } background;
 
   std::vector<elemstyle_t *> elemstyles;
+
+  std::map<item_id_t, GdkPixbuf *> node_icons;
 };
 
 extern "C" {

commit f0322abe8308d0f684cbc01e51bed629564cc49c

fix memleak of all node tags when closing OSM file
This was accidentially caused in commit 368477310bd129bc18f94eb14b7997d5df69874b.

src/osm.cpp
@@ -402,13 +402,10 @@ void osm_t::node_free(node_t *node) {
   delete node;
 }
 
-struct nodefree {
-  osm_t * const osm;
-  nodefree(osm_t *o) : osm(o) {}
-  void operator()(std::pair<item_id_t, node_t *> pair) {
-    delete pair.second;
-  }
-};
+static inline void nodefree(std::pair<item_id_t, node_t *> pair) {
+  pair.second->tags.clear();
+  delete pair.second;
+}
 
 /* ------------------- way handling ------------------- */
 static void osm_unref_node(node_t* node)
@@ -571,7 +568,7 @@ void osm_free(osm_t *osm) {
   if(!osm) return;
 
   std::for_each(osm->ways.begin(), osm->ways.end(), way_free);
-  std::for_each(osm->nodes.begin(), osm->nodes.end(), nodefree(osm));
+  std::for_each(osm->nodes.begin(), osm->nodes.end(), nodefree);
   std::for_each(osm->relations.begin(), osm->relations.end(),
                 osm_relation_free_pair);
   delete osm;