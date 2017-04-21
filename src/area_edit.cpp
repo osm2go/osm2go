c1ba1865751bdca50e573e92c2da82a242979971
Rolf Eike Beer <eike@sf-mail.de>
2017-04-21 21:22:10 +0200

    fix warning about printf() arguments
    The compiler warning was actually right forever: I missed one level of
    indirection and messed it up.

--- src/area_edit.cpp
+++ src/area_edit.cpp
@@ -156,12 +156,14 @@ static gboolean area_warning(context_t *context) {
   double area = selected_area(context);
 
   if(area > WARN_OVER) {
+    gchar *text = g_strdup_printf(warn_text(), area, area / (KMPMIL * KMPMIL),
+                                  WARN_OVER, WARN_OVER/(KMPMIL*KMPMIL));
     ret = yes_no_f(context->dialog, context->area->appdata,
 		   MISC_AGAIN_ID_AREA_TOO_BIG, MISC_AGAIN_FLAG_DONT_SAVE_NO,
 		   _("Area size warning!"),
 		   _("%s\n\nDo you really want to continue?"),
-                   warn_text(), area, area / (KMPMIL * KMPMIL),
-                                WARN_OVER, WARN_OVER/(KMPMIL*KMPMIL));
+                   text);
+    g_free(text);
   }
 
   return ret;
