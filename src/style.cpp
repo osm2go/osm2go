osm2go/osm2go

commit f1bb4cd5ac8acd8ebbc3f2a16e3879a8821e0c5a

use std::vector<std::string> to store a list of file names

src/style.cpp
@@ -24,9 +24,11 @@
 #include "misc.h"
 #include "settings.h"
 
+#include <algorithm>
 #include <cstring>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
+#include <string>
 #include <strings.h>
 
 #if !defined(LIBXML_TREE_ENABLED) || !defined(LIBXML_OUTPUT_ENABLED)
@@ -253,103 +255,105 @@ void style_free(style_t *style) {
   delete style;
 }
 
-static char *style_basename(const char *name) {
-  char *retval;
-  const char *slash = strrchr(name, '/');
-
-  if(slash != NULL)
-    retval = g_strdup(slash + 1);
-  else
-    retval = g_strdup(name);
+static std::string style_basename(const std::string &name) {
+  std::string::size_type pos = name.rfind("/");
 
   /* and cut off extension */
-  if(strrchr(retval, '.'))
-    *strrchr(retval, '.') = 0;
+  std::string::size_type extpos = name.rfind(".");
+  if(pos == name.npos)
+    pos = 0;
+  else
+    pos++; // skip also the '/' itself
 
-  return retval;
+  return name.substr(pos, extpos - pos);
+}
+
+struct combo_add_styles {
+  GtkWidget * const cbox;
+  int cnt;
+  int match;
+  appdata_t * const appdata;
+  combo_add_styles(GtkWidget *w, appdata_t *a) : cbox(w), cnt(0), match(-1), appdata(a) {};
+  void operator()(const std::string &filename);
+};
+
+void combo_add_styles::operator()(const std::string &filename)
+{
+  printf("  file: %s\n", filename.c_str());
+
+  style_t *style = style_parse(appdata, filename.c_str(), NULL, TRUE);
+  printf("    name: %s\n", style->name);
+  combo_box_append_text(cbox, style->name);
+
+  const std::string &basename = style_basename(filename);
+  if(strcmp(basename.c_str(), appdata->settings->style) == 0)
+    match = cnt;
+
+  style_free(style);
+
+  cnt++;
 }
 
 GtkWidget *style_select_widget(appdata_t *appdata) {
-  file_chain_t *chain = file_scan(".style");
+  const std::vector<std::string> &chain = file_scan(".style");
 
   /* there must be at least one style, otherwise */
   /* the program wouldn't be running */
-  g_assert(chain);
+  g_assert(!chain.empty());
 
   GtkWidget *cbox = combo_box_new(_("Style"));
 
   /* fill combo box with presets */
-  int cnt = 0, match = -1;
-  while(chain) {
-    file_chain_t *next = chain->next;
+  combo_add_styles cas(cbox, appdata);
+  std::for_each(chain.begin(), chain.end(), cas);
 
-    printf("  file: %s\n", chain->name);
-
-    style_t *style = style_parse(appdata, chain->name, NULL, TRUE);
-    printf("    name: %s\n", style->name);
-    combo_box_append_text(cbox, style->name);
-
-    char *basename = style_basename(chain->name);
-    if(strcmp(basename, appdata->settings->style) == 0) match = cnt;
-    g_free(basename);
-
-    style_free(style);
-
-    cnt++;
-
-    g_free(chain->name);
-    g_free(chain);
-    chain = next;
-  }
-
-  if(match >= 0)
-    combo_box_set_active(cbox, match);
+  if(cas.match >= 0)
+    combo_box_set_active(cbox, cas.match);
 
   return cbox;
 }
 
+struct style_find {
+  appdata_t * const appdata;
+  const char * const name;
+  style_find(appdata_t *a, const char *n) : appdata(a), name(n) {}
+  bool operator()(const std::string &filename);
+};
+
+bool style_find::operator()(const std::string &filename)
+{
+  style_t *style = style_parse(appdata, filename.c_str(), NULL, TRUE);
+
+  bool match = (strcmp(style->name, name) == 0);
+  style_free(style);
+
+  return match;
+}
+
 void style_change(appdata_t *appdata, const char *name) {
-  char *new_style = NULL, *fname = NULL;
+  const std::vector<std::string> &chain = file_scan(".style");
 
-  file_chain_t *chain = file_scan(".style");
+  const std::vector<std::string>::const_iterator it =
+      std::find_if(chain.begin(), chain.end(), style_find(appdata, name));
 
-  while(chain) {
-    file_chain_t *next = chain->next;
-    style_t *style = style_parse(appdata, chain->name, NULL, TRUE);
-
-    if(new_style == NULL && strcmp(style->name, name) == 0) {
-      new_style = style_basename(chain->name);
-      fname = chain->name;
-      chain->name = NULL;
-    }
-
-    style_free(style);
-
-    g_free(chain->name);
-    g_free(chain);
-    chain = next;
-  }
+  g_assert(it != chain.end());
+  const std::string &new_style = style_basename(*it);
 
   /* check if style has really been changed */
   if(appdata->settings->style &&
-     !strcmp(appdata->settings->style, new_style)) {
-    g_free(new_style);
-    g_free(fname);
+     !strcmp(appdata->settings->style, new_style.c_str())) {
     return;
   }
 
-  style_t *nstyle = style_load_fname(appdata, fname);
+  style_t *nstyle = style_load_fname(appdata, it->c_str());
   if (nstyle == NULL) {
     errorf(GTK_WIDGET(appdata->window),
-           _("Error loading style %s"), fname);
-    g_free(new_style);
-    g_free(fname);
+           _("Error loading style %s"), it->c_str());
     return;
   }
-  g_free(fname);
 
   g_free(appdata->settings->style);
-  appdata->settings->style = new_style;
+  appdata->settings->style = g_strdup(new_style.c_str());
 
   map_clear(appdata, MAP_LAYER_OBJECTS_ONLY);
   /* let gtk clean up first */

src/misc.cpp
@@ -247,8 +247,8 @@ gchar *find_file(const char *n1, const char *n2, const char *n3) {
 
 /* scan all data directories for the given file extension and */
 /* return a list of files matching this extension */
-file_chain_t *file_scan(const char *extension) {
-  file_chain_t *chain = NULL, **chainP = &chain;
+std::vector<std::string> file_scan(const char *extension) {
+  std::vector<std::string> chain;
 
   const char **path = data_paths;
   char *p = getenv("HOME");
@@ -269,13 +269,12 @@ file_chain_t *file_scan(const char *extension) {
 
 	if(name) {
 	  if(g_str_has_suffix(name, extension)) {
-	    gchar *fullname = g_strconcat(dirname, name, NULL);
-	    if(g_file_test(fullname, G_FILE_TEST_IS_REGULAR)) {
-	      *chainP = g_new0(file_chain_t, 1);
-	      (*chainP)->name = fullname;
-	      chainP = &(*chainP)->next;
-	    } else
-	      g_free(fullname);
+            std::string fullname;
+            fullname.reserve(strlen(dirname) + strlen(name) + 1);
+            fullname = dirname;
+            fullname += name;
+            if(g_file_test(fullname.c_str(), G_FILE_TEST_IS_REGULAR))
+              chain.push_back(fullname);
 	  }
 	}
       } while(name);

src/misc.h
@@ -32,11 +32,6 @@
 #define MISC_AGAIN_ID_EXPORT_OVERWRITE (1<<6)
 #define MISC_AGAIN_ID_AREA_TOO_BIG     (1<<7)
 
-typedef struct file_chain_t {
-  char *name;
-  struct file_chain_t *next;
-} file_chain_t;
-
 /* these flags prevent you from leaving the dialog with no (or yes) */
 /* if the "dont show me this dialog again" checkbox is selected. This */
 /* makes sure, that you can't permanently switch certain things in, but */
@@ -51,6 +46,12 @@ typedef struct file_chain_t {
 #endif
 
 #ifdef __cplusplus
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> file_scan(const char *extension);
+
 extern "C" {
 #endif
 
@@ -71,7 +72,6 @@ gboolean yes_no_f(GtkWidget *parent,
 		  const char *title, const char *fmt, ...) G_GNUC_PRINTF(6, 7);
 
 gchar *find_file(const char *n1, const char *n2, const char *n3);
-file_chain_t *file_scan(const char *extension);
 
 /* dialog size are specified rather fuzzy */
 #define MISC_DIALOG_NOSIZE  -1

commit ee120bdceaa26d219038d2189154b9d9c1f910a0

select the used style when creating the style selection widget
Broken in f1bb4cd5ac8acd8ebbc3f2a16e3879a8821e0c5a.

src/style.cpp
@@ -267,9 +267,9 @@ static std::string style_basename(const std::string &name) {
 struct combo_add_styles {
   GtkWidget * const cbox;
   int cnt;
-  int match;
+  int &match;
   appdata_t * const appdata;
-  combo_add_styles(GtkWidget *w, appdata_t *a) : cbox(w), cnt(0), match(-1), appdata(a) {};
+  combo_add_styles(GtkWidget *w, appdata_t *a, int &m) : cbox(w), cnt(0), match(m), appdata(a) {};
   void operator()(const std::string &filename);
 };
 
@@ -343,7 +343,8 @@ GtkWidget *style_select_widget(appdata_t *appdata) {
   GtkWidget *cbox = combo_box_new(_("Style"));
 
   /* fill combo box with presets */
-  combo_add_styles cas(cbox, appdata);
+  int match = -1;
+  combo_add_styles cas(cbox, appdata, match);
   std::for_each(chain.begin(), chain.end(), cas);
 
   if(cas.match >= 0)