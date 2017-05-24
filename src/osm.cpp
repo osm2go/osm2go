osm2go/osm2go

commit cb7fff264827392f6d48dee37852dc9ab0c66576

use more specific variants of g_assert()

src/osm.cpp
@@ -158,7 +158,7 @@ item_id_t object_t::get_id() const {
 }
 
 void object_t::set_flags(int set) {
-  g_assert(is_real());
+  g_assert_true(is_real());
   obj->flags |=  set;
 }
 
@@ -413,7 +413,7 @@ void osm_t::node_free(node_t *node) {
   nodes.erase(node->id);
 
   /* there must not be anything left in this chain */
-  g_assert(!node->map_item_chain);
+  g_assert_null(node->map_item_chain);
 
   delete node;
 }
@@ -592,7 +592,7 @@ static inline gint __attribute__((nonnull(2))) my_strcmp(const xmlChar *a, const
 static void skip_element(xmlTextReaderPtr reader) {
   g_assert(xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT);
   const xmlChar *name = xmlTextReaderConstName(reader);
-  g_assert(name);
+  g_assert_nonnull(name);
   int depth = xmlTextReaderDepth(reader);
 
   if(xmlTextReaderIsEmptyElement(reader))
@@ -930,7 +930,7 @@ static osm_t *process_osm(xmlTextReaderPtr reader) {
   /* no attributes of interest */
 
   const xmlChar *name = xmlTextReaderConstName(reader);
-  g_assert(name);
+  g_assert_nonnull(name);
 
   /* read next node */
   int num_elems = 0;
@@ -1334,7 +1334,7 @@ void osm_t::way_restore(way_t *way, const std::vector<item_id_chain_t> &id_chain
   ways[way->id] = way;
 
   /* restore node memberships by converting ids into real pointers */
-  g_assert(way->node_chain.empty());
+  g_assert_true(way->node_chain.empty());
   way_member_ref fc(this, way->node_chain);
   std::for_each(id_chain.begin(), id_chain.end(), fc);
 
@@ -1602,7 +1602,7 @@ void osm_unref_way_free::operator()(node_t* node)
     /* associated ways as the only such way is the one we are currently */
     /* deleting */
     const way_chain_t &way_chain = osm->node_delete(node, false, false);
-    g_assert(way_chain.size() == 1);
+    g_assert_cmpuint(way_chain.size(), ==, 1);
     g_assert(way_chain.front() == way);
   }
 }
@@ -1912,7 +1912,7 @@ std::vector<stag_t *> osm_tags_list_copy(const std::vector<stag_t> &tags) {
 
 void tag_list_t::copy(const tag_list_t &other)
 {
-  g_assert(!contents);
+  g_assert_null(contents);
 
   if(other.empty())
     return;
@@ -1995,7 +1995,7 @@ std::string object_t::get_name() const {
   }
 
   if(type) {
-    g_assert(ret.empty());
+    g_assert_true(ret.empty());
     ret = type;
   }
 
@@ -2165,7 +2165,7 @@ bool way_t::ends_with_node(const node_t *node) const
     return false;
 
   /* any valid way must have at least two nodes */
-  g_assert(!node_chain.empty());
+  g_assert_true(node_chain.empty());
 
   if(node_chain.front() == node)
     return true;
@@ -2183,7 +2183,7 @@ void way_t::cleanup() {
   tags.clear();
 
   /* there must not be anything left in this chain */
-  g_assert(!map_item_chain);
+  g_assert_null(map_item_chain);
 }
 
 member_t::member_t(type_t t)

src/canvas.c
@@ -36,6 +36,7 @@
 #include "canvas.h"
 
 #include "appdata.h"
+#include "misc.h"
 
 /* The fuzziness allows to specify how far besides an object a user may */
 /* click so it's still considered a click onto that object. This can */
@@ -61,7 +62,7 @@ static void canvas_item_info_dechain(canvas_item_info_t *item_info) {
   while(*itemP && *itemP != item_info)
     itemP = &(*itemP)->next;
 
-  g_assert(*itemP);
+  g_assert_nonnull(*itemP);
 
   /* check if we are removing the last entry in the list and */
   /* adjust last pointer if yes */
@@ -108,7 +109,7 @@ static gint item_info_destroy(G_GNUC_UNUSED canvas_item_t *canvas_item,
 static void canvas_item_prepend(canvas_t *canvas, canvas_group_t group,
 			canvas_item_t *canvas_item, canvas_item_info_t *item) {
   if(!canvas->item_info[group].first) {
-    g_assert(!canvas->item_info[group].last);
+    g_assert_null(canvas->item_info[group].last);
     canvas->item_info[group].last = item;
   } else
     canvas->item_info[group].first->prev = item;
@@ -128,7 +129,7 @@ static void canvas_item_prepend(canvas_t *canvas, canvas_group_t group,
 static void canvas_item_append(canvas_t *canvas, canvas_group_t group,
 	       canvas_item_t *canvas_item, canvas_item_info_t *item) {
   if(!canvas->item_info[group].last) {
-    g_assert(!canvas->item_info[group].first);
+    g_assert_null(canvas->item_info[group].first);
     canvas->item_info[group].first = item;
   } else
     canvas->item_info[group].last->next = item;
@@ -163,7 +164,7 @@ static canvas_item_info_t *canvas_item_get_info(canvas_t *canvas,
 
 void canvas_item_info_push(canvas_t *canvas, canvas_item_t *item) {
   canvas_item_info_t *item_info = canvas_item_get_info(canvas, item);
-  g_assert(item_info);
+  g_assert_nonnull(item_info);
 
   printf("pushing item_info %p to background\n", item_info);
 

src/canvas_goocanvas.c
@@ -22,6 +22,7 @@
 #endif
 
 #include "appdata.h"
+#include "misc.h"
 
 /* ------------------- creating and destroying the canvas ----------------- */
 
@@ -334,7 +335,7 @@ void canvas_item_to_bottom(canvas_item_t *item) {
     g_object_get_data(G_OBJECT(goo_canvas_item_get_canvas(item)),
 		      "canvas-pointer");
 
-  g_assert(canvas);
+  g_assert_nonnull(canvas);
   canvas_item_info_push(canvas, item);
 #endif
 }
@@ -469,7 +470,7 @@ void canvas_item_get_segment_pos(canvas_item_t *item, gint seg,
   canvas_points_t *points = NULL;
   g_object_get(G_OBJECT(item), "points", &points, NULL);
 
-  g_assert(points);
+  g_assert_nonnull(points);
   g_assert_cmpint(seg, <, points->num_points-1);
 
   *x0 = points->coords[2 * seg + 0];

src/diff.cpp
@@ -377,7 +377,7 @@ static void diff_restore_node(xmlNodePtr node_node, osm_t *osm) {
     return;
   }
 
-  g_assert(node != O2G_NULLPTR);
+  g_assert_nonnull(node);
 
   std::vector<tag_t> ntags = xml_scan_tags(node_node->children);
   /* check if the same changes have been done upstream */
@@ -448,7 +448,7 @@ static void diff_restore_way(xmlNodePtr node_way, osm_t *osm) {
     return;
   }
 
-  g_assert(way != O2G_NULLPTR);
+  g_assert_nonnull(way);
 
   /* handle hidden flag */
   if(xml_get_prop_is(node_way, "hidden", "true"))
@@ -550,7 +550,7 @@ static void diff_restore_relation(xmlNodePtr node_rel, osm_t *osm) {
     return;
   }
 
-  g_assert(relation != O2G_NULLPTR);
+  g_assert_nonnull(relation);
 
   bool was_changed = false;
   std::vector<tag_t> ntags = xml_scan_tags(node_rel->children);

src/iconbar.c
@@ -23,6 +23,7 @@
 #include "icon.h"
 #include "info.h"
 #include "map_edit.h"
+#include "misc.h"
 
 #ifdef FINGER_UI
 #define TOOL_ICON(a)  a "_thumb"
@@ -357,7 +358,7 @@ void iconbar_free(iconbar_t *iconbar) {
 /* fremantle. technically they are still part of the iconbar and thus */
 /* are registered there */
 void iconbar_register_buttons(appdata_t *appdata, GtkWidget *ok, GtkWidget *cancel) {
-  g_assert(appdata->iconbar);
+  g_assert_nonnull(appdata->iconbar);
 
   appdata->iconbar->ok = ok;
   g_signal_connect_swapped(GTK_OBJECT(ok), "clicked",

src/info.cpp
@@ -89,7 +89,7 @@ void tag_context_t::update_collisions()
     do {
       stag_t *tag = O2G_NULLPTR;
       gtk_tree_model_get(GTK_TREE_MODEL(store), &iter, TAG_COL_DATA, &tag, -1);
-      g_assert(tag);
+      g_assert_nonnull(tag);
       gtk_list_store_set(store, &iter,
          TAG_COL_COLLISION, info_tag_key_collision(tags, tag), -1);
 
@@ -103,10 +103,10 @@ static void on_tag_remove(G_GNUC_UNUSED GtkWidget *button, tag_context_t *contex
 
   GtkTreeSelection *selection = list_get_selection(context->list);
   if(gtk_tree_selection_get_selected(selection, &model, &iter)) {
-    stag_t *tag;
+    stag_t *tag = O2G_NULLPTR;
     gtk_tree_model_get(model, &iter, TAG_COL_DATA, &tag, -1);
 
-    g_assert(tag);
+    g_assert_nonnull(tag);
 
     /* de-chain */
     printf("de-chaining tag %s/%s\n", tag->key.c_str(), tag->value.c_str());
@@ -515,7 +515,7 @@ void info_dialog(GtkWidget *parent, appdata_t *appdata) {
 /* given */
 bool info_dialog(GtkWidget *parent, appdata_t *appdata, object_t &object) {
 
-  g_assert(object.is_real());
+  g_assert_true(object.is_real());
 
   /* use implicit selection if not explicitely given */
   tag_context_t context(appdata, object);

src/josm_elemstyles.cpp
@@ -307,10 +307,10 @@ void StyleSax::startElement(const xmlChar *name, const xmlChar **attrs)
       else if(strcmp(reinterpret_cast<const char *>(attrs[i]), "b") == 0)
         b = attrs[i + 1];
     }
-    g_assert(k != O2G_NULLPTR);
+    g_assert_nonnull(k);
     styles.back()->conditions.push_back(elemstyle_condition_t(k, v));
     if(b) {
-      g_assert(v == O2G_NULLPTR);
+      g_assert_null(v);
       elemstyle_condition_t &cond = styles.back()->conditions.back();
       cond.isBool = true;
       cond.boolValue = parse_gboolean(reinterpret_cast<const char *>(b), true_values);
@@ -369,8 +369,8 @@ void StyleSax::startElement(const xmlChar *name, const xmlChar **attrs)
 
     line->bg.valid = hasBgColor && hasBgWidth;
 
-    g_assert(hasColor);
-    g_assert(hasWidth);
+    g_assert_true(hasColor);
+    g_assert_true(hasWidth);
 
     break;
   }
@@ -403,7 +403,7 @@ void StyleSax::startElement(const xmlChar *name, const xmlChar **attrs)
     }
 
     /* this has to be present */
-    g_assert(hasColor);
+    g_assert_true(hasColor);
     break;
   }
   case TagIcon:
@@ -414,7 +414,7 @@ void StyleSax::startElement(const xmlChar *name, const xmlChar **attrs)
         elemstyle->icon.filename = josm_icon_name_adjust(reinterpret_cast<const char *>(attrs[i + 1]));
     }
 
-    g_assert(!elemstyle->icon.filename.empty());
+    g_assert_false(elemstyle->icon.filename.empty());
 
     break;
   default:

src/josm_presets.cpp
@@ -80,7 +80,7 @@ static bool store_value(const presets_widget_t *widget, std::vector<stag_t *> &t
     const char *chstr;
     if(ctag != tags.end()) {
       /* update the previous tag structure */
-      g_assert(strcasecmp((*ctag)->key.c_str(), widget->key.c_str()) == 0);
+      g_assert_cmpint(strcasecmp((*ctag)->key.c_str(), widget->key.c_str()), ==, 0);
       /* only update if the value actually changed */
       if((*ctag)->value != value) {
         changed = true; /* mark as updated, actual change below */
@@ -297,7 +297,7 @@ static void presets_item_dialog(presets_context_t *context,
 
     /* skip all following non-interactive widgets: use the first one that
      * was found to be interactive above. */
-    g_assert((*it)->is_interactive());
+    g_assert_true((*it)->is_interactive());
 
     /* create table of required size */
     GtkWidget *table = gtk_table_new(std::accumulate(it, item->widgets.end(), 0, widget_rows), 2, FALSE);
@@ -424,7 +424,7 @@ cb_menu_item(GtkWidget *menu_item, gpointer data) {
   presets_context_t *context = (presets_context_t*)data;
 
   presets_item *item = static_cast<presets_item *>(g_object_get_data(G_OBJECT(menu_item), "item"));
-  g_assert(item);
+  g_assert_nonnull(item);
 
   presets_item_dialog(context, item);
 }
@@ -657,8 +657,8 @@ on_presets_picker_selected(GtkTreeSelection *selection, gpointer data) {
     }
 
     /* views parent is a scrolled window whichs parent in turn is the hbox */
-    g_assert(sub);
-    g_assert(view->parent);
+    g_assert_nonnull(sub);
+    g_assert_nonnull(view->parent);
     GtkWidget *hbox = view->parent->parent;
 
     gtk_box_pack_start_defaults(GTK_BOX(hbox), sub);
@@ -941,7 +941,7 @@ static gint button_press(GtkWidget *widget, GdkEventButton *event,
   gtk_menu_popup(GTK_MENU(context->menu), O2G_NULLPTR, O2G_NULLPTR, O2G_NULLPTR, O2G_NULLPTR,
                  event->button, event->time);
 #else
-  g_assert(context->submenus.empty());
+  g_assert_true(context->submenus.empty());
   /* popup our special picker like menu */
   GtkWidget *dialog =
       gtk_dialog_new_with_buttons(_("Presets"),
@@ -1152,7 +1152,7 @@ bool presets_widget_key::matchValue(const char *val) const
 
 const char *presets_widget_key::getValue(GtkWidget *widget) const
 {
-  g_assert(!widget);
+  g_assert_null(widget);
   return value.c_str();
 }
 

commit da5bb80f016e162731d071fe0d12881b037a7010

fix crash by inverted assertion
Broken in cb7fff264827392f6d48dee37852dc9ab0c66576.

src/osm.cpp
@@ -2297,7 +2297,7 @@ bool way_t::ends_with_node(const node_t *node) const
     return false;
 
   /* any valid way must have at least two nodes */
-  g_assert_true(node_chain.empty());
+  g_assert_false(node_chain.empty());
 
   if(node_chain.front() == node)
     return true;