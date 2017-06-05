32b839723294845b9a1091e0298cfac6bcc4e714
Rolf Eike Beer <eike@sf-mail.de>
2017-06-05 10:13:05 +0200

    tests: extend diff_restore test to also save the diff
    Now the original diff is checked as well as a freshly saved diff of the same
    data.

--- test/diff_restore.cpp
+++ test/diff_restore.cpp
@@ -11,8 +11,74 @@
 #include <gtk/gtk.h>
 #include <iostream>
 
+static void verify_diff(osm_t *osm)
+{
+  g_assert_cmpuint(12, ==, osm->nodes.size());
+  g_assert_cmpuint(3, ==, osm->ways.size());
+  g_assert_cmpuint(3, ==, osm->relations.size());
+
+  // new tag added in diff
+  const node_t * const n72 = osm->nodes[638499572];
+  g_assert_nonnull(n72);
+  g_assert((n72->flags & OSM_FLAG_DIRTY) != 0);
+  g_assert_nonnull(n72->tags.get_value("testtag"));
+  g_assert_cmpuint(n72->tags.asMap().size(), ==, 5);
+  // in diff, but the same as in .osm
+  const node_t * const n23 = osm->nodes[3577031223LL];
+  g_assert_nonnull(n23);
+  g_assert((n23->flags & OSM_FLAG_DIRTY) == 0);
+  g_assert_true(n23->tags.empty());
+  // deleted in diff
+  const way_t * const w = osm->ways[351899455];
+  g_assert_nonnull(w);
+  g_assert((w->flags & OSM_FLAG_DELETED) != 0);
+  // added in diff
+  const node_t * const nn1 = osm->nodes[-1];
+  g_assert_nonnull(nn1);
+  g_assert_cmpfloat(nn1->pos.lat, ==, 52.2693518);
+  g_assert_cmpfloat(nn1->pos.lon, ==, 9.5760140);
+  g_assert_true(nn1->tags.empty());
+  // added in diff, same position as existing node
+  const node_t * const nn2 = osm->nodes[-2];
+  g_assert_nonnull(nn2);
+  g_assert_cmpfloat(nn2->pos.lat, ==, 52.269497);
+  g_assert_cmpfloat(nn2->pos.lon, ==, 9.5752223);
+  g_assert_true(nn2->tags.empty());
+  // which is this one
+  const node_t * const n27 = osm->nodes[3577031227LL];
+  g_assert_nonnull(n27);
+  g_assert((n27->flags & OSM_FLAG_DIRTY) == 0);
+  g_assert_cmpfloat(nn2->pos.lat, ==, n27->pos.lat);
+  g_assert_cmpfloat(nn2->pos.lon, ==, n27->pos.lon);
+  // the upstream version has "wheelchair", we have "source"
+  // our modification must survive
+  const way_t * const w452 = osm->ways[351899452];
+  g_assert_nonnull(w452);
+  g_assert_nonnull(w452->tags.get_value("source"));
+  g_assert_null(w452->tags.get_value("wheelchair"));
+  g_assert_cmpuint(w452->tags.asMap().size(), ==, 3);
+  const relation_t * const r255 = osm->relations[296255];
+  g_assert_cmpuint(r255->flags & OSM_FLAG_DIRTY, ==, OSM_FLAG_DIRTY);
+  g_assert_cmpuint(r255->members.size(), ==, 164);
+  const object_t r255m572(const_cast<node_t *>(n72));
+  std::vector<member_t>::const_iterator r255it = r255->find_member_object(r255m572);
+  r255it = r255->find_member_object(r255m572);
+  g_assert(r255it != r255->members.end());
+  g_assert(r255it->role != 0);
+  g_assert_cmpint(strcmp(r255it->role, "forward_stop"), ==, 0);
+  g_assert_cmpuint(r255->tags.asMap().size(), ==, 8);
+
+  xmlChar *rel_str = r255->generate_xml(42);
+  printf("%s\n", rel_str);
+  xmlFree(rel_str);
+
+  g_assert_false(diff_is_clean(osm, true));
+}
+
 int main(int argc, char **argv)
 {
+  int result = 0;
+
   if(argc != 3)
     return EINVAL;
 
@@ -53,68 +119,46 @@ int main(int argc, char **argv)
   project_t project(argv[2], argv[1]);
   diff_restore(O2G_NULLPTR, &project, osm);
 
-  g_assert_cmpuint(12, ==, osm->nodes.size());
-  g_assert_cmpuint(3, ==, osm->ways.size());
-  g_assert_cmpuint(3, ==, osm->relations.size());
+  verify_diff(osm);
 
-  // new tag added in diff
-  g_assert_nonnull(n72);
-  g_assert((n72->flags & OSM_FLAG_DIRTY) != 0);
-  g_assert_nonnull(n72->tags.get_value("testtag"));
-  g_assert_cmpuint(n72->tags.asMap().size(), ==, 5);
-  // in diff, but the same as in .osm
-  const node_t * const n23 = osm->nodes[3577031223LL];
-  g_assert_nonnull(n23);
-  g_assert((n23->flags & OSM_FLAG_DIRTY) == 0);
-  g_assert_true(n23->tags.empty());
-  // deleted in diff
-  const way_t * const w = osm->ways[351899455];
-  g_assert_nonnull(w);
-  g_assert((w->flags & OSM_FLAG_DELETED) != 0);
-  // added in diff
-  const node_t * const nn1 = osm->nodes[-1];
-  g_assert_nonnull(nn1);
-  g_assert_cmpfloat(nn1->pos.lat, ==, 52.2693518);
-  g_assert_cmpfloat(nn1->pos.lon, ==, 9.5760140);
-  g_assert_true(nn1->tags.empty());
-  // added in diff, same position as existing node
-  const node_t * const nn2 = osm->nodes[-2];
-  g_assert_nonnull(nn2);
-  g_assert_cmpfloat(nn2->pos.lat, ==, 52.269497);
-  g_assert_cmpfloat(nn2->pos.lon, ==, 9.5752223);
-  g_assert_true(nn2->tags.empty());
-  // which is this one
-  const node_t * const n27 = osm->nodes[3577031227LL];
-  g_assert_nonnull(n27);
-  g_assert((n27->flags & OSM_FLAG_DIRTY) == 0);
-  g_assert_cmpfloat(nn2->pos.lat, ==, n27->pos.lat);
-  g_assert_cmpfloat(nn2->pos.lon, ==, n27->pos.lon);
-  // the upstream version has "wheelchair", we have "source"
-  // our modification must survive
-  const way_t * const w452 = osm->ways[351899452];
-  g_assert_nonnull(w452);
-  g_assert_nonnull(w452->tags.get_value("source"));
-  g_assert_null(w452->tags.get_value("wheelchair"));
-  g_assert_cmpuint(w452->tags.asMap().size(), ==, 3);
-  g_assert_cmpuint(r255->flags & OSM_FLAG_DIRTY, ==, OSM_FLAG_DIRTY);
-  g_assert_cmpuint(r255->members.size(), ==, 164);
-  r255it = r255->find_member_object(r255m572);
-  g_assert(r255it != r255->members.end());
-  g_assert(r255it->role != 0);
-  g_assert_cmpint(strcmp(r255it->role, "forward_stop"), ==, 0);
-  g_assert_cmpuint(r255->tags.asMap().size(), ==, 8);
+  char tmpdir[] = "/tmp/osm2go-diff_restore-XXXXXX";
 
-  xmlChar *rel_str = r255->generate_xml(42);
-  printf("%s\n", rel_str);
-  xmlFree(rel_str);
+  if(mkdtemp(tmpdir) == O2G_NULLPTR) {
+    std::cerr << "cannot create temporary directory" << std::endl;
+    result = 1;
+  } else {
+    std::string bpath = tmpdir + std::string("/") + argv[2];
+    mkdir(bpath.c_str(), 0755);
+    bpath.erase(bpath.rfind('/') + 1);
+    project_t sproject(argv[2], bpath.c_str());
 
-  g_assert_false(diff_is_clean(osm, true));
+    diff_save(&sproject, osm);
+
+    delete osm;
+    osm = osm_t::parse(std::string(), osm_path.c_str(), &icons);
+    g_assert_nonnull(osm);
+
+    diff_restore(O2G_NULLPTR, &sproject, osm);
+
+    verify_diff(osm);
+
+    bpath += argv[2];
+    bpath += '/';
+    bpath += argv[2];
+    bpath += '.';
+    bpath += "diff";
+    unlink(bpath.c_str());
+    bpath.erase(bpath.rfind('/'));
+    rmdir(bpath.c_str());
+    bpath.erase(bpath.rfind('/'));
+    rmdir(bpath.c_str());
+  }
 
   delete osm;
 
   xmlCleanupParser();
 
-  return 0;
+  return result;
 }
 
 void main_ui_enable(appdata_t *)
