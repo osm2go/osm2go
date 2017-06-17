2eb3eed8e9b5c39844c312f2d89a68c5921adcaf
Rolf Eike Beer <eike@sf-mail.de>
2017-06-17 16:09:05 +0200

    let build_release-N900.sh extract the version number from CMakeLists.txt

--- build_release-N900.sh
+++ build_release-N900.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ $# -eq 0 -o $# -gt 2 ]; then
-	echo "Usage: $(basename ${0}) version [icons.tar]" >&2
+if [ $# -gt 1 ]; then
+	echo "Usage: $(basename ${0}) [icons.tar]" >&2
 	exit 1
 fi
 
 set -ex
 
-VERSION=${1}
+VERSION=$(sed -rn '/^project/s/.* VERSION +([^ ]+).*/\1/p' "$(dirname ${0})/CMakeLists.txt")
 TMPDIR=$(mktemp -d)
 
 mkdir ${TMPDIR}/osm2go-${VERSION}
@@ -15,8 +15,8 @@ mkdir ${TMPDIR}/osm2go-${VERSION}
 # create a copy of the source dir, as we need to run CMake inside it
 git archive --prefix=osm2go-${VERSION}/ ${VERSION} | tar x -C ${TMPDIR}
 
-if [ $# -eq 2 ]; then
-	tar -x -C ${TMPDIR}/osm2go-${VERSION} -f "${2}"
+if [ $# -eq 1 ]; then
+	tar -x -C ${TMPDIR}/osm2go-${VERSION} -f "${1}"
 	find ${TMPDIR}/osm2go-${VERSION}/data -name '*.png' -exec touch {} +
 fi
 
