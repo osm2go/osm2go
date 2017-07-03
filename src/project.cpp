osm2go/osm2go

commit 93a9c0e3dcfad2493ecbf1703c25897d4792b191

use std::string to store path in project_t

src/project.cpp
@@ -192,10 +192,10 @@ static bool project_read(const std::string &project_file, project_t *project,
 	      /* if the project path actually is a prefix of this, */
 	      /* then just remove this prefix */
 	      if((str[0] == '/') &&
-		 (strlen((char *)str) > strlen(project->path)) &&
-		 !strncmp((char *)str, project->path, strlen(project->path))) {
+		 (strlen((char *)str) > project->path.size()) &&
+		 !strncmp((char *)str, project->path.c_str(), project->path.size())) {
 
-		project->osm = g_strdup((gchar *)str + strlen(project->path));
+		project->osm = g_strdup((gchar *)str + project->path.size());
 		printf("osm name converted to relative %s\n", project->osm);
 	      } else
 		project->osm = g_strdup((gchar *)str);
@@ -226,11 +226,11 @@ gboolean project_save(GtkWidget *parent, project_t *project) {
   printf("saving project to %s\n", project_file.c_str());
 
   /* check if project path exists */
-  if(!g_file_test(project->path, G_FILE_TEST_IS_DIR)) {
+  if(!g_file_test(project->path.c_str(), G_FILE_TEST_IS_DIR)) {
     /* make sure project base path exists */
-    if(g_mkdir_with_parents(project->path, S_IRWXU) != 0) {
+    if(g_mkdir_with_parents(project->path.c_str(), S_IRWXU) != 0) {
       errorf(GTK_WIDGET(parent),
-	     _("Unable to create project path %s"), project->path);
+	     _("Unable to create project path %s"), project->path.c_str());
       return FALSE;
     }
   }
@@ -355,8 +355,7 @@ static gboolean osm_file_exists(const project_t *project) {
   if(project->name[0] == '/')
     return g_file_test(project->name, G_FILE_TEST_IS_REGULAR);
   else {
-    std::string full = project->path;
-    full += project->osm;
+    const std::string full = project->path + project->osm;
     return g_file_test(full.c_str(), G_FILE_TEST_IS_REGULAR);
   }
 }
@@ -484,19 +483,18 @@ static bool project_delete(select_context_t *context, project_t *project) {
   }
 
   /* remove entire directory from disk */
-  GDir *dir = g_dir_open(project->path, 0, NULL);
+  GDir *dir = g_dir_open(project->path.c_str(), 0, NULL);
   const gchar *name;
-  std::string path = project->path;
-  path += '/';
   std::string fullname;
+  fullname.resize(project->path.size() + strlen(project->name) + 8); // long enough for all usual project filenames
   while ((name = g_dir_read_name(dir))) {
-    fullname = path + name;
+    fullname = project->path + name;
     g_remove(fullname.c_str());
   }
   g_dir_close(dir);
 
   /* remove the projects directory */
-  g_remove(project->path);
+  g_remove(project->path.c_str());
 
   /* remove from view */
   GtkTreeIter iter;
@@ -1459,7 +1457,6 @@ osm_t *project_parse_osm(const project_t *project, struct icon_t **icons) {
 
 project_t::project_t(const gchar *n, const char *base_path)
   : name(g_strdup(n))
-  , path(g_strconcat(base_path, name, "/", NULL))
   , desc(0)
   , server(0)
   , rserver(0)
@@ -1468,6 +1465,7 @@ project_t::project_t(const gchar *n, const char *base_path)
   , wms_path(0)
   , map_state(0)
   , data_dirty(FALSE)
+  , path(std::string(base_path) +  name + '/')
 {
   memset(&wms_offset, 0, sizeof(wms_offset));
   memset(&min, 0, sizeof(min));
@@ -1483,7 +1481,6 @@ project_t::~project_t()
   g_free(wms_server);
   g_free(wms_path);
 
-  g_free(const_cast<gchar *>(path));
   g_free(osm);
 
   map_state_free(map_state);

src/wms.cpp
@@ -1264,17 +1264,19 @@ static const char *wms_exts[] = { "png", "gif", "jpg", NULL };
 /* try to load an existing image into map */
 void wms_load(appdata_t *appdata) {
   int i;
-  gchar *filename = g_strjoin("/wms.", appdata->project->path, DUMMYEXT, NULL);
-  gchar *ext = filename + strlen(filename) - strlen(DUMMYEXT);
+  std::string filename = appdata->project->path + "/wms.";
+  filename.resize(filename.size() + strlen(DUMMYEXT));
+  const std::string::size_type extpos = filename.size();
 
   for(i = 0; wms_exts[i]; i++) {
-    memcpy(ext, wms_exts[i], strlen(wms_exts[i]));
+    filename.erase(extpos);
+    filename += wms_exts[i];
 
-    if(g_file_test(filename, G_FILE_TEST_EXISTS)) {
+    if(g_file_test(filename.c_str(), G_FILE_TEST_EXISTS)) {
       appdata->map->bg.offset.x = appdata->project->wms_offset.x;
       appdata->map->bg.offset.y = appdata->project->wms_offset.y;
 
-      map_set_bg_image(appdata->map, (char*)filename);
+      map_set_bg_image(appdata->map, filename.c_str());
 
       /* restore image to saved position */
       gint x = appdata->osm->bounds->min.x + appdata->map->bg.offset.x;
@@ -1288,22 +1290,21 @@ void wms_load(appdata_t *appdata) {
       break;
     }
   }
-  g_free(filename);
 }
 
 void wms_remove_file(project_t *project) {
   int i;
-  gchar *filename = g_strjoin("/wms.", project->path, DUMMYEXT, NULL);
-  gchar *ext = filename + strlen(filename) - strlen(DUMMYEXT);
+  std::string filename = project->path + "/wms.";
+  filename.resize(filename.size() + strlen(DUMMYEXT));
+  const std::string::size_type extpos = filename.size();
 
   for(i = 0; wms_exts[i]; i++) {
-    memcpy(ext, wms_exts[i], strlen(wms_exts[i]));
+    filename.erase(extpos);
+    filename += wms_exts[i];
 
-    if(g_file_test(filename, G_FILE_TEST_EXISTS))
-      g_remove(filename);
+    if(g_file_test(filename.c_str(), G_FILE_TEST_EXISTS))
+      g_remove(filename.c_str());
   }
-
-  g_free(filename);
 }
 
 void wms_remove(appdata_t *appdata) {

src/diff.cpp
@@ -258,7 +258,7 @@ void diff_save(const project_t *project, const osm_t *osm) {
 
   /* write the diff to a new file so the original one needs intact until
    * saving is completed */
-  char *ndiff = g_strconcat(project->path, "save.diff", NULL);
+  const std::string ndiff = project->path + "save.diff";
 
   xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
   xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "diff");
@@ -269,14 +269,12 @@ void diff_save(const project_t *project, const osm_t *osm) {
   std::for_each(osm->ways.begin(), osm->ways.end(), diff_save_ways(root_node));
   std::for_each(osm->relations.begin(), osm->relations.end(), diff_save_relations(root_node));
 
-  xmlSaveFormatFileEnc(ndiff, doc, "UTF-8", 1);
+  xmlSaveFormatFileEnc(ndiff.c_str(), doc, "UTF-8", 1);
   xmlFreeDoc(doc);
 
   /* if we reach this point writing the new file worked and we */
   /* can delete the backup */
-  g_rename(ndiff, diff_name.c_str());
-
-  g_free(ndiff);
+  g_rename(ndiff.c_str(), diff_name.c_str());
 }
 
 static item_id_t xml_get_prop_int(xmlNode *node, const char *prop, item_id_t def) {

src/osm.cpp
@@ -1038,7 +1038,7 @@ static osm_t *process_file(const char *filename) {
 
 #include <sys/time.h>
 
-osm_t *osm_parse(const char *path, const char *filename, icon_t **icon) {
+osm_t *osm_parse(const std::string &path, const char *filename, icon_t **icon) {
 
   struct timeval start;
   gettimeofday(&start, NULL);
@@ -1048,9 +1048,8 @@ osm_t *osm_parse(const char *path, const char *filename, icon_t **icon) {
   if(filename[0] == '/')
     osm = process_file(filename);
   else {
-    char *full = g_strconcat(path, filename, NULL);
-    osm = process_file(full);
-    g_free(full);
+    const std::string full = path + filename;
+    osm = process_file(full.c_str());
   }
 
   struct timeval end;

src/osm.h
@@ -177,7 +177,6 @@ struct osm_t {
 #endif
 };
 
-osm_t *osm_parse(const char *path, const char *filename, struct icon_t **icons);
 gboolean osm_sanity_check(GtkWidget *parent, const osm_t *osm);
 void osm_free(osm_t *osm);
 
@@ -423,6 +422,7 @@ public:
   void cleanup();
 };
 
+osm_t *osm_parse(const std::string &path, const char *filename, struct icon_t **icons);
 /**
  * @brief parse the XML node for tag values
  * @param a_node the XML node to parse

src/osm_api.cpp
@@ -135,29 +135,26 @@ gboolean osm_download(GtkWidget *parent, settings_t *settings,
 
   /* Download the new file to a new name. If something goes wrong then the
    * old file will still be in place to be opened. */
-  gchar *update = g_strconcat(project->path, "update.osm", NULL);
-  g_remove(update);
+  const std::string update = project->path + "update.osm";
+  g_remove(update.c_str());
 
-  gboolean result = net_io_download_file(parent, settings, url, update,
+  gboolean result = net_io_download_file(parent, settings, url, update.c_str(),
                                          project->name);
   g_free(url);
 
   /* if there's a new file use this from now on */
-  if(result && g_file_test(update, G_FILE_TEST_IS_REGULAR)) {
+  if(result && g_file_test(update.c_str(), G_FILE_TEST_IS_REGULAR)) {
     printf("download ok, replacing previous file\n");
 
     if(project->osm[0] == '/') {
-      g_rename(update, project->osm);
+      g_rename(update.c_str(), project->osm);
     } else {
-      gchar *fname = g_strconcat(project->path, project->osm, NULL);
-      g_rename(update, fname);
-      g_free(fname);
+      const std::string fname = project->path + project->osm;
+      g_rename(update.c_str(), fname.c_str());
     }
 
     result = TRUE;
   }
-
-  g_free(update);
 
   return result;
 }

src/project.h
@@ -37,7 +37,6 @@ typedef struct project_t {
 #endif
 
   const char * const name;
-  const char * const path;
 
   xmlChar *desc;
   const char *server; /**< the server string used, either rserver or settings->server */
@@ -53,6 +52,9 @@ typedef struct project_t {
   pos_t min, max;
 
   gboolean data_dirty;     /* needs to download new data */
+#ifdef __cplusplus
+  const std::string path;
+#endif
 } project_t;
 
 #ifdef __cplusplus

src/track.cpp
@@ -271,7 +271,7 @@ void track_save(project_t *project, track_t *track) {
     return;
   }
 
-  gchar *trk_name = g_strconcat(project->path, project->name, ".trk", NULL);
+  gchar *trk_name = g_strconcat(project->path.c_str(), project->name, ".trk", NULL);
 
   if(!track) {
     g_remove(trk_name);
@@ -281,14 +281,14 @@ void track_save(project_t *project, track_t *track) {
 
   /* check if there already is such a diff file and make it a backup */
   /* in case new diff saving fails */
-  char *backup = g_strconcat(project->path, "backup.trk", NULL);
+  const std::string backup = project->path + "backup.trk";
   xmlDocPtr doc = NULL;
   if(g_file_test(trk_name, G_FILE_TEST_IS_REGULAR)) {
-    printf("backing up existing file \"%s\" to \"%s\"\n", trk_name, backup);
-    g_remove(backup);
-    g_rename(trk_name, backup);
+    printf("backing up existing file \"%s\" to \"%s\"\n", trk_name, backup.c_str());
+    g_remove(backup.c_str());
+    g_rename(trk_name, backup.c_str());
     /* parse the old file and get the DOM */
-    doc = xmlReadFile(backup, NULL, 0);
+    doc = xmlReadFile(backup.c_str(), NULL, 0);
   }
 
   track_write(trk_name, track, doc);
@@ -296,10 +296,9 @@ void track_save(project_t *project, track_t *track) {
 
   /* if we reach this point writing the new file worked and we */
   /* can delete the backup */
-  g_remove(backup);
+  g_remove(backup.c_str());
 
   g_free(trk_name);
-  g_free(backup);
 }
 
 void track_export(const track_t *track, const char *filename) {

commit 8290f842adfa46febb47ec072ab1d4ab5e3d2c86

use reserve() instead of resize() where appropiate
Drop 2 needless incarnations that actually broke functionality.

Broken in 93a9c0e3dcfad2493ecbf1703c25897d4792b191.

src/project.cpp
@@ -483,7 +483,7 @@ static bool project_delete(select_context_t *context, project_t *project) {
   GDir *dir = g_dir_open(project->path.c_str(), 0, O2G_NULLPTR);
   const gchar *name;
   std::string fullname;
-  fullname.resize(project->path.size() + project->name.size() + 8); // long enough for all usual project filenames
+  fullname.reserve(project->path.size() + project->name.size() + 8); // long enough for all usual project filenames
   while ((name = g_dir_read_name(dir))) {
     fullname = project->path + name;
     g_remove(fullname.c_str());

src/wms.cpp
@@ -1070,7 +1070,7 @@ void wms_import(appdata_t *appdata) {
                             (wms.path.find('?') != std::string::npos ? "&" : "?");
 
   std::string url;
-  url.resize(256); // make enough room that most URLs will need no reallocation
+  url.reserve(256); // make enough room that most URLs will need no reallocation
   url = wms.server + wms.path + append_char +
                                  "SERVICE=wms"
                                  "&VERSION=1.1.1"
@@ -1237,14 +1237,11 @@ void wms_import(appdata_t *appdata) {
 }
 
 static const char *wms_exts[] = { "png", "gif", "jpg", O2G_NULLPTR };
-/* this must be the longest one */
-#define DUMMYEXT wms_exts[0]
 
 /* try to load an existing image into map */
 void wms_load(appdata_t *appdata) {
   int i;
   std::string filename = appdata->project->path + "/wms.";
-  filename.resize(filename.size() + strlen(DUMMYEXT));
   const std::string::size_type extpos = filename.size();
 
   for(i = 0; wms_exts[i]; i++) {
@@ -1274,7 +1271,6 @@ void wms_load(appdata_t *appdata) {
 void wms_remove_file(project_t *project) {
   int i;
   std::string filename = project->path + "/wms.";
-  filename.resize(filename.size() + strlen(DUMMYEXT));
   const std::string::size_type extpos = filename.size();
 
   for(i = 0; wms_exts[i]; i++) {