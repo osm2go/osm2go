0b0b48316175657471f9c53f51fe946c0827d80c
Rolf Eike Beer <eike@sf-mail.de>
2017-09-30 23:13:51 +0200

    tests: check searching item at position

--- test/canvas_points.cpp
+++ test/canvas_points.cpp
@@ -34,11 +34,46 @@ bool testSegment()
   return ret;
 }
 
+bool testInObject()
+{
+  bool ret = true;
+
+  canvas_t * const canvas = canvas_t::create();
+  g_assert_nonnull(canvas);
+
+  canvas_points_t * const points = canvas_points_t::create(5);
+  g_assert_nonnull(points);
+
+  points->coords[0] = 0;
+  points->coords[1] = 200;
+  points->coords[2] = 200;
+  points->coords[3] = 400;
+  points->coords[4] = 400;
+  points->coords[5] = 200;
+  points->coords[6] = 200;
+  points->coords[7] = 0;
+  points->coords[8] = points->coords[0];
+  points->coords[9] = points->coords[1];
+
+  canvas_item_t * const line = canvas->polygon_new(CANVAS_GROUP_WAYS, points, 1, 0, 0);
+  g_assert_nonnull(line);
+
+  canvas_item_t *search = canvas->get_item_at(200, 200);
+  g_assert(line == search);
+
+  search = canvas->get_item_at(40, 50);
+  g_assert_null(search);
+
+  return ret;
+}
+
 int main()
 {
   bool ret = true;
 
   ret &= testSegment();
+
+  ret &= testInObject();
 
   return ret ? 0 : 1;
 }
