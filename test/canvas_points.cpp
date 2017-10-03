5d8ae87ae9dc93bf602c091d649a6600894b233d
Rolf Eike Beer <eike@sf-mail.de>
2017-10-03 12:49:34 +0200

    tests: use proper test macro to check unique_ptr contents

--- test/canvas_points.cpp
+++ test/canvas_points.cpp
@@ -11,16 +11,16 @@ bool testSegment()
   bool ret = true;
 
   std::unique_ptr<canvas_points_t> points(canvas_points_t::create(4));
-  g_assert_nonnull(points);
+  g_assert_true(points);
 
   for (unsigned int i = 0; i < 8; i++)
     points->coords()[i] = 1 << i;
 
   std::unique_ptr<canvas_t> canvas(canvas_t::create());
-  g_assert_nonnull(canvas);
+  g_assert_true(canvas);
 
   std::unique_ptr<canvas_item_t> line(canvas->polyline_new(CANVAS_GROUP_WAYS, points.get(), 1, 0));
-  g_assert_nonnull(line);
+  g_assert_true(line);
 
   std::unique_ptr<canvas_points_t> seg(line->get_segment(1));
   g_assert_true(seg);
@@ -37,10 +37,10 @@ bool testInObject()
   bool ret = true;
 
   std::unique_ptr<canvas_t> canvas(canvas_t::create());
-  g_assert_nonnull(canvas);
+  g_assert_true(canvas);
 
   std::unique_ptr<canvas_points_t> points(canvas_points_t::create(5));
-  g_assert_nonnull(points);
+  g_assert_true(points);
 
   points->coords()[0] = 0;
   points->coords()[1] = 200;
@@ -54,7 +54,7 @@ bool testInObject()
   points->coords()[9] = points->coords()[1];
 
   std::unique_ptr<canvas_item_t> line(canvas->polygon_new(CANVAS_GROUP_WAYS, points.get(), 1, 0, 0));
-  g_assert_nonnull(line);
+  g_assert_true(line);
 
   canvas_item_t *search = canvas->get_item_at(200, 200);
   g_assert(line.get() == search);
