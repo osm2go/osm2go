osm2go/osm2go

commit 13cc2e95dd334b8f1f1f14cce4a996be3efee1fd

remove appdata_t from info and relation_edit interfaces

src/info.cpp
@@ -19,7 +19,6 @@
 
 #include "info.h"
 
-#include "appdata.h"
 #include "josm_presets.h"
 #include "list.h"
 #include "map.h"
@@ -303,8 +302,8 @@ static bool replace_with_last(const tag_context_t *context, const osm_t::TagMap 
 
 static void on_tag_last(tag_context_t *context) {
   const osm_t::TagMap &ntags = context->object.type == NODE ?
-                               context->appdata.map->last_node_tags :
-                               context->appdata.map->last_way_tags;
+                               context->map->last_node_tags :
+                               context->map->last_way_tags;
 
   if(!replace_with_last(context, ntags))
     return;
@@ -373,8 +372,8 @@ void tag_context_t::info_tags_replace() {
 }
 
 static void on_relations(tag_context_t *context) {
-  relation_membership_dialog(context->dialog, context->appdata.presets,
-                             context->appdata.osm, context->object);
+  relation_membership_dialog(context->dialog, context->presets,
+                             context->osm, context->object);
 }
 
 static GtkWidget *tag_widget(tag_context_t *context) {
@@ -398,17 +397,17 @@ static GtkWidget *tag_widget(tag_context_t *context) {
                            buttons, columns, context->store);
 
   list_set_custom_user_button(context->list, LIST_BUTTON_USER1,
-                              josm_build_presets_button(context->appdata.icons,
-                                                        context->appdata.presets, context));
-  if(!context->appdata.presets)
+                              josm_build_presets_button(context->osm->icons,
+                                                        context->presets, context));
+  if(likely(context->presets != O2G_NULLPTR))
     list_button_enable(context->list, LIST_BUTTON_USER1, FALSE);
 
   /* disable if no appropriate "last" tags have been stored or if the */
   /* selected item isn't a node or way */
   if(((context->object.type == NODE) &&
-      (context->appdata.map->last_node_tags.empty())) ||
+      (context->map->last_node_tags.empty())) ||
      ((context->object.type == WAY) &&
-      (context->appdata.map->last_way_tags.empty())) ||
+      (context->map->last_way_tags.empty())) ||
      ((context->object.type != NODE) && (context->object.type != WAY)))
 	list_button_enable(context->list, LIST_BUTTON_USER0, FALSE);
 
@@ -432,11 +431,11 @@ static void table_attach(GtkWidget *table, GtkWidget *child, int x, int y) {
 static GtkWidget *details_widget(const tag_context_t &context, bool big) {
   GtkWidget *table = gtk_table_new(big?4:2, 2, FALSE);  // y, x
 
-  const std::map<int, std::string>::const_iterator userIt = context.appdata.osm->users.find(context.object.obj->user);
+  const std::map<int, std::string>::const_iterator userIt = context.osm->users.find(context.object.obj->user);
   GtkWidget *label;
 
   /* ------------ user ----------------- */
-  if(userIt != context.appdata.osm->users.end()) {
+  if(userIt != context.osm->users.end()) {
     if(big) table_attach(table, gtk_label_new(_("User:")), 0, 0);
 
     label = gtk_label_new(userIt->second.c_str());
@@ -547,23 +546,23 @@ static void info_more(const tag_context_t &context) {
 
 /* edit tags of currently selected node or way or of the relation */
 /* given */
-void info_dialog(GtkWidget *parent, appdata_t &appdata) {
-  bool ret = info_dialog(parent, appdata, appdata.map->selected.object);
+void info_dialog(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets) {
+  bool ret = info_dialog(parent, map, osm, presets, map->selected.object);
 
   /* since nodes being parts of ways but with no tags are invisible, */
   /* the result of editing them may have changed their visibility */
-  if(ret && appdata.map->selected.object.type != RELATION)
-    appdata.map->redraw_item(appdata.map->selected.object);
+  if(ret && map->selected.object.type != RELATION)
+    map->redraw_item(map->selected.object);
 }
 
 /* edit tags of currently selected node or way or of the relation */
 /* given */
-bool info_dialog(GtkWidget *parent, appdata_t &appdata, object_t &object) {
+bool info_dialog(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets, object_t &object) {
 
   assert(object.is_real());
 
   /* use implicit selection if not explicitely given */
-  tag_context_t context(appdata, object);
+  tag_context_t context(map, osm, presets, object);
   char *str = O2G_NULLPTR;
 
   switch(context.object.type) {
@@ -644,8 +643,10 @@ bool info_dialog(GtkWidget *parent, appdata_t &appdata, object_t &object) {
   return ok;
 }
 
-tag_context_t::tag_context_t(appdata_t &a, const object_t &o)
-  : appdata(a)
+tag_context_t::tag_context_t(map_t *m, osm_t *os, presets_items *p, const object_t &o)
+  : map(m)
+  , osm(os)
+  , presets(p)
   , dialog(O2G_NULLPTR)
   , list(O2G_NULLPTR)
   , store(O2G_NULLPTR)

src/iconbar.cpp
@@ -81,7 +81,7 @@ public:
 };
 
 static void on_info_clicked(appdata_t *appdata) {
-  info_dialog(appdata->window, *appdata);
+  info_dialog(appdata->window, appdata->map, appdata->osm, appdata->presets);
 }
 
 static void on_node_add_clicked(map_t *map) {

src/info.h
@@ -26,14 +26,17 @@
 #include <string>
 #include <vector>
 
-struct appdata_t;
+class map_t;
+struct presets_items;
 
 class tag_context_t {
 public:
-  explicit tag_context_t(appdata_t &a, const object_t &o);
+  explicit tag_context_t(map_t *m, osm_t *os, presets_items *p, const object_t &o);
   ~tag_context_t();
 
-  appdata_t &appdata;
+  map_t * const map;
+  osm_t * const osm;
+  presets_items * const presets;
   GtkWidget *dialog, *list;
   GtkListStore *store;
   object_t object;
@@ -43,7 +46,7 @@ public:
   void update_collisions(const std::string &k);
 };
 
-void info_dialog(GtkWidget *parent, appdata_t &appdata);
-bool info_dialog(GtkWidget *parent, appdata_t &appdata, object_t &object);
+void info_dialog(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets);
+bool info_dialog(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets, object_t &object);
 
 #endif // INFO_H

src/main.cpp
@@ -323,7 +323,7 @@ cb_menu_undo_changes(appdata_t *appdata) {
 static void
 cb_menu_osm_relations(appdata_t *appdata) {
   /* list relations of all objects */
-  relation_list(appdata->window, *appdata);
+  relation_list(appdata->window, appdata->map, appdata->osm, appdata->presets);
 }
 
 #ifndef FREMANTLE
@@ -524,7 +524,6 @@ cb_menu_track_export(appdata_t *appdata) {
 #define uispecific_main_menu_new gtk_menu_new
 
 #endif
-
 
 #ifndef FREMANTLE
 // Half-arsed slapdash common menu item constructor. Let's use GtkBuilder

src/map.cpp
@@ -1287,7 +1287,7 @@ static void map_button_release(map_t *map, int x, int y) {
       map_node_select(map, node);
 
       /* let the user specify some tags for the new node */
-      info_dialog(map->appdata.window, map->appdata);
+      info_dialog(map->appdata.window, map, map->appdata.osm, map->appdata.presets);
     }
     break;
   }
@@ -1439,7 +1439,7 @@ bool map_t::key_press_event(unsigned int keyval) {
       map_action_ok(this);
     /* otherwise if info is enabled call that */
     else if(appdata.iconbar->isInfoEnabled())
-      info_dialog(appdata.window, appdata);
+      info_dialog(appdata.window, this, appdata.osm, appdata.presets);
     break;
 
   case GDK_Escape:   // same as HILDON_HARDKEY_ESC
@@ -1765,7 +1765,7 @@ void map_action_ok(map_t *map) {
       map_node_select(map, node);
 
       /* let the user specify some tags for the new node */
-      info_dialog(map->appdata.window, map->appdata);
+      info_dialog(map->appdata.window, map, map->appdata.osm, map->appdata.presets);
     }
     }
 

src/map_edit.cpp
@@ -275,7 +275,7 @@ void map_edit_way_add_ok(map_t *map) {
   map->action.way = O2G_NULLPTR;
 
   /* let the user specify some tags for the new way */
-  info_dialog(map->appdata.window, map->appdata);
+  info_dialog(map->appdata.window, map, map->appdata.osm, map->appdata.presets);
 }
 
 /* -------------------------- way_node_add ----------------------- */

src/relation_edit.cpp
@@ -19,7 +19,6 @@
 
 #include "relation_edit.h"
 
-#include "appdata.h"
 #include "info.h"
 #include "josm_presets.h"
 #include "list.h"
@@ -74,9 +73,21 @@ relitem_context_t::relitem_context_t(object_t &o, const presets_items *pr, osm_t
 struct entry_insert_text {
   GtkWidget * const entry;
   explicit entry_insert_text(GtkWidget *en) : entry(en) {}
-  void operator()(const std::string &role) {
+  inline void operator()(const std::string &role) {
     combo_box_append_text(entry, role.c_str());
   }
+};
+
+struct relation_context_t {
+  inline relation_context_t(map_t *m, osm_t *o, presets_items *p, GtkWidget *d)
+    : map(m), osm(o), presets(p), dialog(d) {}
+
+  map_t * const map;
+  osm_t * const osm;
+  presets_items * const presets;
+  GtkWidget * const dialog;
+  GtkWidget *list, *show_btn;
+  GtkListStore *store;
 };
 
 static bool relation_add_item(GtkWidget *parent, relation_t *relation,
@@ -183,10 +194,9 @@ static void relation_remove_item(relation_t *relation, const object_t &object) {
   relation->flags |= OSM_FLAG_DIRTY;
 }
 
-static gboolean relation_info_dialog(GtkWidget *parent, appdata_t &appdata,
-				     relation_t *relation) {
+static bool relation_info_dialog(relation_context_t *context, relation_t *relation) {
   object_t object(relation);
-  return info_dialog(parent, appdata, object);
+  return info_dialog(context->dialog, context->map, context->osm, context->presets, object);
 }
 
 static void changed(GtkTreeSelection *sel, relitem_context_t *context) {
@@ -434,16 +444,6 @@ void relation_membership_dialog(GtkWidget *parent, const presets_items *presets,
 
 /* -------------------- global relation list ----------------- */
 
-struct relation_context_t {
-  relation_context_t(appdata_t &a, GtkWidget *d)
-    : appdata(a), dialog(d) {}
-
-  appdata_t &appdata;
-  GtkWidget * const dialog;
-  GtkWidget *list, *show_btn;
-  GtkListStore *store;
-};
-
 enum {
   RELATION_COL_TYPE = 0,
   RELATION_COL_NAME,
@@ -681,10 +681,10 @@ static void on_relation_members(relation_context_t *context) {
 /* user clicked "select" button in relation list */
 static void on_relation_select(relation_context_t *context, GtkWidget *but) {
   relation_t *sel = get_selected_relation(context);
-  context->appdata.map->item_deselect();
+  context->map->item_deselect();
 
   if(sel) {
-    context->appdata.map->select_relation(sel);
+    context->map->select_relation(sel);
 
     /* tell dialog to close as we want to see the selected relation */
 
@@ -701,12 +701,12 @@ static void on_relation_add(relation_context_t *context) {
   /* create a new relation */
 
   relation_t *relation = new relation_t(0);
-  if(!relation_info_dialog(context->dialog, context->appdata, relation)) {
+  if(!relation_info_dialog(context, relation)) {
     printf("tag edit cancelled\n");
     relation->cleanup();
     delete relation;
   } else {
-    context->appdata.osm->relation_attach(relation);
+    context->osm->relation_attach(relation);
 
     /* append a row for the new data */
 
@@ -734,7 +734,7 @@ static void on_relation_edit(relation_context_t *context) {
 
   printf("edit relation #" ITEM_ID_FORMAT "\n", sel->id);
 
-  if (!relation_info_dialog(context->dialog, context->appdata, sel))
+  if (!relation_info_dialog(context, sel))
     return;
 
   // Locate the changed item
@@ -785,7 +785,7 @@ static void on_relation_remove(relation_context_t *context) {
     gtk_list_store_remove(context->store, &iter);
 
   /* then really delete it */
-  context->appdata.osm->relation_delete(sel);
+  context->osm->relation_delete(sel);
 
   relation_list_selected(context->list, O2G_NULLPTR);
 }
@@ -846,7 +846,7 @@ static GtkWidget *relation_list_widget(relation_context_t &context) {
 
   relation_list_widget_functor fc(context.store);
 
-  const std::map<item_id_t, relation_t *> &rchain = context.appdata.osm->relations;
+  const std::map<item_id_t, relation_t *> &rchain = context.osm->relations;
   std::for_each(rchain.begin(), rchain.end(), fc);
 
   g_object_unref(context.store);
@@ -857,8 +857,8 @@ static GtkWidget *relation_list_widget(relation_context_t &context) {
 }
 
 /* a global view on all relations */
-void relation_list(GtkWidget *parent, appdata_t &appdata) {
-  relation_context_t context(appdata,
+void relation_list(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets) {
+  relation_context_t context(map, osm, presets,
                      misc_dialog_new(MISC_DIALOG_LARGE, _("All relations"),
                                      GTK_WINDOW(parent),
                                      GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE,

src/relation_edit.h
@@ -22,7 +22,7 @@
 
 #include <gtk/gtk.h>
 
-struct appdata_t;
+class map_t;
 struct object_t;
 struct osm_t;
 struct presets_items;
@@ -31,7 +31,7 @@ class relation_t;
 void relation_membership_dialog(GtkWidget *parent, const presets_items *presets,
                                 osm_t *osm, object_t &object);
 
-void relation_list(GtkWidget *parent, appdata_t &appdata);
+void relation_list(GtkWidget *parent, map_t *map, osm_t *osm, presets_items *presets);
 void relation_show_members(GtkWidget *parent, const relation_t *relation);
 
 #endif // RELATION_EDIT_H

commit 877b7822ba287dadce20088fd234159c73760484

fix inverted logic for presets button availability
Fixes: 13cc2e95dd334b8f1f1f14cce4a996be3efee1fd

src/info.cpp
@@ -399,7 +399,7 @@ static GtkWidget *tag_widget(tag_context_t *context) {
   list_set_custom_user_button(context->list, LIST_BUTTON_USER1,
                               josm_build_presets_button(context->osm->icons,
                                                         context->presets, context));
-  if(likely(context->presets != O2G_NULLPTR))
+  if(unlikely(context->presets == O2G_NULLPTR))
     list_button_enable(context->list, LIST_BUTTON_USER1, FALSE);
 
   /* disable if no appropriate "last" tags have been stored or if the */