osm2go/osm2go

commit 29476eb4a61f9870f501214f2297810066eed19b

make direct GPSd interface consistent with liblocation one
Always return an invalid position if GPS tracking is disabled. With this
knowledge simplify area_edit.

src/gps.cpp
@@ -112,13 +112,15 @@ pos_t gpsd_state_t::get_pos(float* alt)
 {
   pos_t pos(NAN, NAN);
 
-  g_mutex_lock(mutex);
-  if(gpsdata.set & STATUS_SET) {
-    if(gpsdata.status != STATUS_NO_FIX) {
-      if(gpsdata.set & LATLON_SET)
-        pos = gpsdata.fix.pos;
-      if(alt && gpsdata.set & ALTITUDE_SET)
-        *alt = gpsdata.fix.alt;
+  if(enable) {
+    g_mutex_lock(mutex);
+    if(gpsdata.set & STATUS_SET) {
+      if(gpsdata.status != STATUS_NO_FIX) {
+        if(gpsdata.set & LATLON_SET)
+          pos = gpsdata.fix.pos;
+        if(alt && gpsdata.set & ALTITUDE_SET)
+          *alt = gpsdata.fix.alt;
+      }
     }
   }
 

src/area_edit.cpp
@@ -22,7 +22,6 @@
 #include "appdata.h"
 #include "gps.h"
 #include "misc.h"
-#include "settings.h"
 
 #ifdef ENABLE_OSM_GPS_MAP
 #include "osm-gps-map.h"
@@ -677,9 +676,7 @@ static void on_page_switch(GtkNotebook *nb, GtkWidget *pg, guint, context_t *con
 static gboolean map_gps_update(gpointer data) {
   context_t *context = static_cast<context_t *>(data);
 
-  pos_t pos(NAN, NAN);
-  if(context->area.appdata.settings->enable_gps)
-    pos = context->area.appdata.gps_state->get_pos();
+  pos_t pos = context->area.appdata.gps_state->get_pos();
 
   if(pos.valid()) {
     g_object_set(context->map.widget, "gps-track-highlight-radius", 0, O2G_NULLPTR);

src/gps.h
@@ -50,10 +50,13 @@ public:
    * In case no valid position was received an invalid pos_t will be returned.
    *
    * If @alt was given but no altitude was received the value is set to NAN.
+   *
+   * This returns an invalid position if GPS tracking is disabled.
    */
   virtual pos_t get_pos(float *alt = O2G_NULLPTR) = 0;
 
   virtual void setEnable(bool en) = 0;
+
   /**
    * @brief register or clear the GPS callback
    * @param cb the new callback function, set to NULL to unregister

commit 7387beda06299df51f8830453524833271f031c6

fix locking in GPSd
Fixes: 29476eb4a61f9870f501214f2297810066eed19b

src/gps.cpp
@@ -122,9 +122,8 @@ pos_t gpsd_state_t::get_pos(float* alt)
           *alt = gpsdata.fix.alt;
       }
     }
+    g_mutex_unlock(mutex);
   }
-
-  g_mutex_unlock(mutex);
 
   return pos;
 }