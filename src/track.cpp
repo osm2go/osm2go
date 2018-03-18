osm2go/osm2go

commit ab9f0db7f981582972d0c792f8e37f76334f8c4a

make project_t own the osm_t instance
Clean up some APIs to make use of this.

src/track.cpp
@@ -453,7 +453,7 @@ static int update(void *data) {
 
   /* ignore updates while no valid osm file is loaded, e.g. when switching */
   /* projects */
-  if(unlikely(!appdata.osm))
+  if(unlikely(appdata.project->osm == O2G_NULLPTR))
     return 1;
 
   /* the map is only gone of the main screen is being closed */
@@ -477,7 +477,7 @@ static int update(void *data) {
   if(pos.valid()) {
     printf("valid position %.6f/%.6f alt %.2f\n", pos.lat, pos.lon, alt);
     lpos_t lpos;
-    lpos = pos.toLpos(appdata.osm->bounds);
+    lpos = pos.toLpos(appdata.project->osm->bounds);
     if(track_append_position(appdata, pos, alt, lpos) && settings->trackVisibility >= ShowPosition)
       appdata.map->track_pos(lpos);
   } else {

src/appdata.h
@@ -55,7 +55,6 @@ struct appdata_t {
 
   map_state_t &map_state;
   map_t *map;
-  osm_t *osm;
   icon_t &icons;
   style_t *style;
   gps_state_t * const gps_state;

src/diff.cpp
@@ -166,12 +166,13 @@ void diff_save_relations::operator()(const std::pair<item_id_t, relation_t *> pa
   diff_save_tags(relation, node_rel);
 }
 
-void diff_save(const project_t *project, const osm_t *osm) {
-  if(unlikely(osm == O2G_NULLPTR))
+void diff_save(const project_t *project) {
+  if(unlikely(project->osm == O2G_NULLPTR))
     return;
 
   const std::string &diff_name = diff_filename(project);
 
+  osm_t *osm = project->osm;
   if(osm->is_clean(true)) {
     printf("data set is clean, removing diff if present\n");
     unlinkat(project->dirfd, diff_name.c_str(), 0);
@@ -503,8 +504,9 @@ static void diff_restore_relation(xmlNodePtr node_rel, osm_t *osm) {
   }
 }
 
-unsigned int diff_restore_file(const project_t *project, osm_t *osm) {
+unsigned int diff_restore_file(const project_t *project) {
   struct stat st;
+  osm_t * const osm = project->osm;
 
   /* first try to open a backup which is only present if saving the */
   /* actual diff didn't succeed */
@@ -586,10 +588,10 @@ unsigned int diff_restore_file(const project_t *project, osm_t *osm) {
 }
 
 void diff_restore(appdata_t &appdata) {
-  if(unlikely(!appdata.osm))
+  if(unlikely(appdata.project->osm == O2G_NULLPTR))
     return;
 
-  unsigned int flags = diff_restore_file(appdata.project, appdata.osm);
+  unsigned int flags = diff_restore_file(appdata.project);
   if(flags & DIFF_HAS_HIDDEN) {
     printf("hidden flags have been restored, enable show_add menu\n");
 

src/diff.h
@@ -38,8 +38,8 @@ enum diff_restore_results {
   DIFF_HAS_HIDDEN = (1 << 5), ///< some of the object have the hidden flag set
 };
 
-void diff_save(const project_t *project, const osm_t *osm) __attribute__((nonnull(1)));
-unsigned int diff_restore_file(const project_t *project, osm_t *osm) __attribute__((nonnull(1,2)));
+void diff_save(const project_t *project) __attribute__((nonnull(1)));
+unsigned int diff_restore_file(const project_t *project) __attribute__((nonnull(1)));
 void diff_restore(appdata_t &appdata);
 bool diff_present(const project_t *project);
 void diff_remove(const project_t *project);

src/map.cpp
@@ -649,14 +649,15 @@ template<bool b> void free_track_item_chain(track_seg_t &seg) {
 }
 
 static void map_free_map_item_chains(appdata_t &appdata) {
-  if(unlikely(!appdata.osm))
+  osm_t * const osm = appdata.project->osm;
+  if(unlikely(osm == O2G_NULLPTR))
     return;
 
   /* free all map_item_chains */
-  std::for_each(appdata.osm->nodes.begin(), appdata.osm->nodes.end(),
+  std::for_each(osm->nodes.begin(), osm->nodes.end(),
                 free_map_item_chain);
 
-  std::for_each(appdata.osm->ways.begin(), appdata.osm->ways.end(),
+  std::for_each(osm->ways.begin(), osm->ways.end(),
                 free_map_item_chain);
 
   if (appdata.track.track) {
@@ -750,7 +751,7 @@ static void map_limit_scroll(map_t *map, canvas_t::canvas_unit_t unit, int &sx, 
 
   // Data rect minimum and maximum
   // limit stops - prevent scrolling beyond these
-  const bounds_t &bounds = map->appdata.osm->bounds;
+  const bounds_t &bounds = map->appdata.project->osm->bounds;
   int min_sy_cu = 0.95 * (bounds.min.y - dim.height);
   int min_sx_cu = 0.95 * (bounds.min.x - dim.width);
   int max_sy_cu = 0.95 * (bounds.max.y + dim.height);
@@ -769,7 +770,7 @@ static void map_limit_scroll(map_t *map, canvas_t::canvas_unit_t unit, int &sx, 
  * Specifically the map is allowed to be no smaller than the viewport. */
 static bool map_limit_zoom(map_t *map, double &zoom) {
     // Data rect minimum and maximum
-    const bounds_t &bounds = map->appdata.osm->bounds;
+    const bounds_t &bounds = map->appdata.project->osm->bounds;
 
     /* get size of visible area in pixels and convert to meters of intended */
     /* zoom by deviding by zoom (which is basically pix/m) */
@@ -811,10 +812,10 @@ static bool map_limit_zoom(map_t *map, double &zoom) {
 bool map_t::scroll_to_if_offscreen(const lpos_t lpos) {
 
   // Ignore anything outside the working area
-  if(unlikely(!appdata.osm))
+  if(unlikely(appdata.project->osm == O2G_NULLPTR))
     return false;
 
-  if (!appdata.osm->bounds.contains(lpos)) {
+  if (!appdata.project->osm->bounds.contains(lpos)) {
     printf("cannot scroll to (%d, %d): outside the working area\n", lpos.x, lpos.y);
     return false;
   }
@@ -1078,7 +1079,8 @@ static void map_touchnode_update(map_t *map, int x, int y) {
   lpos_t pos = map->canvas->window2world(x, y);
   node_t *rnode = O2G_NULLPTR;
   hl_nodes fc(cur_node, pos, map, rnode);
-  std::for_each(map->appdata.osm->nodes.begin(), map->appdata.osm->nodes.end(), fc);
+  osm_t * const osm = map->appdata.project->osm;
+  std::for_each(osm->nodes.begin(), osm->nodes.end(), fc);
 
   if(rnode != O2G_NULLPTR) {
     delete map->touchnode;
@@ -1199,11 +1201,12 @@ void map_t::button_release(int x, int y) {
     lpos_t pos = canvas->window2world(x, y);
 
     node_t *node = O2G_NULLPTR;
-    if(!appdata.osm->bounds.contains(pos))
+    osm_t * const osm = appdata.project->osm;
+    if(!osm->bounds.contains(pos))
       outside_error();
     else {
-      node = appdata.osm->node_new(pos);
-      appdata.osm->node_attach(node);
+      node = osm->node_new(pos);
+      osm->node_attach(node);
       draw(node);
     }
     set_action(MAP_ACTION_IDLE);
@@ -1214,7 +1217,7 @@ void map_t::button_release(int x, int y) {
       map_node_select(this, node);
 
       /* let the user specify some tags for the new node */
-      info_dialog(appdata_t::window, this, appdata.osm, appdata.presets);
+      info_dialog(appdata_t::window, this, osm, appdata.presets);
     }
     break;
   }
@@ -1319,7 +1322,7 @@ map_t::~map_t()
 }
 
 void map_t::init() {
-  const bounds_t &bounds = appdata.osm->bounds;
+  const bounds_t &bounds = appdata.project->osm->bounds;
 
   /* update canvas background color */
   canvas->set_background(style->background.color);
@@ -1373,7 +1376,7 @@ void map_t::clear(clearLayers layers) {
 }
 
 void map_t::paint() {
-  osm_t * const osm = appdata.osm;
+  osm_t * const osm = appdata.project->osm;
 
   style->colorize_world(osm);
 
@@ -1473,7 +1476,7 @@ void map_action_ok(map_t *map) {
       break;
 
     node_t *node = O2G_NULLPTR;
-    osm_t * const osm = map->appdata.osm;
+    osm_t * const osm = map->appdata.project->osm;
 
     if(!osm->bounds.ll.contains(pos)) {
       map_t::outside_error();
@@ -1490,7 +1493,7 @@ void map_action_ok(map_t *map) {
       map_node_select(map, node);
 
       /* let the user specify some tags for the new node */
-      info_dialog(appdata_t::window, map, map->appdata.osm, map->appdata.presets);
+      info_dialog(appdata_t::window, map, osm, map->appdata.presets);
     }
     }
 
@@ -1511,7 +1514,7 @@ void node_deleted_from_ways::operator()(way_t *way) {
     /* this way now only contains one node and thus isn't a valid */
     /* way anymore. So it'll also get deleted (which in turn may */
     /* cause other nodes to be deleted as well) */
-    map->appdata.osm->way_delete(way);
+    map->appdata.project->osm->way_delete(way);
   } else {
     object_t object(way);
     map->redraw_item(object);
@@ -1546,11 +1549,12 @@ void map_delete_selected(map_t *map) {
   printf("request to delete %s #" ITEM_ID_FORMAT "\n",
          objtype, item.object.obj->id);
 
+  osm_t * const osm = map->appdata.project->osm;
   switch(item.object.type) {
   case object_t::NODE: {
     /* check if this node is part of a way with two nodes only. */
     /* we cannot delete this as this would also delete the way */
-    if(map->appdata.osm->find_way(short_way(item.object.node)) != O2G_NULLPTR &&
+    if(osm->find_way(short_way(item.object.node)) != O2G_NULLPTR &&
        !yes_no_f(O2G_NULLPTR, 0, _("Delete node in short way(s)?"),
                  _("Deleting this node will also delete one or more ways "
                    "since they'll contain only one node afterwards. "
@@ -1558,18 +1562,18 @@ void map_delete_selected(map_t *map) {
       return;
 
     /* and mark it "deleted" in the database */
-    const way_chain_t &chain = map->appdata.osm->node_delete(item.object.node);
+    const way_chain_t &chain = osm->node_delete(item.object.node);
     std::for_each(chain.begin(), chain.end(), node_deleted_from_ways(map));
 
     break;
   }
 
   case object_t::WAY:
-    map->appdata.osm->way_delete(item.object.way);
+    osm->way_delete(item.object.way);
     break;
 
   case object_t::RELATION:
-    map->appdata.osm->relation_delete(item.object.relation);
+    osm->relation_delete(item.object.relation);
     break;
 
   default:
@@ -1602,7 +1606,7 @@ static std::vector<lpos_t> canvas_points_init(const bounds_t &bounds,
 }
 
 void map_t::track_draw_seg(track_seg_t &seg) {
-  const bounds_t &bounds = appdata.osm->bounds;
+  const bounds_t &bounds = appdata.project->osm->bounds;
 
   /* a track_seg needs at least 2 points to be drawn */
   if (seg.track_points.empty())
@@ -1671,7 +1675,7 @@ void map_t::track_draw_seg(track_seg_t &seg) {
 /* update the last visible fragment of this segment since a */
 /* gps position may have been added */
 void map_t::track_update_seg(track_seg_t &seg) {
-  const bounds_t &bounds = appdata.osm->bounds;
+  const bounds_t &bounds = appdata.project->osm->bounds;
 
   printf("-- APPENDING TO TRACK --\n");
 
@@ -1844,8 +1848,8 @@ void map_show_all_functor::operator()(std::pair<item_id_t, way_t *> pair)
 }
 
 void map_t::show_all() {
-  std::for_each(appdata.osm->ways.begin(), appdata.osm->ways.end(),
-                map_show_all_functor(this));
+  std::map<item_id_t, way_t *> &ways = appdata.project->osm->ways;
+  std::for_each(ways.begin(), ways.end(), map_show_all_functor(this));
 
   appdata.uicontrol->setActionEnable(MainUi::MENU_ITEM_MAP_SHOW_ALL, false);
 }

src/map_edit.cpp
@@ -25,6 +25,7 @@
 #include "map.h"
 #include "map_hl.h"
 #include "misc.h"
+#include "project.h"
 #include "style.h"
 #include "uicontrol.h"
 
@@ -74,6 +75,7 @@ void map_edit_way_add_segment(map_t *map, int x, int y) {
 
     /* use the existing node if one was touched */
     node_t *node = map->touchnode_get_node();
+    osm_t * const osm = map->appdata.project->osm;
     if(node != O2G_NULLPTR) {
       printf("  re-using node #" ITEM_ID_FORMAT "\n", node->id);
 
@@ -81,7 +83,7 @@ void map_edit_way_add_segment(map_t *map, int x, int y) {
 
       /* check whether this node is first or last one of a different way */
       way_t *touch_way = O2G_NULLPTR;
-      touch_way = map->appdata.osm->find_way(check_first_last_node(node));
+      touch_way = osm->find_way(check_first_last_node(node));
 
       /* remeber this way as this may be the last node placed */
       /* and we might want to join this with this other way */
@@ -106,10 +108,10 @@ void map_edit_way_add_segment(map_t *map, int x, int y) {
       /* a new node */
       map->action.ends_on = O2G_NULLPTR;
 
-      if(!map->appdata.osm->bounds.contains(pos))
+      if(!osm->bounds.contains(pos))
         map_t::outside_error();
       else
-        node = map->appdata.osm->node_new(pos);
+        node = osm->node_new(pos);
     }
 
     if(node) {
@@ -157,7 +159,7 @@ void map_unref_ways::operator()(node_t* node)
 }
 
 void map_edit_way_add_cancel(map_t *map) {
-  osm_t *osm = map->appdata.osm;
+  osm_t *osm = map->appdata.project->osm;
   assert(osm != O2G_NULLPTR);
 
   printf("  removing temporary way\n");
@@ -196,14 +198,14 @@ void map_draw_nodes::operator()(node_t* node)
     /* we can be sure that no node gets inserted twice (even if twice in */
     /* the ways chain) because it gets assigned a non-ID_ILLEGAL id when */
     /* being moved to the osm node chain */
-    map->appdata.osm->node_attach(node);
+    map->appdata.project->osm->node_attach(node);
   }
 
   map->draw(node);
 }
 
 void map_edit_way_add_ok(map_t *map) {
-  osm_t *osm = map->appdata.osm;
+  osm_t *osm = map->appdata.project->osm;
 
   assert(osm != O2G_NULLPTR);
   assert(map->action.way != O2G_NULLPTR);
@@ -224,7 +226,7 @@ void map_edit_way_add_ok(map_t *map) {
     map->action.way = map->action.extending;
   } else {
     /* now move the way itself into the main data structure */
-    map->appdata.osm->way_attach(map->action.way);
+    osm->way_attach(map->action.way);
   }
 
   /* we might already be working on the "ends_on" way as we may */
@@ -269,7 +271,7 @@ void map_edit_way_add_ok(map_t *map) {
   map->action.way = O2G_NULLPTR;
 
   /* let the user specify some tags for the new way */
-  info_dialog(appdata_t::window, map, map->appdata.osm, map->appdata.presets);
+  info_dialog(appdata_t::window, map, osm, map->appdata.presets);
 }
 
 /* -------------------------- way_node_add ----------------------- */
@@ -292,8 +294,8 @@ void map_edit_way_node_add(map_t *map, int px, int py) {
     int insert_after = item->get_segment(pos) + 1;
     if(insert_after > 0) {
       /* create new node */
-      node_t* node = map->appdata.osm->node_new(pos);
-      map->appdata.osm->node_attach(node);
+      node_t* node = map->appdata.project->osm->node_new(pos);
+      map->appdata.project->osm->node_attach(node);
 
       /* insert it into ways chain of nodes */
       way_t *way = item->object.way;
@@ -407,7 +409,7 @@ void map_edit_way_cut(map_t *map, int px, int py) {
   map_item_chain_destroy(way->map_item_chain);
 
   /* create a duplicate of the currently selected way */
-  way_t * const neww = way->split(map->appdata.osm, cut_at, cut_at_node);
+  way_t * const neww = way->split(map->appdata.project->osm, cut_at, cut_at_node);
 
   printf("original way still has %zu nodes\n", way->node_chain.size());
 
@@ -463,7 +465,7 @@ struct find_way_ends {
 };
 
 void map_edit_node_move(map_t *map, map_item_t *map_item, int ex, int ey) {
-  osm_t *osm = map->appdata.osm;
+  osm_t *osm = map->appdata.project->osm;
 
   assert(map_item->object.type == object_t::NODE);
   node_t *node = map_item->object.node;
@@ -599,7 +601,7 @@ void map_edit_way_reverse(map_t *map) {
 
   unsigned int n_tags_flipped;
   unsigned int n_roles_flipped;
-  item.object.way->reverse(map->appdata.osm, n_tags_flipped, n_roles_flipped);
+  item.object.way->reverse(map->appdata.project->osm, n_tags_flipped, n_roles_flipped);
 
   map->select_way(item.object.way);
 

src/osm_api.h
@@ -28,6 +28,6 @@ struct project_t;
 class settings_t;
 
 bool osm_download(osm2go_platform::Widget *parent, project_t *project);
-void osm_upload(appdata_t &appdata, osm_t *osm, project_t *project);
+void osm_upload(appdata_t &appdata, project_t *project);
 
 #endif // OSM_API_H

src/osm_api_p.h
@@ -37,7 +37,7 @@ struct project_t;
 
 class osm_upload_context_t {
 public:
-  osm_upload_context_t(appdata_t &a, osm_t *o, project_t *p, const char *c, const char *s);
+  osm_upload_context_t(appdata_t &a, project_t *p, const char *c, const char *s);
 
   appdata_t &appdata;
   osm_t * const osm;

commit 5668de307339ff01461087aac4c59b5ba24e515e

do not crash if no project is (not yet) loaded on startup
Fixes: ab9f0db7f981582972d0c792f8e37f76334f8c4a

src/track.cpp
@@ -453,7 +453,7 @@ static int update(void *data) {
 
   /* ignore updates while no valid osm file is loaded, e.g. when switching */
   /* projects */
-  if(unlikely(appdata.project->osm == O2G_NULLPTR))
+  if(unlikely(appdata.project == O2G_NULLPTR || appdata.project->osm == O2G_NULLPTR))
     return 1;
 
   /* the map is only gone of the main screen is being closed */