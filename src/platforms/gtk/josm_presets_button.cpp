osm2go/osm2go

commit 2e31642964f3063e8a78dc43e11bb0cfa6ebd0d3

make presets_items an abstract base class

src/platforms/gtk/josm_presets_button.cpp
@@ -349,7 +349,7 @@ static void presets_item_dialog(const presets_item *item) {
     if(changed)
       tag_context->info_tags_replace();
 
-    std::vector<presets_item_t *> &lru = presets_context_t::instance->presets->lru;
+    std::vector<presets_item_t *> &lru = static_cast<presets_items_internal *>(presets_context_t::instance->presets)->lru;
     std::vector<presets_item_t *>::iterator litBegin = lru.begin();
     std::vector<presets_item_t *>::iterator lit = std::find(litBegin, lru.end(), item);
     // if it is already the first item in the list nothing is to do
@@ -705,7 +705,8 @@ GtkWidget *presets_context_t::preset_picker_recent() {
   GtkTreeView *view;
   insert_recent_items<false> fc(this, presets_picker_store(&view));
 
-  std::for_each(presets->items.begin(), presets->items.end(), fc);
+  const std::vector<presets_item_t *> &pitems = static_cast<const presets_items_internal *>(presets)->items;
+  std::for_each(pitems.begin(), pitems.end(), fc);
 
   return presets_picker_embed(view, fc.store, this);
 }
@@ -714,7 +715,8 @@ GtkWidget *presets_context_t::preset_picker_lru() {
   GtkTreeView *view;
   insert_recent_items<true> fc(this, presets_picker_store(&view));
 
-  std::for_each(presets->lru.begin(), presets->lru.end(), fc);
+  const std::vector<presets_item_t *> &pitems = static_cast<const presets_items_internal *>(presets)->items;
+  std::for_each(pitems.begin(), pitems.end(), fc);
 
   return presets_picker_embed(view, fc.store, this);
 }
@@ -788,7 +790,7 @@ presets_context_t::presets_picker(const std::vector<presets_item_t *> &items,
   picker_add_functor fc(this, store, subicon, top_level, show_recent);
 
   std::for_each(items.begin(), items.end(), fc);
-  const std::vector<presets_item_t *> &lru = presets->lru;
+  const std::vector<presets_item_t *> &lru = static_cast<const presets_items_internal *>(presets)->lru;
 
   if(top_level &&
      std::find_if(lru.begin(), lru.end(),
@@ -865,7 +867,9 @@ static gint button_press(GtkWidget *widget, GdkEventButton *event) {
   /* create root picker */
   GtkWidget *hbox = gtk_hbox_new(TRUE, 0);
 
-  GtkWidget *root = presets_context_t::instance->presets_picker(presets_context_t::instance->presets->items, true);
+  GtkWidget *root = presets_context_t::instance->presets_picker(
+                        static_cast<presets_items_internal *>(presets_context_t::instance->presets)->items,
+                                                                true);
   gtk_box_pack_start_defaults(GTK_BOX(hbox), root);
 
   gtk_box_pack_start_defaults(GTK_BOX(GTK_DIALOG(dialog.get())->vbox), hbox);

src/appdata.h
@@ -32,7 +32,7 @@ class MainUi;
 struct map_state_t;
 class map_t;
 struct osm_t;
-struct presets_items;
+class presets_items;
 struct project_t;
 class statusbar_t;
 struct style_t;

src/info.h
@@ -28,7 +28,7 @@
 #include <vector>
 
 class map_t;
-struct presets_items;
+class presets_items;
 
 class tag_context_t {
 protected:

src/josm_presets.cpp
@@ -164,7 +164,7 @@ void role_collect_functor::operator()(const presets_item::role &role)
   result.insert(role.name);
 }
 
-std::set<std::string> preset_roles(const relation_t *relation, const object_t &obj, const presets_items *presets)
+std::set<std::string> presets_items_internal::roles(const relation_t *relation, const object_t &obj) const
 {
   // collect existing roles first
   role_collect_functor::RoleCountMap existingRoles;
@@ -178,10 +178,10 @@ std::set<std::string> preset_roles(const relation_t *relation, const object_t &o
   std::set<std::string> ret;
   const presets_item *item = nullptr;
   relation_preset_functor fc(relation, &item);
-  const std::vector<presets_item_t *>::const_iterator itEnd = presets->items.end();
+  const std::vector<presets_item_t *>::const_iterator itEnd = items.end();
   role_collect_functor rfc(ret, existingRoles, presets_type_mask(obj));
 
-  if(std::find_if(presets->items.begin(), itEnd, fc) != itEnd)
+  if(std::find_if(items.begin(), itEnd, fc) != itEnd)
     std::for_each(item->roles.begin(), item->roles.end(), rfc);
 
   return ret;

src/josm_presets.h
@@ -25,18 +25,22 @@
 
 #include <osm2go_platform.h>
 
-struct presets_items;
-
-presets_items *josm_presets_load(void);
-void josm_presets_free(presets_items *presets);
-
 struct object_t;
 class relation_t;
 class tag_context_t;
 
+class presets_items {
+protected:
+  presets_items() {}
+public:
+  virtual ~presets_items() {}
+
+  static presets_items *load(void);
+  virtual std::set<std::string> roles(const relation_t *relation, const object_t &obj) const = 0;
+};
+
 std::string josm_icon_name_adjust(const char *name);
 
 osm2go_platform::Widget *josm_build_presets_button(presets_items *presets, tag_context_t *tag_context);
-std::set<std::string> preset_roles(const relation_t *relation, const object_t &obj, const presets_items *presets);
 
 #endif // JOSM_PRESETS_H

src/josm_presets_p.h
@@ -332,14 +332,17 @@ public:
 
 #define LRU_MAX 10	///< how many items we want in presets_items::lru at most
 
-struct presets_items {
-  presets_items();
-  ~presets_items();
+class presets_items_internal : public presets_items {
+public:
+  presets_items_internal();
+  ~presets_items_internal();
   std::vector<presets_item_t *> items;
   std::vector<presets_item_t *> chunks;
   std::vector<presets_item_t *> lru;
 
   bool addFile(const std::string &filename, const std::string &basepath, int basefd);
+
+  virtual std::set<std::string> roles(const relation_t *relation, const object_t &obj) const override;
 };
 
 static inline unsigned int widget_rows(unsigned int init, const presets_element_t *w) {

src/josm_presets_parser.cpp
@@ -162,7 +162,7 @@ class PresetSax {
   std::vector<State> state;
   std::stack<presets_item_t *> items; // the current item stack (i.e. menu layout)
   std::stack<presets_element_t *> widgets; // the current widget stack (i.e. dialog layout)
-  presets_items &presets;
+  presets_items_internal &presets;
   const std::string &basepath;
   const int basedirfd;
   const std::vector<std::string> &langs;
@@ -192,7 +192,7 @@ class PresetSax {
   void dumpState(const char *before = nullptr, const char *after = nullptr) const;
 
 public:
-  explicit PresetSax(presets_items &p, const std::string &b, int basefd);
+  explicit PresetSax(presets_items_internal &p, const std::string &b, int basefd);
 
   bool parse(const std::string &filename);
 
@@ -339,7 +339,7 @@ const std::vector<std::string> &userLangs()
   return lcodes;
 }
 
-PresetSax::PresetSax(presets_items &p, const std::string &b, int basefd)
+PresetSax::PresetSax(presets_items_internal &p, const std::string &b, int basefd)
   : presets(p)
   , basepath(b)
   , basedirfd(basefd)
@@ -390,11 +390,9 @@ void PresetSax::find_link_ref::operator()(PresetSax::LLinks::value_type &l)
   if(unlikely(!px.resolvePresetLink(l.first, l.second))) {
     printf("found preset_link with unmatched preset_name '%s'\n", l.second.c_str());
     find_link_parent fc(l.first);
-    std::vector<presets_item_t *>::const_iterator it =
-        std::find_if(px.presets.items.begin(), px.presets.items.end(), fc);
-    if(it == px.presets.items.end()) {
-      const ChunkMap::const_iterator cit =
-          std::find_if(px.chunks.begin(), px.chunks.end(), fc);
+    const std::vector<presets_item_t *>::const_iterator itEnd = px.presets.items.end();
+    if(std::find_if(std::cbegin(px.presets.items), itEnd, fc) == itEnd) {
+      const ChunkMap::const_iterator cit = std::find_if(px.chunks.begin(), px.chunks.end(), fc);
       assert(cit != px.chunks.end());
     }
   }
@@ -957,7 +955,7 @@ struct move_chunks_functor {
   }
 };
 
-bool presets_items::addFile(const std::string &filename, const std::string &basepath, int basefd)
+bool presets_items_internal::addFile(const std::string &filename, const std::string &basepath, int basefd)
 {
   PresetSax p(*this, basepath, basefd);
   if(!p.parse(filename))
@@ -970,10 +968,10 @@ bool presets_items::addFile(const std::string &filename, const std::string &base
   return true;
 }
 
-struct presets_items *josm_presets_load(void) {
+presets_items *presets_items::load(void) {
   printf("Loading JOSM presets ...\n");
 
-  struct presets_items *presets = new presets_items();
+  std::unique_ptr<presets_items_internal> presets(new presets_items_internal());
 
   const std::string &filename = find_file("defaultpresets.xml");
   if(likely(!filename.empty()))
@@ -1011,19 +1009,13 @@ struct presets_items *josm_presets_load(void) {
     }
   }
 
-  if(unlikely(presets->items.empty())) {
-    delete presets;
+  if(unlikely(presets->items.empty()))
     return nullptr;
-  }
 
-  return presets;
+  return presets.release();
 }
 
 /* ----------------------- cleaning up --------------------- */
-
-void josm_presets_free(struct presets_items *presets) {
-  delete presets;
-}
 
 struct MatchValue {
   MatchValue(const char *n, presets_element_t::Match m) : name(n), match(m) {}
@@ -1193,12 +1185,13 @@ presets_item_group::~presets_item_group()
   std::for_each(items.begin(), items.end(), std::default_delete<presets_item_t>());
 }
 
-presets_items::presets_items()
+presets_items_internal::presets_items_internal()
+  : presets_items()
 {
   lru.reserve(LRU_MAX);
 }
 
-presets_items::~presets_items()
+presets_items_internal::~presets_items_internal()
 {
   std::for_each(items.begin(), items.end(), std::default_delete<presets_item_t>());
   std::for_each(chunks.begin(), chunks.end(), std::default_delete<presets_item_t>());

src/platforms/gtk/main.cpp
@@ -1030,8 +1030,7 @@ appdata_t::~appdata_t() {
   if(project)
     project->save(nullptr);
 
-  josm_presets_free(presets);
-
+  delete presets;
   delete gps_state;
   delete settings;
   delete style;
@@ -1263,7 +1262,7 @@ static int application_run(const char *proj)
 
   gtk_widget_show_all(appdata_t::window);
 
-  appdata.presets = josm_presets_load();
+  appdata.presets = presets_items::load();
 
   /* let gtk do its thing before loading the data, */
   /* so the user sees something */

commit c4057fd2291c4d81dd70dd06467328b172678bca

fix LRU presets menu
Fixes: 2e31642964f3063e8a78dc43e11bb0cfa6ebd0d3

src/platforms/gtk/josm_presets_button.cpp
@@ -350,14 +350,15 @@ static void presets_item_dialog(const presets_item *item) {
       tag_context->info_tags_replace();
 
     std::vector<presets_item_t *> &lru = static_cast<presets_items_internal *>(presets_context_t::instance->presets)->lru;
-    std::vector<presets_item_t *>::iterator litBegin = lru.begin();
-    std::vector<presets_item_t *>::iterator lit = std::find(litBegin, lru.end(), item);
-    // if it is already the first item in the list nothing is to do
-    if(lit == lru.end()) {
+    const std::vector<presets_item_t *>::iterator litBegin = lru.begin();
+    const std::vector<presets_item_t *>::iterator litEnd = lru.end();
+    const std::vector<presets_item_t *>::iterator lit = std::find(litBegin, litEnd, item);
+    if(lit == litEnd) {
       // drop the oldest ones if too many
       if(lru.size() >= LRU_MAX)
         lru.resize(LRU_MAX - 1);
       lru.insert(litBegin, const_cast<presets_item *>(item));
+    // if it is already the first item in the list nothing is to do
     } else if(lit != litBegin) {
       // move to front
       std::rotate(litBegin, lit, lit + 1);
@@ -712,7 +713,7 @@ GtkWidget *presets_context_t::preset_picker_lru() {
   GtkTreeView *view;
   insert_recent_items<true> fc(this, presets_picker_store(&view));
 
-  const std::vector<presets_item_t *> &pitems = static_cast<const presets_items_internal *>(presets)->items;
+  const std::vector<presets_item_t *> &pitems = static_cast<const presets_items_internal *>(presets)->lru;
   std::for_each(pitems.begin(), pitems.end(), fc);
 
   return presets_picker_embed(view, fc.store, this);