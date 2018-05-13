osm2go/osm2go

commit bc55f098523895ad532338034b8b4264be419c68

move the check if a given position is visible into canvas_t

src/platforms/gtk/canvas_goocanvas.cpp
@@ -311,7 +311,7 @@ static gint item_at_compare(gconstpointer i, gconstpointer f)
 
   const canvas_t::item_mapping_t::const_iterator it = fc.canvas->item_mapping.find(const_cast<canvas_item_t *>(citem));
   if(it == fc.canvas->item_mapping.end()) {
-    printf("item %p not in canvas map\n", citem);
+    g_debug("item %p not in canvas map\n", citem);
     return -1;
   }
 
@@ -547,4 +547,37 @@ int canvas_t::get_item_segment(const canvas_item_t *item, lpos_t pos) const {
 
   return canvas_item_info_get_segment(static_cast<const canvas_item_info_poly *>(it->second),
                                       pos.x, pos.y, 0);
+}
+
+bool canvas_t::isVisible(const lpos_t lpos) const
+{
+  // Viewport dimensions in canvas space
+
+  /* get size of visible area in canvas units (meters) */
+  const canvas_dimensions dim = get_viewport_dimensions(canvas_t::UNIT_METER);
+
+  // Is the point still onscreen?
+  int sx, sy;
+  scroll_get(sx, sy);
+  int viewport_left   = sx - dim.width / 2;
+  int viewport_right  = sx + dim.width / 2;
+  int viewport_top    = sy - dim.height / 2;
+  int viewport_bottom = sy + dim.height / 2;
+
+  if (lpos.x > viewport_right) {
+    g_debug("** off right edge (%d > %d)\n", lpos.x, viewport_right);
+    return true;
+  } else if (lpos.x < viewport_left) {
+    g_debug("** off left edge (%d < %d)\n", lpos.x, viewport_left);
+    return true;
+  }
+  if (lpos.y > viewport_bottom) {
+    g_debug("** off bottom edge (%d > %d)\n", lpos.y, viewport_bottom);
+    return true;
+  } else if (lpos.y < viewport_top) {
+    g_debug("** off top edge (%d < %d)\n", lpos.y, viewport_top);
+    return true;
+  }
+
+  return false;
 }

src/canvas.h
@@ -162,9 +162,16 @@ public:
   void item_info_push(canvas_item_t *item);
 
   /**
-  * @brief get the polygon/polyway segment a certain coordinate is over
-  */
+   * @brief get the polygon/polyway segment a certain coordinate is over
+   */
   int get_item_segment(const canvas_item_t *item, lpos_t pos) const;
+
+  /**
+   * @brief check if the given coordinate is currently visible on screen
+   *
+   * The coordinate must be within the project bounds.
+   */
+  bool isVisible(const lpos_t lpos) const;
 };
 
 #endif // CANVAS_H

src/map.cpp
@@ -778,36 +778,7 @@ bool map_t::scroll_to_if_offscreen(const lpos_t lpos) {
     return false;
   }
 
-  // Viewport dimensions in canvas space
-
-  /* get size of visible area in canvas units (meters) */
-  canvas_dimensions dim = canvas->get_viewport_dimensions(canvas_t::UNIT_METER);
-
-  // Is the point still onscreen?
-  bool recentre_needed = false;
-  int sx, sy;
-  canvas->scroll_get(sx, sy);
-  int viewport_left   = sx - dim.width / 2;
-  int viewport_right  = sx + dim.width / 2;
-  int viewport_top    = sy - dim.height / 2;
-  int viewport_bottom = sy + dim.height / 2;
-
-  if (lpos.x > viewport_right) {
-    printf("** off right edge (%d > %d)\n", lpos.x, viewport_right);
-    recentre_needed = true;
-  } else if (lpos.x < viewport_left) {
-    printf("** off left edge (%d < %d)\n", lpos.x, viewport_left);
-    recentre_needed = true;
-  }
-  if (lpos.y > viewport_bottom) {
-    printf("** off bottom edge (%d > %d)\n", lpos.y, viewport_bottom);
-    recentre_needed = true;
-  } else if (lpos.y < viewport_top) {
-    printf("** off top edge (%d < %d)\n", lpos.y, viewport_top);
-    recentre_needed = true;
-  }
-
-  if(recentre_needed) {
+  if(!canvas->isVisible(lpos)) {
     // Just centre both at once
     int new_sx = lpos.x; // XXX (lpos.x - (aw/2));
     int new_sy = lpos.y; // XXX (lpos.y - (ah/2));
@@ -815,6 +786,7 @@ bool map_t::scroll_to_if_offscreen(const lpos_t lpos) {
     map_limit_scroll(this, new_sx, new_sy);
     canvas->scroll_to(new_sx, new_sy);
   }
+
   return true;
 }
 

commit 910b0f336259e5f268ab5b6c9f4ffce04ac76457

fix inverted return value from canvas_t::isVisible()
Fixes: bc55f098523895ad532338034b8b4264be419c68

src/platforms/gtk/canvas_goocanvas.cpp
@@ -566,18 +566,18 @@ bool canvas_t::isVisible(const lpos_t lpos) const
 
   if (lpos.x > viewport_right) {
     g_debug("** off right edge (%d > %d)\n", lpos.x, viewport_right);
-    return true;
+    return false;
   } else if (lpos.x < viewport_left) {
     g_debug("** off left edge (%d < %d)\n", lpos.x, viewport_left);
-    return true;
+    return false;
   }
   if (lpos.y > viewport_bottom) {
     g_debug("** off bottom edge (%d > %d)\n", lpos.y, viewport_bottom);
-    return true;
+    return false;
   } else if (lpos.y < viewport_top) {
     g_debug("** off top edge (%d < %d)\n", lpos.y, viewport_top);
-    return true;
+    return false;
   }
 
-  return false;
+  return true;
 }