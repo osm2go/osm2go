osm2go/osm2go

commit 8db755603ceae90459aac4803180aca186512373

simplify canvas_t::erase()
Instead of doing one function call for every item in a group to get it and one
more to remove it, simply remove the group item itself (which will clear all of
it's child items) and create a new one.

src/platforms/gtk/canvas_goocanvas.cpp
@@ -172,17 +172,14 @@ void canvas_t::set_bounds(int minx, int miny, int maxx, int maxy) {
 /* ------------------- creating and destroying objects ---------------- */
 
 void canvas_t::erase(unsigned int group_mask) {
-  int group;
-
   canvas_goocanvas *gcanvas = static_cast<canvas_goocanvas *>(this);
+  GooCanvasItem *root = goo_canvas_get_root_item(GOO_CANVAS(widget));
 
-  for(group=0;group<CANVAS_GROUPS;group++) {
-
+  /* create the groups */
+  for(unsigned int group = 0; group < gcanvas->group.size(); group++) {
     if(group_mask & (1<<group)) {
-      gint children = goo_canvas_item_get_n_children(gcanvas->group[group]);
-      g_debug("Removing %d children from group %d", children, group);
-      while(children--)
-	goo_canvas_item_remove_child(gcanvas->group[group], children);
+      goo_canvas_item_remove(gcanvas->group[group]);
+      gcanvas->group[group] = goo_canvas_group_new(root, nullptr);
     }
   }
 }

commit a0260b30917a457cfb683301c43a8ab74be9f768

fix group stacking order when recreating canvas groups
This would result in the frisket been drawn below the map after uploading
changes because the newly created groups having a higher z-order.

Fixes: 8db755603ceae90459aac4803180aca186512373

src/platforms/gtk/canvas_goocanvas.cpp
@@ -260,10 +260,14 @@ void canvas_t::erase(unsigned int group_mask) {
   GooCanvasItem *root = goo_canvas_get_root_item(GOO_CANVAS(widget));
 
   /* create the groups */
-  for(unsigned int group = 0; group < gcanvas->group.size(); group++) {
-    if(group_mask & (1<<group)) {
+  for(unsigned int group = 0; group < gcanvas->group.size() && group_mask != 0; group++) {
+    if(group_mask & (1 << group)) {
       goo_canvas_item_remove(gcanvas->group[group]);
       gcanvas->group[group] = goo_canvas_group_new(root, nullptr);
+      // restore z-order
+      if(group < gcanvas->group.size() - 1)
+        goo_canvas_item_lower(gcanvas->group[group], gcanvas->group[group + 1]);
+      group_mask ^= 1 << group;
     }
   }
 }