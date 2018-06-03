osm2go/osm2go

commit 2c43b3d73d5364a679de91d7235b251a32a2ed02

fold osm2go_platform::string_select_widget() into osm2go_platform::combo_box_new()

src/platforms/gtk/platform.cpp
@@ -64,18 +64,6 @@ struct combo_add_string {
   }
 };
 
-GtkWidget *osm2go_platform::string_select_widget(const char *title, const std::vector<std::string> &entries, int match) {
-  GtkWidget *cbox = osm2go_platform::combo_box_new(title);
-
-  /* fill combo box with entries */
-  std::for_each(entries.begin(), entries.end(), combo_add_string(cbox));
-
-  if(match >= 0)
-    osm2go_platform::combo_box_set_active(cbox, match);
-
-  return cbox;
-}
-
 void osm2go_platform::dialog_size_hint(GtkWindow *window, osm2go_platform::DialogSizeHint hint)
 {
   static const gint dialog_sizes[][2] = {

src/platforms/gtk/desktop/platform.cpp
@@ -20,9 +20,10 @@
 #include <osm2go_platform.h>
 #include <osm2go_platform_gtk.h>
 
-#include <gtk/gtk.h>
-
 #include <misc.h>
+
+#include <algorithm>
+#include <gtk/gtk.h>
 
 #include <osm2go_cpp.h>
 
@@ -107,10 +108,26 @@ bool osm2go_platform::check_button_get_active(GtkWidget *button)
   return gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)) == TRUE;
 }
 
+struct combo_add_string {
+  GtkComboBoxText * const cbox;
+  explicit combo_add_string(GtkWidget *w) : cbox(GTK_COMBO_BOX_TEXT(w)) {}
+  inline void operator()(const char *entry) {
+    gtk_combo_box_text_append_text(cbox, entry);
+  }
+};
+
 /* the title is only used on fremantle with the picker widget */
-GtkWidget *osm2go_platform::combo_box_new(const char *)
+GtkWidget *osm2go_platform::combo_box_new(const char *, const std::vector<const char *> &items, int active)
 {
-  return gtk_combo_box_text_new();
+  GtkWidget *cbox = gtk_combo_box_text_new();
+
+  /* fill combo box with entries */
+  std::for_each(items.begin(), items.end(), combo_add_string(cbox));
+
+  if(active >= 0)
+    osm2go_platform::combo_box_set_active(cbox, active);
+
+  return cbox;
 }
 
 GtkWidget *osm2go_platform::combo_box_entry_new(const char *)

src/platforms/gtk/fremantle/platform.cpp
@@ -22,8 +22,7 @@
 
 #include "dbus.h"
 
-#include <osm2go_cpp.h>
-
+#include <algorithm>
 #include <hildon/hildon-check-button.h>
 #include <hildon/hildon-entry.h>
 #include <hildon/hildon-pannable-area.h>
@@ -31,6 +30,8 @@
 #include <hildon/hildon-touch-selector-entry.h>
 #include <libosso.h>
 #include <tablet-browser-interface.h>
+
+#include <osm2go_cpp.h>
 
 static osso_context_t *osso_context;
 
@@ -206,10 +207,26 @@ static GtkWidget *combo_box_new_with_selector(const gchar *title, GtkWidget *sel
   return button;
 }
 
-/* the title is only used on fremantle with the picker widget */
-GtkWidget *osm2go_platform::combo_box_new(const char *title) {
+struct combo_add_string {
+  HildonTouchSelector * const selector;
+  explicit combo_add_string(HildonTouchSelector *sel) : selector(sel) {}
+  inline void operator()(const char *entry) {
+    hildon_touch_selector_append_text(selector, entry);
+  }
+};
+
+GtkWidget *osm2go_platform::combo_box_new(const char *title, const std::vector<const char *> &items, int active)
+{
   GtkWidget *selector = hildon_touch_selector_new_text();
-  return combo_box_new_with_selector(title, selector);
+  GtkWidget *cbox = combo_box_new_with_selector(title, selector);
+
+  /* fill combo box with entries */
+  std::for_each(items.begin(), items.end(), combo_add_string(HILDON_TOUCH_SELECTOR(selector)));
+
+  if(active >= 0)
+    osm2go_platform::combo_box_set_active(cbox, active);
+
+  return cbox;
 }
 
 /**

src/platforms/gtk/area_edit.cpp
@@ -796,10 +796,10 @@ bool area_edit_t::run() {
   gtk_table_attach_defaults(table,
 			    context.extent.height, 1, 2, 2, 3);
 
-  context.extent.mil_km = combo_box_new(_("Unit"));
-  combo_box_append_text(context.extent.mil_km, _("mi"));
-  combo_box_append_text(context.extent.mil_km, _("km"));
-  combo_box_set_active(context.extent.mil_km, 1); // km
+  std::vector<const char *> units(2);
+  units[0] = _("mi");
+  units[1] = _("km");
+  context.extent.mil_km = osm2go_platform::combo_box_new(_("Unit"), units, 1);
 
   gtk_table_attach(table, context.extent.mil_km, 2, 3, 1, 3,
 		   static_cast<GtkAttachOptions>(0), static_cast<GtkAttachOptions>(0),

src/platforms/gtk/josm_presets_button.cpp
@@ -944,12 +944,10 @@ presets_element_t::attach_key *presets_element_combo::attach(preset_attach_conte
   int active = editable ? 0 : 1; // account for the extra "unset" entry for non-editable ones
   bool matched = false; // no need to search if editable, the text will explicitely be set anyway
 
-  if(editable) {
+  if(editable)
     ret = combo_box_entry_new(text.c_str());
-  } else {
-    ret = combo_box_new(text.c_str());
-    combo_box_append_text(ret, _("<unset>"));
-  }
+  else
+    ret = combo_box_new(text.c_str(), std::vector<const char *>(1, _("<unset>")));
 
   const std::vector<std::string> &d = display_values.empty() ? values : display_values;
 

src/platforms/gtk/main.cpp
@@ -233,14 +233,13 @@ cb_menu_map_show_all(appdata_t *appdata) {
 /* ---------------------------------------------------------- */
 
 GtkWidget *track_vis_select_widget(TrackVisibility current) {
-  std::vector<std::string> labels;
+  std::vector<const char *> labels;
   labels.push_back(_("Hide tracks"));
   labels.push_back(_("Show current position"));
   labels.push_back(_("Show current segment"));
   labels.push_back(_("Show all segments"));
 
-  return osm2go_platform::string_select_widget(_("Track visibility"), labels,
-                                               static_cast<int>(current));
+  return osm2go_platform::combo_box_new(_("Track visibility"), labels, static_cast<int>(current));
 }
 
 #ifndef FREMANTLE

src/platforms/gtk/osm2go_platform_gtk.h
@@ -54,7 +54,13 @@ namespace osm2go_platform {
   bool check_button_get_active(GtkWidget *button);
   bool isCheckButtonWidget(GtkWidget *widget);
 
-  GtkWidget *combo_box_new(const char *title);
+  /**
+   * @brief create a new combo box
+   * @param title the title string (only used on Fremantle)
+   * @param items the texts to fill
+   * @param active the item to preselect or -1 for none
+   */
+  GtkWidget *combo_box_new(const char *title, const std::vector<const char *> &items = std::vector<const char *>(), int active = -1);
   GtkWidget *combo_box_entry_new(const char *title);
   void combo_box_append_text(GtkWidget *cbox, const char *text);
   void combo_box_set_active(GtkWidget *cbox, int index);
@@ -63,8 +69,6 @@ namespace osm2go_platform {
   void combo_box_set_active_text(GtkWidget *cbox, const char *text);
   bool isComboBoxWidget(GtkWidget *widget);
   bool isComboBoxEntryWidget(GtkWidget *widget);
-
-  GtkWidget *string_select_widget(const char *title, const std::vector<std::string> &entries, int match);
 
   void setEntryText(GtkEntry *entry, const char *text, const char *placeholder);
 

src/platforms/gtk/style_widgets.cpp
@@ -43,8 +43,8 @@ struct combo_add_styles {
   int cnt;
   int &match;
   const std::string &currentstyle;
-  std::vector<std::string> &styles;
-  combo_add_styles(const std::string &sname, int &m, std::vector<std::string> &s)
+  std::vector<const char *> &styles;
+  combo_add_styles(const std::string &sname, int &m, std::vector<const char *> &s)
     : cnt(0), match(m), currentstyle(sname), styles(s) {}
   void operator()(const std::pair<std::string, std::string> &pair);
 };
@@ -54,7 +54,7 @@ void combo_add_styles::operator()(const std::pair<std::string, std::string> &pai
   if(match < 0 && style_basename(pair.second) == currentstyle)
     match = cnt;
 
-  styles.push_back(pair.first);
+  styles.push_back(pair.first.c_str());
 
   cnt++;
 }
@@ -67,11 +67,11 @@ static GtkWidget *style_select_widget(const std::string &currentstyle,
 
   /* fill combo box with presets */
   int match = -1;
-  std::vector<std::string> stylesNames;
+  std::vector<const char *> stylesNames;
   std::for_each(styles.begin(), styles.end(),
                 combo_add_styles(currentstyle, match, stylesNames));
 
-  return osm2go_platform::string_select_widget(_("Style"), stylesNames, match);
+  return osm2go_platform::combo_box_new(_("Style"), stylesNames, match);
 }
 
 GtkWidget *style_select_widget(const std::string &currentstyle) {

commit 3d4e89587e962b5b5c7f7ae9da78d4d7a7752b24

remove dead code
Fixes: 2c43b3d73d5364a679de91d7235b251a32a2ed02

src/platforms/gtk/platform.cpp
@@ -56,14 +56,6 @@ void osm2go_platform::Timer::stop()
   }
 }
 
-struct combo_add_string {
-  GtkWidget * const cbox;
-  explicit combo_add_string(GtkWidget *w) : cbox(w) {}
-  void operator()(const std::string &entry) {
-    osm2go_platform::combo_box_append_text(cbox, entry.c_str());
-  }
-};
-
 void osm2go_platform::dialog_size_hint(GtkWindow *window, osm2go_platform::DialogSizeHint hint)
 {
   static const gint dialog_sizes[][2] = {