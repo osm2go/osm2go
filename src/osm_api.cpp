osm2go/osm2go

commit d0459133a6858552292fca145757710615cf2c02

reduce the size of all OSM objects
The reason why version was stored in item_id_t (i.e. 64 bit) was to simplify
code when parsing the result of the OSM API return value. Rewrite that code to
handle a smaller version variable, saving 4 byte per object. On my test project
(36000 nodes, 7000 ways) this saves about 400k RAM.

src/osm_api.cpp
@@ -356,7 +356,10 @@ static void upload_object(osm_upload_context_t &context, base_object_t *obj) {
   if(xml_str) {
     printf("uploading %s " ITEM_ID_FORMAT " to %s\n", obj->apiString(), obj->id, url.c_str());
 
-    if(osm_update_item(context, xml_str.get(), url.c_str(), obj->isNew() ? &obj->id : &obj->version)) {
+    item_id_t tmp;
+    if(osm_update_item(context, xml_str.get(), url.c_str(), obj->isNew() ? &obj->id : &tmp)) {
+      if(obj->isNew())
+        obj->version = tmp;
       obj->flags ^= OSM_FLAG_DIRTY;
       context.project->data_dirty = true;
     }
@@ -386,7 +389,7 @@ void upload_objects<T>::operator()(T *obj)
 static void log_deletion(osm_upload_context_t &context, const base_object_t *obj) {
   assert(obj->flags & OSM_FLAG_DELETED);
 
-  context.appendf(nullptr, _("Deleted %s #" ITEM_ID_FORMAT " (version " ITEM_ID_FORMAT ")\n"),
+  context.appendf(nullptr, _("Deleted %s #" ITEM_ID_FORMAT " (version %u)\n"),
           obj->apiString(), obj->id, obj->version);
 }
 

src/osm.cpp
@@ -1294,7 +1294,7 @@ xmlChar *base_object_t::generate_xml(const std::string &changeset) const
     snprintf(str, sizeof(str), ITEM_ID_FORMAT, id);
     xmlNewProp(xml_node, BAD_CAST "id", BAD_CAST str);
   }
-  snprintf(str, sizeof(str), ITEM_ID_FORMAT, version);
+  snprintf(str, sizeof(str), "%u", version);
   xmlNewProp(xml_node, BAD_CAST "version", BAD_CAST str);
   xmlNewProp(xml_node, BAD_CAST "changeset", BAD_CAST changeset.c_str());
 
@@ -2140,12 +2140,12 @@ void tag_list_t::replace(const osm_t::TagMap &ntags)
   std::for_each(ntags.begin(), ntags.end(), tag_fill_functor(*contents));
 }
 
-base_object_t::base_object_t(item_id_t ver, item_id_t i) noexcept
+base_object_t::base_object_t(unsigned int ver, item_id_t i) noexcept
   : id(i)
-  , version(ver)
   , time(0)
-  , flags(version == 0 ? OSM_FLAG_DIRTY : 0)
+  , flags(ver == 0 ? OSM_FLAG_DIRTY : 0)
   , user(0)
+  , version(ver)
 {
 }
 
@@ -2171,8 +2171,8 @@ void base_object_t::osmchange_delete(xmlNodePtr parent_node, const char *changes
 
   xmlNewProp(obj_node, BAD_CAST "id", BAD_CAST id_string().c_str());
 
-  char buf[32] = { 0 };
-  snprintf(buf, sizeof(buf), ITEM_ID_FORMAT, version);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "%u", version);
 
   xmlNewProp(obj_node, BAD_CAST "version", BAD_CAST buf);
   xmlNewProp(obj_node, BAD_CAST "changeset", BAD_CAST changeset);
@@ -2192,7 +2192,7 @@ way_t::way_t()
   memset(&draw, 0, sizeof(draw));
 }
 
-way_t::way_t(item_id_t ver, item_id_t i)
+way_t::way_t(unsigned int ver, item_id_t i)
   : visible_item_t(ver, i)
 {
   memset(&draw, 0, sizeof(draw));
@@ -2320,7 +2320,7 @@ relation_t::relation_t()
 {
 }
 
-relation_t::relation_t(item_id_t ver, item_id_t i)
+relation_t::relation_t(unsigned int ver, item_id_t i)
   : base_object_t(ver, i)
 {
 }
@@ -2368,7 +2368,7 @@ node_t::node_t() noexcept
   memset(&lpos, 0, sizeof(lpos));
 }
 
-node_t::node_t(item_id_t ver, const lpos_t lp, const pos_t &p, item_id_t i) noexcept
+node_t::node_t(unsigned int ver, const lpos_t lp, const pos_t &p, item_id_t i) noexcept
   : visible_item_t(ver, i)
   , ways(0)
   , pos(p)

src/osm.h
@@ -427,14 +427,14 @@ private:
 
 class base_object_t {
 public:
-  explicit base_object_t(item_id_t ver = 0, item_id_t i = ID_ILLEGAL) noexcept;
+  explicit base_object_t(unsigned int ver = 0, item_id_t i = ID_ILLEGAL) noexcept;
 
   item_id_t id;
-  item_id_t version;
   tag_list_t tags;
   time_t time;
   unsigned int flags;
   int user;
+  unsigned int version;
 
   /**
    * @brief replace the tags and set dirty flag if they were actually different
@@ -472,7 +472,7 @@ protected:
 
 class visible_item_t : public base_object_t {
 protected:
-  inline visible_item_t(item_id_t ver = 0, item_id_t i = ID_ILLEGAL) noexcept
+  inline visible_item_t(unsigned int ver = 0, item_id_t i = ID_ILLEGAL) noexcept
     : base_object_t(ver, i)
     , map_item_chain(nullptr)
     , zoom_max(0.0f)
@@ -489,7 +489,7 @@ public:
 class node_t : public visible_item_t {
 public:
   explicit node_t() noexcept;
-  explicit node_t(item_id_t ver, const lpos_t lp, const pos_t &p, item_id_t i = ID_ILLEGAL) noexcept;
+  explicit node_t(unsigned int ver, const lpos_t lp, const pos_t &p, item_id_t i = ID_ILLEGAL) noexcept;
   virtual ~node_t() {}
 
   unsigned int ways;
@@ -514,7 +514,7 @@ typedef std::vector<node_t *> node_chain_t;
 class way_t: public visible_item_t {
 public:
   explicit way_t();
-  explicit way_t(item_id_t ver, item_id_t i = ID_ILLEGAL);
+  explicit way_t(unsigned int ver, item_id_t i = ID_ILLEGAL);
   virtual ~way_t() {}
 
   /* visual representation from elemstyle */
@@ -595,7 +595,7 @@ protected:
 class relation_t : public base_object_t {
 public:
   explicit relation_t();
-  explicit relation_t(item_id_t ver, item_id_t i = ID_ILLEGAL);
+  explicit relation_t(unsigned int ver, item_id_t i = ID_ILLEGAL);
   virtual ~relation_t() {}
 
   std::vector<member_t> members;

commit eb0177151624c1c119fbb86f03ef96e4b04c89b5

fix random version number shown on modified objects after upload
Fixes: d0459133a6858552292fca145757710615cf2c02

src/osm_api.cpp
@@ -367,7 +367,7 @@ static void upload_object(osm_upload_context_t &context, base_object_t *obj) {
 
     item_id_t tmp;
     if(osm_update_item(context, xml_str.get(), url.c_str(), obj->isNew() ? &obj->id : &tmp)) {
-      if(obj->isNew())
+      if(!obj->isNew())
         obj->version = tmp;
       obj->flags ^= OSM_FLAG_DIRTY;
       context.project->data_dirty = true;