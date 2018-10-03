50951ec020eb1de9d8134bd3a879de0c24227e37
Rolf Eike Beer <eike@sf-mail.de>
2018-10-03 12:40:26 +0200

    clean up icon import script

--- data/icons/josm_import.sh
+++ data/icons/josm_import.sh
@@ -12,41 +12,40 @@ STYLE="styles/standard/"
 
 if [ "$1" == "" ]; then
     echo "Usage: $0 JOSM_IMAGES_PATH"
-    exit 0;
-fi;
+    exit 0
+fi
 
 # cut trailing slash if present
 CPATH=${1%/}
 
 rm -rf $SUBDIRS
 
-for f in `find $CPATH -iname '*.png'`; do
-    NAME=.`echo $f | cut -b $[${#CPATH}+1]-`
+for f in $(find $CPATH -iname '*.png'); do
+    NAME=.$(echo $f | cut -b $((${#CPATH}+1))-)
     FOUND=""
 
     # search in presets.xml
-    PRESETNAME=`echo $NAME | cut -b 3-`
-    if [ "`grep $PRESETNAME ../presets.xml`" != "" ]; then
-	echo "$NAME is referenced in presets.xml";
+    PRESETNAME=$(echo $NAME | cut -b 3-)
+    if grep -q $PRESETNAME ../presets.xml; then
+	echo "$NAME is referenced in presets.xml"
 	FOUND="true"
-    fi;
-    
+    fi
+
     # search in elemstyles.xml
     # elemstyle references the files without the leading
     # "styles/standard/"
-    if [ "`echo $PRESETNAME | grep $STYLE`" != "" ]; then
-	ELEMNAME=`echo $PRESETNAME | cut -b $[${#STYLE}+1]-`
-	if [ "`grep $ELEMNAME ../elemstyles.xml`" != "" ]; then
-	    echo "$NAME is referenced in elemstyles.xml";
+    if [ "$(echo $PRESETNAME | grep $STYLE)" != "" ]; then
+	ELEMNAME=$(echo $PRESETNAME | cut -b $((${#STYLE}+1))-)
+	if grep -q $ELEMNAME ../elemstyles.xml; then
+	    echo "$NAME is referenced in elemstyles.xml"
 	    FOUND="true"
-	fi;
-    fi;
-    
-    if [ "$FOUND" != "" ]; then
-	mkdir -p `dirname $NAME`
-	convert $f -scale $SIZE $NAME;
+	fi
+    fi
+
+    if [ -n "$FOUND" ]; then
+	mkdir -p $(dirname $NAME)
+	convert $f -scale $SIZE $NAME
     else
 	echo "Skipping unused $NAME"
-    fi;
-    
-done;
+    fi
+done
