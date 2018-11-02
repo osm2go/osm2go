ad18acc8367bd71e9ecf46b9473d79ae8c7f5ace
Rolf Eike Beer <eike@sf-mail.de>
2018-11-02 22:57:35 +0100

    fix loss of precision when calculating highlight arrow position
    The arrows are sometimes misplaced, which can not be explained only with
    rounding errors, but this one here is basically for free.

--- src/map.cpp
+++ src/map.cpp
@@ -202,8 +202,8 @@ void draw_selected_way_functor::operator()(node_t *node)
     float len = std::sqrt(std::pow(diff.x, 2) + std::pow(diff.y, 2));
     if(len > map->style->highlight.arrow_limit * arrow_width) {
       struct { float x, y; } center;
-      center.x = (last->lpos.x + node->lpos.x) / 2;
-      center.y = (last->lpos.y + node->lpos.y) / 2;
+      center.x = (last->lpos.x + node->lpos.x) / 2.0;
+      center.y = (last->lpos.y + node->lpos.y) / 2.0;
 
       /* create a new map item for every arrow */
       len /= arrow_width;
