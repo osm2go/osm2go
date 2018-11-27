osm2go/osm2go

commit e41376e247b1f33c287099a67c6dd0f78b6be220

JOSM presets: add support for multiselects

src/josm_presets_parser.cpp
@@ -140,6 +140,7 @@ class PresetSax {
     TagKey,
     TagText,
     TagCombo,
+    TagMultiselect,
     TagListEntry,
     TagCheck,
     TagLabel,
@@ -257,14 +258,15 @@ const PresetSax::StateMap &PresetSax::preset_state_map() {
 #if __cplusplus >= 201103L
     const std::vector<State> item_chunks = { TagChunk, TagItem };
     const std::vector<State> pr_gr = { TagPresets, TagGroup };
+    const std::vector<State> selectables = { TagCombo, TagMultiselect };
 # define VECTOR_ONE(a) { a }
 #else
-    std::vector<State> item_chunks(2);
-    item_chunks[0] = TagChunk;
+    std::vector<State> item_chunks(2, TagChunk);
     item_chunks[1] = TagItem;
-    std::vector<State> pr_gr(2);
-    pr_gr[0] = TagPresets;
+    std::vector<State> pr_gr(2, TagPresets);
     pr_gr[1] = TagGroup;
+    std::vector<State> selectables(2, TagCombo);
+    selectables[1] = TagMultiselect;
 # define VECTOR_ONE(a) std::vector<State>(1, (a))
 #endif
 
@@ -283,7 +285,8 @@ const PresetSax::StateMap &PresetSax::preset_state_map() {
     map.push_back(StateMap::value_type("key", TagKey, item_chunks));
     map.push_back(StateMap::value_type("text", TagText, item_chunks));
     map.push_back(StateMap::value_type("combo", TagCombo, item_chunks));
-    map.push_back(StateMap::value_type("list_entry", TagListEntry, VECTOR_ONE(TagCombo)));
+    map.push_back(StateMap::value_type("multiselect", TagMultiselect, item_chunks));
+    map.push_back(StateMap::value_type("list_entry", TagListEntry, selectables));
     map.push_back(StateMap::value_type("check", TagCheck, item_chunks));
     map.push_back(StateMap::value_type("label", TagLabel, item_chunks));
     map.push_back(StateMap::value_type("space", TagSpace, item_chunks));
@@ -757,11 +760,53 @@ void PresetSax::startElement(const char *name, const char **attrs)
                                       editable);
     break;
   }
+  case TagMultiselect: {
+    std::array<const char *, 8> names = { { "key", "text", "display_values", "match", "default", "delimiter", "values", "rows" } };
+    const AttrMap &a = findAttributes(attrs, names.data(), names.size(), 6);
+    const AttrMap::const_iterator aitEnd = a.end();
+
+    const std::string &key = NULL_OR_MAP_STR(a.find("key"));
+    const std::string &text = NULL_OR_MAP_STR(a.find("text"));
+    const std::string &def = NULL_OR_MAP_STR(a.find("default"));
+    const char *match = NULL_OR_MAP_VAL(a.find("match"));
+    const char *display_values= NULL_OR_MAP_VAL(a.find("display_values"));
+    const char *values = NULL_OR_MAP_VAL(a.find("values"));
+    const char *del = NULL_OR_MAP_VAL(a.find("delimiter"));
+    const char *rowstr = NULL_OR_MAP_VAL(a.find("rows"));
+
+    char delimiter = ';';
+    if(del != nullptr) {
+      if(unlikely(strlen(del) != 1))
+        dumpState("found", "combo with invalid delimiter ", del);
+      else
+        delimiter = *del;
+    }
+
+    if(unlikely(!values && display_values)) {
+      dumpState("found", "combo with display_values but not values");
+      display_values = nullptr;
+    }
+
+    unsigned int rows = 0;
+    if(rowstr != nullptr) {
+      char *endp;
+      rows = strtoul(rowstr, &endp, 10);
+      if(unlikely(*endp != '\0')) {
+        dumpState("ignoring invalid count value of", "role");
+        rows = 0;
+      }
+    }
+
+    widget = new presets_element_multiselect(key, text, def, match, delimiter,
+                                      presets_element_selectable::split_string(values, delimiter),
+                                      presets_element_selectable::split_string(display_values, delimiter), rows);
+    break;
+  }
   case TagListEntry: {
     assert(!items.empty());
     assert(!widgets.empty());
-    assert_cmpnum(widgets.top()->type, WIDGET_TYPE_COMBO);
-    presets_element_combo * const combo = static_cast<presets_element_combo *>(widgets.top());
+    assert(widgets.top()->type == WIDGET_TYPE_COMBO || widgets.top()->type == WIDGET_TYPE_MULTISELECT);
+    presets_element_selectable * const sel = static_cast<presets_element_selectable *>(widgets.top());
 
     std::array<const char *, 2> names = { { "display_value", "value" } };
     const AttrMap &a = findAttributes(attrs, names.data(), names.size(), 3);
@@ -772,12 +817,12 @@ void PresetSax::startElement(const char *name, const char **attrs)
     if(unlikely(!value)) {
       dumpState("found", "list_entry without value");
     } else {
-      combo->values.push_back(value);
+      sel->values.push_back(value);
       const char *dv = NULL_OR_MAP_VAL(a.find("display_value"));
       // make sure there is always a string to show, in case all elements have the
       // default value the list will be cleared when the containing widget tag is
       // closed
-      combo->display_values.push_back(dv == nullptr ? value : dv);
+      sel->display_values.push_back(dv == nullptr ? value : dv);
     }
     break;
   }
@@ -955,6 +1000,21 @@ void PresetSax::endElement(const xmlChar *name)
     // none of that has a display_value given
     if(unlikely(combo->values == combo->display_values))
       combo->display_values.clear();
+    break;
+  }
+  case TagMultiselect: {
+    assert(!items.empty());
+    assert(!widgets.empty());
+    presets_element_multiselect * const ms = static_cast<presets_element_multiselect *>(widgets.top());
+    widgets.pop();
+    if(unlikely(ms->key.empty())) {
+      dumpState("ignoring", "multiselect without key");
+      delete ms;
+    }
+    // this usually happens when the list if filled by <list_entry> tags and
+    // none of that has a display_value given
+    if(unlikely(ms->values == ms->display_values))
+      ms->display_values.clear();
     break;
   }
   case TagText:
@@ -1170,6 +1230,18 @@ presets_element_combo::presets_element_combo(const std::string &k, const std::st
                                            std::vector<std::string> vals, std::vector<std::string> dvals,
                                            bool canEdit)
   : presets_element_selectable(WIDGET_TYPE_COMBO, k, txt, deflt, m, vals, dvals, canEdit)
+{
+}
+
+presets_element_multiselect::presets_element_multiselect(const std::string &k, const std::string &txt,
+                                                         const std::string &deflt, const char *m, char del,
+                                                         std::vector<std::string> vals,
+                                                         std::vector<std::string> dvals, unsigned int rws)
+  : presets_element_selectable(WIDGET_TYPE_MULTISELECT, k, txt, deflt, m, vals, dvals, false)
+  , delimiter(del)
+#ifndef FREMANTLE
+  , rows_height(rws == 0 ? std::min(8, static_cast<int>(vals.size())) : rws)
+#endif
 {
 }
 

src/josm_presets.cpp
@@ -192,6 +192,52 @@ bool presets_element_combo::matchValue(const std::string &val) const
   return std::find(values.begin(), values.end(), val) != values.end();
 }
 
+std::vector<unsigned int> presets_element_multiselect::matchedIndexes(const std::string &preset) const
+{
+  std::vector<unsigned int> indexes;
+
+  if(preset.empty())
+    return indexes;
+
+  std::vector<std::string> parts;
+  std::string::size_type pos = preset.find(delimiter);
+  const std::vector<std::string>::const_iterator itEnd = values.end();
+  const std::vector<std::string>::const_iterator itBegin = values.begin();
+
+  std::vector<std::string>::const_iterator it;
+  if(pos == std::string::npos) {
+    it = std::find(itBegin, itEnd, preset);
+  } else {
+    // the first one
+    it = std::find(itBegin, itEnd, preset.substr(0, pos));
+    if(it != itEnd)
+      indexes.push_back(it - itBegin);
+    // skip first delimiter
+    pos++;
+    // all indermediate ones
+    std::string::size_type newpos = preset.find(delimiter, pos);
+    while(newpos != std::string::npos) {
+      it = std::find(itBegin, itEnd, preset.substr(pos, newpos - pos));
+      if(it != itEnd)
+        indexes.push_back(it - itBegin);
+      pos = newpos + 1;
+      newpos = preset.find(delimiter, pos);
+    }
+    // the last one
+    it = std::find(itBegin, itEnd, preset.substr(pos));
+  }
+
+  if(it != itEnd)
+    indexes.push_back(it - itBegin);
+
+  return indexes;
+}
+
+bool presets_element_multiselect::matchValue(const std::string &val) const
+{
+  return !matchedIndexes(val).empty();
+}
+
 bool presets_element_key::matchValue(const std::string &val) const
 {
   return (value == val);

src/josm_presets_p.h
@@ -35,6 +35,7 @@ enum presets_element_type_t {
   WIDGET_TYPE_SEPARATOR,
   WIDGET_TYPE_SPACE,
   WIDGET_TYPE_COMBO,
+  WIDGET_TYPE_MULTISELECT,
   WIDGET_TYPE_CHECK,
   WIDGET_TYPE_TEXT,
   WIDGET_TYPE_KEY,
@@ -192,6 +193,34 @@ public:
   presets_element_combo(const std::string &k, const std::string &txt,
                         const std::string &deflt, const char *m, std::vector<std::string> vals,
                         std::vector<std::string> dvals, bool canEdit);
+
+  virtual attach_key *attach(preset_attach_context &attctx, const std::string &preset) const override;
+  virtual std::string getValue(attach_key *akey) const override;
+  virtual unsigned int rows() const override {
+    return 1;
+  }
+};
+
+/**
+ * @brief a combo box with pre-defined values
+ */
+class presets_element_multiselect : public presets_element_selectable {
+protected:
+  /**
+   * @brief check which selection items are matched by preset
+   */
+  std::vector<unsigned int> matchedIndexes(const std::string &preset) const;
+
+  virtual bool matchValue(const std::string &val) const override;
+public:
+  presets_element_multiselect(const std::string &k, const std::string &txt,
+                              const std::string &deflt, const char *m, char del,
+                              std::vector<std::string> vals, std::vector<std::string> dvals, unsigned int rws);
+
+  const char delimiter;
+#ifndef FREMANTLE
+  const unsigned int rows_height;
+#endif
 
   virtual attach_key *attach(preset_attach_context &attctx, const std::string &preset) const override;
   virtual std::string getValue(attach_key *akey) const override;

src/platforms/gtk/josm_presets_button.cpp
@@ -233,6 +233,7 @@ void get_widget_functor::operator()(const presets_element_t* w)
   case WIDGET_TYPE_KEY:
   case WIDGET_TYPE_CHECK:
   case WIDGET_TYPE_COMBO:
+  case WIDGET_TYPE_MULTISELECT:
   case WIDGET_TYPE_TEXT:
     text = w->getValue(akey);
     break;
@@ -986,6 +987,38 @@ presets_element_t::attach_key *presets_element_combo::attach(preset_attach_conte
 }
 
 std::string presets_element_combo::getValue(presets_element_t::attach_key *akey) const
+{
+  return osm2go_platform::select_widget_value(reinterpret_cast<GtkWidget *>(akey));
+}
+
+presets_element_t::attach_key *presets_element_multiselect::attach(preset_attach_context &attctx,
+                                                                   const std::string &preset) const
+{
+  const std::string &pr = preset.empty() ? def : preset;
+
+  GtkListStore *store = selectorModel(values, display_values);
+  GtkWidget *ret = osm2go_platform::select_widget_wrapped(text.c_str(), GTK_TREE_MODEL(store),
+                                                          osm2go_platform::AllowMultiSelection,
+                                                          &delimiter);
+  g_object_unref(store);
+
+  const std::vector<unsigned int> &indexes = matchedIndexes(pr);
+
+  select_widget_select(ret, indexes);
+
+#ifndef FREMANTLE
+  // arbitrary number for height scaling
+  gtk_widget_set_size_request(ret, -1, rows_height * 24);
+
+  attach_right(attctx, text.c_str(), ret);
+#else
+  attach_both(attctx, ret);
+#endif
+
+  return reinterpret_cast<presets_element_t::attach_key *>(ret);
+}
+
+std::string presets_element_multiselect::getValue(presets_element_t::attach_key *akey) const
 {
   return osm2go_platform::select_widget_value(reinterpret_cast<GtkWidget *>(akey));
 }

test/presets_classes.cpp
@@ -164,6 +164,111 @@ static void checkComboMatch()
   assert_cmpnum(w_kvf.matches(osm_t::TagMap()), -1);
 }
 
+static void checkMultiMatch()
+{
+  static osm_t::TagMap::value_type tag_first("testkey", tag_testkey_testtext.second + ";abc");
+  static osm_t::TagMap::value_type tag_middle("testkey", "abc;" + tag_testkey_testtext.second + ";abc");
+  static osm_t::TagMap::value_type tag_last("testkey", "abc;" + tag_testkey_testtext.second);
+  std::vector<std::string> values;
+  values.push_back("nonmatch");
+  values.push_back("");
+  values.push_back(tag_testkey_testtext.second);
+  values.push_back("another nonmatch");
+  std::vector<std::string> empty_vector;
+  const std::vector<std::string> backup = values;
+
+  presets_element_multiselect w_0(tag_testkey_testtext.first,
+                           "visual text",
+                           values.front(),
+                           nullptr, ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_testkey_other)), 0);
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_testkey_testtext)), 0);
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_testkey_other)), 0);
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_first)), 0);
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_middle)), 0);
+  assert_cmpnum(w_0.matches(VECTOR_ONE(tag_last)), 0);
+  assert_cmpnum(w_0.matches(osm_t::TagMap()), 0);
+
+  values = backup;
+  presets_element_multiselect w_ign(tag_testkey_testtext.first,
+                             "visual text",
+                             values.front(),
+                             "none", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_ign.matches(VECTOR_ONE(tag_testkey_other)), 0);
+  assert_cmpnum(w_ign.matches(VECTOR_ONE(tag_testkey_testtext)), 0);
+  assert_cmpnum(w_ign.matches(VECTOR_ONE(tag_first)), 0);
+  assert_cmpnum(w_ign.matches(VECTOR_ONE(tag_middle)), 0);
+  assert_cmpnum(w_ign.matches(VECTOR_ONE(tag_last)), 0);
+  assert_cmpnum(w_ign.matches(osm_t::TagMap()), 0);
+
+  values = backup;
+  presets_element_multiselect w_bad(tag_testkey_testtext.first,
+                             "visual text",
+                             values.front(),
+                             "nonsense", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_bad.matches(VECTOR_ONE(tag_testkey_other)), 0);
+  assert_cmpnum(w_bad.matches(VECTOR_ONE(tag_testkey_testtext)), 0);
+  assert_cmpnum(w_bad.matches(VECTOR_ONE(tag_first)), 0);
+  assert_cmpnum(w_bad.matches(VECTOR_ONE(tag_middle)), 0);
+  assert_cmpnum(w_bad.matches(VECTOR_ONE(tag_last)), 0);
+  assert_cmpnum(w_bad.matches(osm_t::TagMap()), 0);
+
+  values = backup;
+  presets_element_multiselect w_key(tag_testkey_testtext.first,
+                             "visual text",
+                             values.front(),
+                             "key", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_key.matches(VECTOR_ONE(tag_testkey_other)), 1);
+  assert_cmpnum(w_key.matches(VECTOR_ONE(tag_testkey_testtext)), 1);
+  assert_cmpnum(w_key.matches(VECTOR_ONE(tag_first)), 1);
+  assert_cmpnum(w_key.matches(VECTOR_ONE(tag_middle)), 1);
+  assert_cmpnum(w_key.matches(VECTOR_ONE(tag_last)), 1);
+  assert_cmpnum(w_key.matches(osm_t::TagMap()), 0);
+
+  values = backup;
+  presets_element_multiselect w_keyf(tag_testkey_testtext.first,
+                              "visual text",
+                              values.front(),
+                              "key!", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_keyf.matches(VECTOR_ONE(tag_testkey_other)), 1);
+  assert_cmpnum(w_keyf.matches(VECTOR_ONE(tag_testkey_testtext)), 1);
+  assert_cmpnum(w_keyf.matches(VECTOR_ONE(tag_first)), 1);
+  assert_cmpnum(w_keyf.matches(VECTOR_ONE(tag_middle)), 1);
+  assert_cmpnum(w_keyf.matches(VECTOR_ONE(tag_last)), 1);
+  assert_cmpnum(w_keyf.matches(osm_t::TagMap()), -1);
+
+  values = backup;
+  presets_element_multiselect w_kv(tag_testkey_testtext.first,
+                            "visual text",
+                            values.front(),
+                            "keyvalue", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_kv.matches(VECTOR_ONE(tag_testkey_other)), 0);
+  assert_cmpnum(w_kv.matches(VECTOR_ONE(tag_testkey_testtext)), 1);
+  assert_cmpnum(w_kv.matches(VECTOR_ONE(tag_first)), 1);
+  assert_cmpnum(w_kv.matches(VECTOR_ONE(tag_middle)), 1);
+  assert_cmpnum(w_kv.matches(VECTOR_ONE(tag_last)), 1);
+  assert_cmpnum(w_kv.matches(osm_t::TagMap()), 0);
+
+  values = backup;
+  presets_element_multiselect w_kvf(tag_testkey_testtext.first,
+                             "visual text",
+                             values.front(),
+                             "keyvalue!", ';', values, empty_vector, 0);
+
+  assert_cmpnum(w_kvf.matches(VECTOR_ONE(tag_testkey_other)), -1);
+  assert_cmpnum(w_kvf.matches(VECTOR_ONE(tag_testkey_testtext)), 1);
+  assert_cmpnum(w_kvf.matches(VECTOR_ONE(tag_first)), 1);
+  assert_cmpnum(w_kvf.matches(VECTOR_ONE(tag_middle)), 1);
+  assert_cmpnum(w_kvf.matches(VECTOR_ONE(tag_last)), 1);
+  assert_cmpnum(w_kvf.matches(osm_t::TagMap()), -1);
+}
+
 static void check_combined()
 {
   presets_item item(presets_item_t::TY_ALL);
@@ -214,10 +319,64 @@ static void check_combined()
   assert(!item.matches(tags));
 }
 
+class presets_element_multiselect_test: public presets_element_multiselect {
+public:
+  presets_element_multiselect_test(char del, std::vector<std::string> vals)
+    : presets_element_multiselect("key", "text", std::string(), nullptr, del,
+                                  vals, std::vector<std::string>(), 0) {}
+
+  inline std::vector<unsigned int> matchTest(const std::string &preset) const
+  { return matchedIndexes(preset); }
+};
+
+static void checkMultiSplit()
+{
+  std::vector<std::string> values;
+  values.push_back("aaa");
+  values.push_back("bbb");
+  values.push_back("ccc");
+  values.push_back("ddd");
+  const presets_element_multiselect_test test1(';', values);
+
+  for(unsigned int i = 0; i < values.size(); i++) {
+    std::vector<unsigned int> res = test1.matchTest(values[i]);
+    assert_cmpnum(res.size(), 1);
+    assert_cmpnum(res[0], i);
+
+    std::string s = ';' + values[i];
+    res = test1.matchTest(s);
+    assert_cmpnum(res.size(), 1);
+    assert_cmpnum(res[0], i);
+
+    s = values[i] + ';';
+    res = test1.matchTest(s);
+    assert_cmpnum(res.size(), 1);
+    assert_cmpnum(res[0], i);
+  }
+
+  std::vector<unsigned int> res = test1.matchTest("aa");
+  assert_cmpnum(res.size(), 0);
+  res = test1.matchTest("bb");
+  assert_cmpnum(res.size(), 0);
+  res = test1.matchTest("bb;cc");
+  assert_cmpnum(res.size(), 0);
+
+  res = test1.matchTest("aaa;ddd");
+  assert_cmpnum(res.size(), 2);
+  assert_cmpnum(res[0], 0);
+  assert_cmpnum(res[1], 3);
+
+  res = test1.matchTest("aa;ddd;f");
+  assert_cmpnum(res.size(), 1);
+  assert_cmpnum(res[0], 3);
+}
+
 int main()
 {
   checkTextMatch();
   checkComboMatch();
+  checkMultiMatch();
+  checkMultiSplit();
   check_combined();
 
   return 0;

test/presets_load.cpp
@@ -64,17 +64,18 @@ struct counter {
   unsigned int &items;
   unsigned int &separators;
   unsigned int &combos;
-  unsigned int &combo_entries;
+  unsigned int &multis;
+  unsigned int &list_entries;
   unsigned int &labels;
   unsigned int &keys;
   unsigned int &checks;
   unsigned int &refs;
   unsigned int &plinks;
   unsigned int &roles;
-  counter(unsigned int &gr, unsigned int &it, unsigned int &sep, unsigned int &c,
-          unsigned int &ce, unsigned int &lb, unsigned int &ky,  unsigned int &chk,
-          unsigned int &rf, unsigned int &pl, unsigned int &rl)
-    : groups(gr), items(it), separators(sep), combos(c), combo_entries(ce),
+  counter(unsigned int &gr,  unsigned int &it, unsigned int &sep, unsigned int &c,
+          unsigned int &mu,  unsigned int &ce, unsigned int &lb,  unsigned int &ky,
+          unsigned int &chk, unsigned int &rf, unsigned int &pl,  unsigned int &rl)
+    : groups(gr), items(it), separators(sep), combos(c), multis(mu), list_entries(ce),
       labels(lb), keys(ky), checks(chk), refs(rf), plinks(pl), roles(rl) {}
   void operator()(const presets_item_t *p);
   void operator()(const presets_element_t *w);
@@ -114,9 +115,16 @@ void counter::operator()(const presets_element_t *w)
     break;
   case WIDGET_TYPE_COMBO: {
     combos++;
-    const presets_element_combo *combo = static_cast<const presets_element_combo *>(w);
-    combo_entries += combo->values.size();
-    std::for_each(combo->display_values.begin(), combo->display_values.end(), display_value_verifactor);
+    const presets_element_selectable *sel = static_cast<const presets_element_selectable *>(w);
+    list_entries += sel->values.size();
+    std::for_each(sel->display_values.begin(), sel->display_values.end(), display_value_verifactor);
+    break;
+  }
+  case WIDGET_TYPE_MULTISELECT: {
+    multis++;
+    const presets_element_selectable *sel = static_cast<const presets_element_selectable *>(w);
+    list_entries += sel->values.size();
+    std::for_each(sel->display_values.begin(), sel->display_values.end(), display_value_verifactor);
     break;
   }
   case WIDGET_TYPE_CHECK:
@@ -247,14 +255,15 @@ int main(int argc, char **argv)
   unsigned int items = 0;
   unsigned int separators = 0;
   unsigned int combos = 0;
-  unsigned int combo_entries = 0;
+  unsigned int multis = 0;
+  unsigned int list_entries = 0;
   unsigned int labels = 0;
   unsigned int keys = 0;
   unsigned int checks = 0;
   unsigned int refs = 0;
   unsigned int plinks = 0;
   unsigned int roles = 0;
-  counter cnt(groups, items, separators, combos, combo_entries, labels, keys, checks, refs, plinks, roles);
+  counter cnt(groups, items, separators, combos, multis, list_entries, labels, keys, checks, refs, plinks, roles);
 
   std::for_each(presets->items.begin(), presets->items.end(), cnt);
   std::for_each(presets->chunks.begin(), presets->chunks.end(), cnt);
@@ -266,7 +275,8 @@ int main(int argc, char **argv)
     << "items: " << items << std::endl
     << "separators: " << separators << std::endl
     << "combos: " << combos << std::endl
-    << "combo_entries: " << combo_entries << std::endl
+    << "multis: " << multis << std::endl
+    << "list_entries: " << list_entries << std::endl
     << "labels: " << labels << std::endl
     << "keys: " << keys << std::endl
     << "checks: " << checks << std::endl

commit 61e31385da032a8eaabe0605ffdfa1922e0112f0

allocate enough room in map for JOSM preset tags
Fixes: e41376e247b1f33c287099a67c6dd0f78b6be220

src/josm_presets_parser.cpp
@@ -273,7 +273,7 @@ const PresetSax::StateMap &PresetSax::preset_state_map() {
 # define VECTOR_ONE(a) std::vector<State>(1, (a))
 #endif
 
-    map.reserve(19);
+    map.reserve(20);
 
     map.push_back(StateMap::value_type("presets", TagPresets, VECTOR_ONE(DocStart)));
     map.push_back(StateMap::value_type("chunk", TagChunk, VECTOR_ONE(TagPresets)));