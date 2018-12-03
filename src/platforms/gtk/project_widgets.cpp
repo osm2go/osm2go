osm2go/osm2go

commit 92acf78e32d2fcf54472f47a4023837ee97ce458

simplify view_selected()

src/platforms/gtk/project_widgets.cpp
@@ -151,7 +151,7 @@ enum {
 static void view_selected(GtkWidget *dialog, const project_t *project)
 {
   /* check if the selected project also has a valid osm file */
-  gboolean has_osm = (project != nullptr && project->osm_file_exists()) ? TRUE : FALSE;
+  gboolean has_osm = project->osm_file_exists() ? TRUE : FALSE;
   gtk_dialog_set_response_sensitive(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT, has_osm);
 }
 
@@ -166,6 +166,7 @@ changed(GtkTreeSelection *selection, gpointer userdata) {
   if(sel) {
     project_t *project = nullptr;
     gtk_tree_model_get(model, &iter, PROJECT_COL_DATA, &project, -1);
+    assert(project != nullptr);
 
     view_selected(context->dialog, project);
   }
@@ -368,7 +369,7 @@ static void on_project_delete(select_context_t *context) {
   project_delete(project);
 
   /* disable ok button button */
-  view_selected(context->dialog, nullptr);
+  gtk_dialog_set_response_sensitive(GTK_DIALOG(context->dialog), GTK_RESPONSE_ACCEPT, FALSE);
 }
 
 static void on_project_edit(select_context_t *context) {

commit a64d5717debb8400ce7ae1533c2e2d1bd82536ee

fix crash when cancelling project creation
Fixes: 92acf78e32d2fcf54472f47a4023837ee97ce458

src/platforms/gtk/project_widgets.cpp
@@ -308,6 +308,8 @@ static project_t *project_new(select_context_t *context) {
   if(!project->save(context->dialog) || !project_edit(context, project.get(), true)) {
     g_debug("creation of project '%s' cancelled, deleting", project->name.c_str());
     project_delete(project.release());
+    gtk_dialog_set_response_sensitive(GTK_DIALOG(context->dialog), GTK_RESPONSE_ACCEPT, FALSE);
+    return nullptr;
   }
 
   /* enable/disable edit/remove buttons */