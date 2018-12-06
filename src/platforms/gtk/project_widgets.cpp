osm2go/osm2go

commit 2b6b88e79ef49352501ce4fcb09877583877625e

allow filtering in project selection

src/platforms/gtk/project_widgets.cpp
@@ -486,6 +486,35 @@ void project_list_add::operator()(const project_t *project)
   }
 }
 
+static gboolean
+project_matches(GtkTreeModel *model, GtkTreeIter *iter, gpointer data)
+{
+  gchar *pname, *pdescr;
+
+  GtkEntry *entry = static_cast<GtkEntry *>(data);
+  const gchar *text = gtk_entry_get_text(entry);
+  if(text == nullptr || *text == '\0')
+    return TRUE;
+
+  gtk_tree_model_get(model, iter, PROJECT_COL_NAME, &pname,
+                                  PROJECT_COL_DESCRIPTION, &pdescr, -1);
+  g_string name(pname);
+  g_string descr(pdescr);
+
+  if(name && strcasestr(name.get(), text) != nullptr)
+    return TRUE;
+  if(descr && strcasestr(descr.get(), text) != nullptr)
+    return TRUE;
+
+  return FALSE;
+}
+
+static void
+on_filter_changed(gpointer user_data)
+{
+  gtk_tree_model_filter_refilter(static_cast<GtkTreeModelFilter *>(user_data));
+}
+
 /**
  * @brief create a widget to list the projects
  * @param context the context struct
@@ -510,8 +539,11 @@ static GtkWidget *project_list_widget(select_context_t &context, bool &has_sel) 
                                          G_TYPE_STRING,    // desc
                                          G_TYPE_POINTER));  // data
 
+  GtkTreeModelFilter *filter = reinterpret_cast<GtkTreeModelFilter *>(
+            gtk_tree_model_filter_new(GTK_TREE_MODEL(context.store.get()), nullptr));
+
   context.list = list_new(LIST_HILDON_WITHOUT_HEADERS, 0, &context, changed,
-                          buttons, columns, GTK_TREE_MODEL(context.store.get()));
+                          buttons, columns, GTK_TREE_MODEL(filter));
 
   GtkTreeIter seliter;
   std::for_each(context.projects.begin(), context.projects.end(),
@@ -522,6 +554,14 @@ static GtkWidget *project_list_widget(select_context_t &context, bool &has_sel) 
 
   if(has_sel)
     list_scroll(context.list, &seliter);
+
+  GtkWidget *entry = osm2go_platform::entry_new(osm2go_platform::EntryFlagsNoAutoCap);
+  gtk_box_pack_start(GTK_BOX(context.list), entry, FALSE, FALSE, 0);
+  gtk_box_reorder_child(GTK_BOX(context.list), entry, 1);
+  gtk_tree_model_filter_set_visible_func(filter, project_matches, entry, nullptr);
+  gtk_widget_grab_focus(entry);
+
+  g_signal_connect_swapped(entry, "changed", G_CALLBACK(on_filter_changed), filter);
 
   return context.list;
 }

commit 523aee3bbf5c82835e54971abea93c484f8e45cf

fix automatic selection of project entries
When a new project or the project with the current GPS position should be
selected  the iterator for the wrong treeview was used.

Fixes: 2b6b88e79ef49352501ce4fcb09877583877625e

src/platforms/gtk/project_widgets.cpp
@@ -348,7 +348,7 @@ static void on_project_new(select_context_t *context) {
   if(project != nullptr) {
     context->projects.push_back(project);
 
-    GtkTreeIter iter;
+    GtkTreeIter iter, fiter;
     const gchar *status_stock_id = project_get_status_icon_stock_id(context->appdata.project,
                                                                     project);
     gtk_list_store_insert_with_values(GTK_LIST_STORE(context->store.get()), &iter,
@@ -360,7 +360,8 @@ static void on_project_new(select_context_t *context) {
                                       -1);
 
     GtkTreeSelection *selection = list_get_selection(context->list);
-    gtk_tree_selection_select_iter(selection, &iter);
+    if(gtk_tree_model_filter_convert_child_iter_to_iter(context->filter, &fiter, &iter) == TRUE)
+      gtk_tree_selection_select_iter(selection, &fiter);
   }
 }
 
@@ -579,8 +580,11 @@ static GtkWidget *project_list_widget(select_context_t &context, bool &has_sel) 
   gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(context.store.get()),
                                        PROJECT_COL_NAME, GTK_SORT_ASCENDING);
 
-  if(has_sel)
-    list_scroll(context.list, &seliter);
+  if(has_sel) {
+    GtkTreeIter fiter;
+    if(gtk_tree_model_filter_convert_child_iter_to_iter(context.filter, &fiter, &seliter) == TRUE)
+      list_scroll(context.list, &fiter);
+  }
 
   GtkWidget *entry = osm2go_platform::entry_new(osm2go_platform::EntryFlagsNoAutoCap);
   gtk_box_pack_start(GTK_BOX(context.list), entry, FALSE, FALSE, 0);