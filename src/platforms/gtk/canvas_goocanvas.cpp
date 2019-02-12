osm2go/osm2go

commit 03e329dc62a8b82ff364361feeea7265fa48d0ee

get rid of osm2go_platform::Pixmap
The main point is to let canvas_t::image_new() directly deal with icon_items.
The code to convert that back to a pixmap is now in the platform specific code,
so other platforms are free to use entirely different things.

The idea behind that is to allow the canvas_item to make better use of SVG icons
instead of rasterizing them right at the start, which destroys every possible
visual improvement during scaling.

src/platforms/gtk/canvas_goocanvas.cpp
@@ -48,6 +48,7 @@
 
 #include <osm2go_annotations.h>
 #include <osm2go_cpp.h>
+#include "osm2go_platform_gtk.h"
 #include "osm2go_stl.h"
 #include "osm2go_platform_gtk.h"
 
@@ -568,8 +569,9 @@ canvas_item_t *canvas_t::polygon_new(canvas_group_t group, const std::vector<lpo
 }
 
 /* place the image in pix centered on x/y on the canvas */
-canvas_item_pixmap *canvas_t::image_new(canvas_group_t group, osm2go_platform::Pixmap pix, int x,
+canvas_item_pixmap *canvas_t::image_new(canvas_group_t group, icon_item *icon, int x,
                                    int y, float hscale, float vscale) {
+  GdkPixbuf *pix = osm2go_platform::icon_pixmap(icon);
   int width = gdk_pixbuf_get_width(pix);
   int height = gdk_pixbuf_get_height(pix);
   GooCanvasItem *item =

src/canvas.h
@@ -65,6 +65,7 @@ typedef enum {
 #endif
 
 class canvas_item_info_t;
+class icon_item;
 struct map_item_t;
 
 struct canvas_item_t {
@@ -160,7 +161,7 @@ public:
   canvas_item_t *polygon_new(canvas_group_t group, const std::vector<lpos_t> &points,
                              unsigned int width, color_t color,
                              color_t fill);
-  canvas_item_pixmap *image_new(canvas_group_t group, osm2go_platform::Pixmap pix, int x, int y,
+  canvas_item_pixmap *image_new(canvas_group_t group, icon_item *icon, int x, int y,
                            float hscale, float vscale);
 
   void item_info_push(canvas_item_t *item);

src/icon.h
@@ -24,19 +24,17 @@
 
 #include <osm2go_platform.h>
 
+class icon_item {
+protected:
+  inline icon_item() {}
+public:
+  int maxDimension() const;
+};
+
 class icon_t {
 protected:
   inline icon_t() {}
 public:
-  class icon_item {
-  protected:
-    inline icon_item() {}
-  public:
-    osm2go_platform::Pixmap buffer();
-
-    int maxDimension() const;
-  };
-
   static icon_t &instance();
   ~icon_t();
 

src/josm_elemstyles.cpp
@@ -550,7 +550,7 @@ void colorize_node::operator()(const elemstyle_t *elemstyle)
   name += '/';
   name += elemstyle->icon.filename;
 
-  icon_t::icon_item *buf = icons.load(name);
+  icon_item *buf = icons.load(name);
 
   /* Free old icon if there's one present, but only after loading (not
    * assigning!) the new one. In case the old and new icon are the same

src/map.cpp
@@ -388,7 +388,7 @@ static void map_node_new(map_t *map, node_t *node, unsigned int radius,
     map_item->item = map->canvas->circle_new(CANVAS_GROUP_NODES,
        node->lpos.x, node->lpos.y, radius, width, fill, border);
   else
-    map_item->item = map->canvas->image_new(CANVAS_GROUP_NODES, it->second->buffer(),
+    map_item->item = map->canvas->image_new(CANVAS_GROUP_NODES, it->second,
                                             node->lpos.x, node->lpos.y,
 		      map->state.detail * map->style->icon.scale,
 		      map->state.detail * map->style->icon.scale);

src/platforms/gtk/icon.cpp
@@ -37,27 +37,27 @@
 #include "osm2go_platform_gtk.h"
 #include <osm2go_stl.h>
 
+class icon_buffer_item : public icon_item {
+public:
+  explicit icon_buffer_item(GdkPixbuf *nbuf);
+
+  std::unique_ptr<GdkPixbuf, g_object_deleter> buf;
+  int use;
+
+  inline GdkPixbuf *buffer() const {
+    return buf.get();
+  }
+};
+
 class icon_buffer : public icon_t {
 public:
-  class icon_buffer_item : public icon_item {
-  public:
-    explicit icon_buffer_item(osm2go_platform::Pixmap nbuf);
-
-    std::unique_ptr<GdkPixbuf, g_object_deleter> buf;
-    int use;
-
-    inline osm2go_platform::Pixmap buffer() {
-      return buf.get();
-    }
-  };
-
   ~icon_buffer();
 
   typedef std::unordered_map<std::string, icon_buffer_item *> BufferMap;
   BufferMap entries;
 };
 
-icon_buffer::icon_buffer_item::icon_buffer_item(osm2go_platform::Pixmap nbuf)
+icon_buffer_item::icon_buffer_item(GdkPixbuf *nbuf)
   : buf(nbuf)
   , use(nbuf != nullptr ? 1 : 0)
 {
@@ -95,7 +95,8 @@ icon_file_exists(const std::string &file) {
   return std::string();
 }
 
-icon_t::icon_item *icon_t::load(const std::string &sname, int limit) {
+icon_item *icon_t::load(const std::string &sname, int limit)
+{
   if(sname.empty())
     return nullptr;
 
@@ -111,11 +112,11 @@ icon_t::icon_item *icon_t::load(const std::string &sname, int limit) {
 
   const std::string &fullname = icon_file_exists(sname);
   if(!fullname.empty()) {
-    osm2go_platform::Pixmap pix = gdk_pixbuf_new_from_file_at_size(fullname.c_str(), limit, limit, nullptr);
+    GdkPixbuf *pix = gdk_pixbuf_new_from_file_at_size(fullname.c_str(), limit, limit, nullptr);
 
     if(likely(pix)) {
       //    g_debug("Successfully loaded icon %s to %p", name, pix);
-      icon_buffer::icon_buffer_item *ret = new icon_buffer::icon_buffer_item(pix);
+      icon_buffer_item *ret = new icon_buffer_item(pix);
       entries[sname] = ret;
       return ret;
     }
@@ -130,28 +131,28 @@ GtkWidget *icon_t::widget_load(const std::string &name, int limit) {
   if(pix == nullptr)
     return nullptr;
 
-  return gtk_image_new_from_pixbuf(pix->buffer());
+  return gtk_image_new_from_pixbuf(static_cast<icon_buffer_item *>(pix)->buffer());
 }
 
-osm2go_platform::Pixmap icon_t::icon_item::buffer()
+int icon_item::maxDimension() const
 {
-  return static_cast<icon_buffer::icon_buffer_item *>(this)->buffer();
-}
-
-int icon_t::icon_item::maxDimension() const
-{
-  const osm2go_platform::Pixmap buf = const_cast<icon_t::icon_item *>(this)->buffer();
+  const GdkPixbuf *buf = static_cast<const icon_buffer_item *>(this)->buffer();
   return std::max(gdk_pixbuf_get_height(buf), gdk_pixbuf_get_width(buf));
 }
 
-static inline void icon_destroy_pair(std::pair<const std::string, icon_buffer::icon_buffer_item *> &pair) {
+GdkPixbuf *osm2go_platform::icon_pixmap(const icon_item *icon)
+{
+  return static_cast<const icon_buffer_item *>(icon)->buffer();
+}
+
+static inline void icon_destroy_pair(std::pair<const std::string, icon_buffer_item *> &pair) {
   delete pair.second;
 }
 
 struct find_icon_buf {
-  const icon_t::icon_item * const buf;
-  explicit find_icon_buf(const icon_t::icon_item *b) : buf(b) {}
-  bool operator()(const std::pair<std::string, icon_t::icon_item *> &pair) {
+  const icon_item * const buf;
+  explicit find_icon_buf(const icon_item *b) : buf(b) {}
+  bool operator()(const std::pair<std::string, icon_item *> &pair) {
     return pair.second == buf;
   }
 };

src/platforms/gtk/josm_presets_button.cpp
@@ -639,12 +639,12 @@ static GtkWidget *presets_picker_embed(GtkTreeView *view, GtkListStore *store,
 static GtkTreeIter preset_insert_item(const presets_item_named *item, icon_t &icons,
                                       GtkListStore *store) {
   /* icon load can cope with empty string as name (returns nullptr then) */
-  icon_t::icon_item *icon = icons.load(item->icon, 16);
+  icon_item *icon = icons.load(item->icon, 16);
 
   /* Append a row and fill in some data */
   GtkTreeIter iter;
 
-  osm2go_platform::Pixmap pixmap = icon == nullptr ? nullptr : icon->buffer();
+  GdkPixbuf *pixmap = icon == nullptr ? nullptr : osm2go_platform::icon_pixmap(icon);
 
   gtk_list_store_insert_with_values(store, &iter, -1,
                                     PRESETS_PICKER_COL_ICON, pixmap,
@@ -713,8 +713,9 @@ struct picker_add_functor {
   GdkPixbuf * const subicon;
   bool &show_recent;
   bool scan_for_recent;
-  picker_add_functor(presets_context_t *c, GtkListStore *s, icon_t::icon_item *i, bool r, bool &w)
-    : context(c), store(s), subicon(i->buffer()), show_recent(w), scan_for_recent(r) {}
+  picker_add_functor(presets_context_t *c, GtkListStore *s, GdkPixbuf *i, bool r, bool &w)
+    : context(c), store(s), subicon(i), show_recent(w)
+    , scan_for_recent(r) {}
   void operator()(const presets_item_t *item);
 };
 
@@ -771,8 +772,9 @@ presets_context_t::presets_picker(const std::vector<presets_item_t *> &items,
   GtkListStore *store = presets_picker_store(&view);
 
   bool show_recent = false;
-  icon_t::icon_item *subicon = icons.load("submenu_arrow");
-  picker_add_functor fc(this, store, subicon, top_level, show_recent);
+  icon_item *subicon = icons.load("submenu_arrow");
+  GdkPixbuf *subpix = osm2go_platform::icon_pixmap(subicon);
+  picker_add_functor fc(this, store, subpix, top_level, show_recent);
 
   std::for_each(items.begin(), items.end(), fc);
   const std::vector<presets_item_t *> &lru = static_cast<const presets_items_internal *>(presets)->lru;
@@ -781,13 +783,13 @@ presets_context_t::presets_picker(const std::vector<presets_item_t *> &items,
      std::find_if(lru.begin(), lru.end(), matching_type_functor(presets_mask)) != lru.end())
     gtk_list_store_insert_with_values(store, nullptr, 0,
                        PRESETS_PICKER_COL_NAME, _("Last used presets"),
-                       PRESETS_PICKER_COL_SUBMENU_ICON, subicon->buffer(),
+                       PRESETS_PICKER_COL_SUBMENU_ICON, subpix,
                        -1);
 
   if(show_recent)
     gtk_list_store_insert_with_values(store, nullptr, 0,
                        PRESETS_PICKER_COL_NAME, _("Used presets"),
-                       PRESETS_PICKER_COL_SUBMENU_ICON, subicon->buffer(),
+                       PRESETS_PICKER_COL_SUBMENU_ICON, subpix,
                        -1);
 
   icons.icon_free(subicon);

src/platforms/gtk/main.cpp
@@ -1160,7 +1160,7 @@ static int application_run(const char *proj)
   appdata.set_title();
   /* Set a decent default size for the window. */
   gtk_window_set_default_size(GTK_WINDOW(appdata_t::window), DEFAULT_WIDTH, DEFAULT_HEIGHT);
-  gtk_window_set_icon(GTK_WINDOW(appdata_t::window), appdata.icons.load(PACKAGE)->buffer());
+  gtk_window_set_icon(GTK_WINDOW(appdata_t::window), osm2go_platform::icon_pixmap(appdata.icons.load(PACKAGE)));
 #endif
 
   g_signal_connect_swapped(appdata_t::window, "key_press_event",

commit ccb5ed053448dac0703dfb0430bd5fcb552ae049

drop duplicate include
Fixes: 03e329dc62a8b82ff364361feeea7265fa48d0ee

src/platforms/gtk/canvas_goocanvas.cpp
@@ -50,7 +50,6 @@
 #include <osm2go_cpp.h>
 #include "osm2go_platform_gtk.h"
 #include "osm2go_stl.h"
-#include "osm2go_platform_gtk.h"
 
 struct canvas_points_deleter {
   void operator()(void *ptr) {