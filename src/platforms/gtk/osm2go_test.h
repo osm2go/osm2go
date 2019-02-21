a0ca7d0985a040ba51db912f3e3653e7fff45e52
Rolf Eike Beer <eike@sf-mail.de>
2019-02-21 22:34:40 +0100

    tests: properly destroy the canvas by releasing it's widget

--- src/platforms/gtk/osm2go_test.h
+++ src/platforms/gtk/osm2go_test.h
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <canvas.h>
+#include "osm2go_platform_gtk.h"
+
 #include <glib.h>
 #include <glib-object.h>
+#include <memory>
+
+class canvas_t;
+extern canvas_t *canvas_t_create();
 
 #if !GLIB_CHECK_VERSION(2,36,0)
 #define OSM2GO_TEST_INIT(argc, argv) g_type_init();(void)argc;(void)argv
 #else
 #define OSM2GO_TEST_INIT(argc, argv) (void)argc;(void)argv
 #endif
+
+class canvas_holder {
+  canvas_t * const c;
+  std::unique_ptr<GtkWidget, g_object_deleter> w;
+public:
+  explicit canvas_holder()
+    : c(canvas_t_create())
+    , w(static_cast<GtkWidget *>(g_object_ref_sink(c->widget)))
+  {
+  }
+
+  inline canvas_t *operator->() { return c; }
+};
--- test/canvas_points.cpp
+++ test/canvas_points.cpp
@@ -5,8 +5,7 @@
 
 #include <osm2go_annotations.h>
 #include <osm2go_stl.h>
-
-extern canvas_t *canvas_t_create();
+#include <osm2go_test.h>
 
 static void testSegment()
 {
@@ -14,24 +13,23 @@ static void testSegment()
   for (unsigned int i = 0; i < 8; i += 2)
     points.push_back(lpos_t(1 << i, 2 << i));
 
-  std::unique_ptr<canvas_t> canvas(canvas_t_create());
-  assert(canvas);
+  canvas_holder canvas;
 
-  std::unique_ptr<canvas_item_t> line(canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0));
+  canvas_item_t * const line = canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0);
   assert(line);
 
-  int segnum = canvas->get_item_segment(line.get(), lpos_t((4 + 16) / 2, (8 + 32) / 2));
+  int segnum = canvas->get_item_segment(line, lpos_t((4 + 16) / 2, (8 + 32) / 2));
   assert_cmpnum(segnum, 1);
 }
 
 static void testInObject()
 {
-  std::unique_ptr<canvas_t> canvas(canvas_t_create());
-  assert(canvas);
+  canvas_holder canvas;
 
   // a circle that should have nothing to do with the initial search
-  std::unique_ptr<canvas_item_circle> circle(canvas->circle_new(CANVAS_GROUP_WAYS, 100, 20, 15,
-                                                                0, color_t::transparent()));
+  canvas_item_circle * const circle = canvas->circle_new(CANVAS_GROUP_WAYS, 100, 20, 15,
+                                                         0, color_t::transparent());
+  assert(circle != nullptr);
 
   // a square, rotated by 45 degrees
   std::vector<lpos_t> points;
@@ -41,11 +39,11 @@ static void testInObject()
   points.push_back(lpos_t(200, 0));
   points.push_back(points.front());
 
-  std::unique_ptr<canvas_item_t> line(canvas->polygon_new(CANVAS_GROUP_WAYS, points, 1, 0, 0));
-  assert(line);
+  canvas_item_t * const line = canvas->polygon_new(CANVAS_GROUP_WAYS, points, 1, 0, 0);
+  assert(line != nullptr);
 
   canvas_item_t *search = canvas->get_item_at(lpos_t(200, 200));
-  assert(line.get() == search);
+  assert(line == search);
 
   search = canvas->get_item_at(lpos_t(40, 50));
   assert_null(search);
@@ -54,7 +52,7 @@ static void testInObject()
   // the given position is slightly outside the circle, but the fuzziness
   // should still catch it
   search = canvas->get_item_at(lpos_t(100, 38));
-  assert(circle.get() == search);
+  assert(circle == search);
 }
 
 static void testToBottom()
@@ -63,15 +61,14 @@ static void testToBottom()
   for (unsigned int i = 0; i < 3; i += 2)
     points.push_back(lpos_t(1 << i, 2 << i));
 
-  std::unique_ptr<canvas_t> canvas(canvas_t_create());
-  assert(canvas);
+  canvas_holder canvas;
 
   // just to be sure that this does no harm
   assert_null(canvas->get_item_at(lpos_t(3, 6)));
 
   // 2 polygons that overlap
-  std::unique_ptr<canvas_item_t> line(canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0));
-  assert(line);
+  canvas_item_t * const line = canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0);
+  assert(line != nullptr);
 
   for (unsigned int i = 0; i < points.size(); i += 2) {
     lpos_t p = points[i];
@@ -80,17 +77,17 @@ static void testToBottom()
     points[i] = p;
   }
 
-  std::unique_ptr<canvas_item_t> line2(canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0));
-  assert(line2);
+  canvas_item_t *line2 = canvas->polyline_new(CANVAS_GROUP_WAYS, points, 1, 0);
+  assert(line2 != nullptr);
 
   canvas_item_t *search1 = canvas->get_item_at(lpos_t(3, 6));
   // must be one of the items, it's exactly on them
-  assert(search1 == line.get() || search1 == line2.get());
+  assert(search1 == line || search1 == line2);
 
   // now the other one must be on top
   canvas->item_to_bottom(search1);
   canvas_item_t *search2 = canvas->get_item_at(lpos_t(3, 6));
-  assert(search2 == line.get() || search2 == line2.get());
+  assert(search2 == line || search2 == line2);
   assert(search1 != search2);
 
   // and back to the first
