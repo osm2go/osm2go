osm2go/osm2go

commit a56988357a432c455907206fef2c7865285bdc6d

use floats for the canvas scroll position
The canvas uses doubles internally anyway, so truncating this does not give any
benefit.

src/project.cpp
@@ -223,9 +223,11 @@ bool project_t::save(osm2go_platform::Widget *parent) {
   xmlNewProp(node, BAD_CAST "zoom", BAD_CAST str);
   format_float(map_state.detail, 4, str);
   xmlNewProp(node, BAD_CAST "detail", BAD_CAST str);
-  snprintf(str, sizeof(str), "%d", map_state.scroll_offset.x);
+  snprintf(str, sizeof(str), "%f", map_state.scroll_offset.x);
+  remove_trailing_zeroes(str);
   xmlNewProp(node, BAD_CAST "scroll-offset-x", BAD_CAST str);
-  snprintf(str, sizeof(str), "%d", map_state.scroll_offset.y);
+  snprintf(str, sizeof(str), "%f", map_state.scroll_offset.y);
+  remove_trailing_zeroes(str);
   xmlNewProp(node, BAD_CAST "scroll-offset-y", BAD_CAST str);
 
   if(wms_offset.x != 0 || wms_offset.y != 0 || !wms_server.empty()) {

src/canvas.h
@@ -135,7 +135,11 @@ public:
   item_mapping_t item_mapping;
 
   lpos_t window2world(const osm2go_platform::screenpos &p) const;
-  void scroll_get(int &sx, int &sy) const;
+
+  /**
+   * @brief query the current position of the scrollbars
+   */
+  void scroll_get(float &sx, float &sy) const;
 
   /****** manipulating the canvas ******/
   void set_background(color_t bg_color);
@@ -174,7 +178,7 @@ public:
    * value if the given position is too close to the canvas edges. The
    * actual position is returned.
    */
-  void scroll_to(int &sx, int &sy);
+  void scroll_to(float &sx, float &sy);
 
   /**
    * @brief relative move of the visible screen area
@@ -182,7 +186,7 @@ public:
    * @param nx the new x position in canvas coordinates
    * @param ny the new y position in canvas coordinates
    */
-  void scroll_step(const osm2go_platform::screenpos &d, int &nx, int &ny);
+  void scroll_step(const osm2go_platform::screenpos &d, float &nx, float &ny);
   void set_bounds(lpos_t min, lpos_t max);
   void item_to_bottom(canvas_item_t *item);
 

src/map.cpp
@@ -1117,7 +1117,7 @@ void map_t::init() {
   set_zoom(state.zoom, false);
   paint();
 
-  printf("restore scroll position %d/%d\n",
+  printf("restore scroll position %f/%f\n",
          state.scroll_offset.x, state.scroll_offset.y);
 
   canvas->scroll_to(state.scroll_offset.x, state.scroll_offset.y);

src/map.h
@@ -87,7 +87,7 @@ struct map_state_t {
 
   float zoom;                          // zoom level (1.0 = 1m/pixel
   float detail;                        // detail level (1.0 = normal)
-  struct { int x,y; } scroll_offset;   // initial scroll offset
+  struct { float x,y; } scroll_offset;   // initial scroll offset
 };
 
 class map_t {

src/platforms/gtk/canvas_goocanvas.cpp
@@ -253,7 +253,8 @@ canvas_dimensions canvas_goocanvas::get_viewport_dimensions() const {
 }
 
 /* get scroll position in meters */
-void canvas_t::scroll_get(int &sx, int &sy) const {
+void canvas_t::scroll_get(float &sx, float &sy) const
+{
   GooCanvas *gc = GOO_CANVAS(widget);
   gdouble zoom = goo_canvas_get_scale(gc);
 
@@ -270,7 +271,7 @@ void canvas_t::scroll_get(int &sx, int &sy) const {
 }
 
 /* set scroll position in meters */
-void canvas_t::scroll_to(int &sx, int &sy)
+void canvas_t::scroll_to(float &sx, float &sy)
 {
   scrollvalue s = boundedScroll(static_cast<canvas_goocanvas *>(this), scrollvalue(sx, sy));
 
@@ -278,7 +279,7 @@ void canvas_t::scroll_to(int &sx, int &sy)
   sy = s.y;
 }
 
-void canvas_t::scroll_step(const osm2go_platform::screenpos &d, int &nx, int &ny)
+void canvas_t::scroll_step(const osm2go_platform::screenpos &d, float &nx, float &ny)
 {
   GooCanvas *gc = GOO_CANVAS(widget);
   gdouble hs = gtk_adjustment_get_value(gc->hadjustment) + d.x();
@@ -628,7 +629,7 @@ bool canvas_goocanvas::isVisible(const lpos_t lpos) const
   const canvas_dimensions dim = static_cast<const canvas_goocanvas *>(this)->get_viewport_dimensions();
 
   // Is the point still onscreen?
-  int sx, sy;
+  float sx, sy;
   scroll_get(sx, sy);
 
   if (lpos.x > sx + dim.width / 2)
@@ -648,7 +649,7 @@ bool canvas_t::ensureVisible(const lpos_t lpos)
   if(static_cast<canvas_goocanvas *>(this)->isVisible(lpos))
     return false;
 
-  int x = lpos.x, y = lpos.y;
+  float x = lpos.x, y = lpos.y;
   scroll_to(x, y);
 
   return true;

commit 907adfd973e59f4cd47d37f9cec98eeaf555b631

fix loading and storing scroll offsets
These could have been negative forever, but were restored using strtoul(),
which returned invalid values on negative input. This became worse when storing
them as float, but even that was wrong as the stored float was not locale
independent.

Fixes: a56988357a432c455907206fef2c7865285bdc6d

src/project.cpp
@@ -109,11 +109,11 @@ bool project_read(const std::string &project_file, project_t::ref project,
 
             str.reset(xmlGetProp(node, BAD_CAST "scroll-offset-x"));
             if(str)
-              project->map_state.scroll_offset = osm2go_platform::screenpos(strtoul(str, nullptr, 10), 0);
+              project->map_state.scroll_offset = osm2go_platform::screenpos(xml_parse_float(str), 0);
 
             str.reset(xmlGetProp(node, BAD_CAST "scroll-offset-y"));
             if(str)
-              project->map_state.scroll_offset = osm2go_platform::screenpos(project->map_state.scroll_offset.x(), strtoul(str, nullptr, 10));
+              project->map_state.scroll_offset = osm2go_platform::screenpos(project->map_state.scroll_offset.x(), xml_parse_float(str));
           } else if(strcmp(reinterpret_cast<const char *>(node->name), "wms") == 0) {
             xmlString str(xmlGetProp(node, BAD_CAST "server"));
             if(!str.empty())
@@ -223,11 +223,9 @@ bool project_t::save(osm2go_platform::Widget *parent) {
   xmlNewProp(node, BAD_CAST "zoom", BAD_CAST str);
   format_float(map_state.detail, 4, str);
   xmlNewProp(node, BAD_CAST "detail", BAD_CAST str);
-  snprintf(str, sizeof(str), "%f", map_state.scroll_offset.x());
-  remove_trailing_zeroes(str);
+  format_float(map_state.scroll_offset.x(), 4, str);
   xmlNewProp(node, BAD_CAST "scroll-offset-x", BAD_CAST str);
-  snprintf(str, sizeof(str), "%f", map_state.scroll_offset.y());
-  remove_trailing_zeroes(str);
+  format_float(map_state.scroll_offset.y(), 4, str);
   xmlNewProp(node, BAD_CAST "scroll-offset-y", BAD_CAST str);
 
   if(wms_offset.x != 0 || wms_offset.y != 0 || !wms_server.empty()) {