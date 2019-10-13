osm2go/osm2go

commit dadd8897e01614c60401533a9928acc95e098fe4

add comment at end of closing namespace braces

src/josm_elemstyles.cpp
@@ -184,7 +184,7 @@ parse_color(const char *col, color_t &color, ColorMap &colors)
   return ret;
 }
 
-}
+
 
 bool parse_color(xmlNode *a_node, const char *name, color_t &color)
 {
@@ -491,7 +491,7 @@ void StyleSax::endElement(const xmlChar *name)
   state = it->oldState;
 }
 
-}
+} // namespace
 
 std::vector<elemstyle_t *> josm_elemstyles_load(const char *name) {
   printf("Loading JOSM elemstyles %s ...\n", name);
@@ -616,7 +616,7 @@ void colorize_node::operator()(const elemstyle_t *elemstyle)
   priority = elemstyle->icon.priority;
 }
 
-}
+} // namespace
 
 void josm_elemstyles_colorize_node(style_t *style, node_t *node) {
   node->zoom_max = style->node.zoom_max;
@@ -783,7 +783,7 @@ void josm_elemstyles_colorize_way_functor::operator()(way_t *way) {
   }
 }
 
-}
+} // namespace
 
 void josm_elemstyles_colorize_way(const style_t *style, way_t *way) {
   josm_elemstyles_colorize_way_functor f(style);

src/canvas.cpp
@@ -54,6 +54,7 @@ int canvas_t::get_item_segment(const canvas_item_t *item, lpos_t pos) const
 }
 
 namespace {
+
 /* remove item_info from chain as its visual representation */
 /* has been destroyed */
 template<typename T> class item_info_destroyer : public canvas_item_destroyer {
@@ -71,7 +72,8 @@ public:
     delete info;
   }
 };
-}
+
+} // namespace
 
 canvas_item_info_t::canvas_item_info_t(canvas_item_type_t t, canvas_t *cv, canvas_item_t *it, canvas_item_destroyer *d)
   : type(t)

src/diff.cpp
@@ -184,7 +184,7 @@ void diff_save_objects<relation_t>::save_additional_info(const relation_t *m, xm
   m->generate_member_xml(xmlnode);
 }
 
-}
+} // namespace
 
 void project_t::diff_save() const {
   if(unlikely(!osm))
@@ -664,7 +664,7 @@ struct osmchange_delete_functor {
   }
 };
 
-}
+} // namespace
 
 void osmchange_delete(const osm_t::dirty_t &dirty, xmlNodePtr xml_node, const char *changeset)
 {

src/josm_presets.cpp
@@ -88,7 +88,7 @@ bool used_preset_functor::operator()(const presets_element_t* w)
   return (ret < 0);
 }
 
-}
+} // namespace
 
 /**
  * @brief check if the currently active object uses this preset and the preset is interactive
@@ -171,7 +171,7 @@ void role_collect_functor::operator()(const presets_item::role &role)
   result.insert(role.name);
 }
 
-}
+} // namespace
 
 std::set<std::string> presets_items_internal::roles(const relation_t *relation, const object_t &obj) const
 {

src/josm_presets_parser.cpp
@@ -1125,7 +1125,7 @@ struct move_chunks_functor {
   }
 };
 
-}
+} // namespace
 
 bool presets_items_internal::addFile(const std::string &filename, const std::string &basepath, int basefd)
 {

src/map_edit.cpp
@@ -57,7 +57,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 void map_t::way_add_segment(lpos_t pos) {
   /* check if this was a double click. This is the case if */

src/map_hl.cpp
@@ -60,7 +60,7 @@ public:
   bool operator()(canvas_item_t *c) const;
 };
 
-}
+} // namespace
 
 bool find_highlighted::operator()(canvas_item_t* c) const
 {

src/osm.cpp
@@ -99,7 +99,7 @@ public:
   }
 };
 
-}
+} // namespace
 
 bool object_t::operator==(const object_t &other) const noexcept
 {

commit 16af8f07012b240bebab1af57ff8e68f89f50bc5

fix build by restoring accidentially deleted }
Fixes: dadd8897e01614c60401533a9928acc95e098fe4

src/josm_elemstyles.cpp
@@ -184,7 +184,7 @@ parse_color(const char *col, color_t &color, ColorMap &colors)
   return ret;
 }
 
-
+} // namespace
 
 bool parse_color(xmlNode *a_node, const char *name, color_t &color)
 {