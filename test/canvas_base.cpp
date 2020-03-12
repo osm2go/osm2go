4c95b479fb6804b26a68a00a97c068a5a5a0b5c7
Rolf Eike Beer <eike@sf-mail.de>
2020-03-12 23:24:06 +0100

    tests: add smoke test for background image

---
+++ test/canvas_base.cpp
@@ -0,0 +1,60 @@
+#include "dummy_map.h"
+
+#include <canvas.h>
+#include <canvas_p.h>
+#include <style.h>
+
+#include <cassert>
+#include <iostream>
+#include <memory>
+
+#include <osm2go_annotations.h>
+#include <osm2go_stl.h>
+#include <osm2go_test.h>
+
+namespace {
+
+void set_bounds(osm_t::ref o)
+{
+  bool b = o->bounds.init(pos_area(pos_t(52.2692786, 9.5750497), pos_t(52.2695463, 9.5755)));
+  assert(b);
+}
+
+void testBackground()
+{
+  char tmpdir[] = "/tmp/osm2go-canvas-XXXXXX";
+
+  if(mkdtemp(tmpdir) == nullptr) {
+    std::cerr << "cannot create temporary directory" << std::endl;
+    return;
+  }
+
+  std::string osm_path = tmpdir;
+  osm_path += '/';
+
+  std::string nonfile = osm_path + "not_here.jpg";
+
+  appdata_t a;
+  canvas_holder canvas;
+  std::unique_ptr<map_t> m(std::make_unique<test_map>(a, *canvas));
+  a.project.reset(new project_t("test_proj", tmpdir));
+  a.project->osm.reset(new osm_t());
+  set_bounds(a.project->osm);
+  m->style.reset(new style_t());
+  a.track.track.reset(new track_t());
+
+  assert(!m->set_bg_image(std::string()));
+  assert(!m->set_bg_image(nonfile));
+  m->remove_bg_image();
+}
+
+} // namespace
+
+int main()
+{
+  testBackground();
+
+  return 0;
+}
+
+#include "dummy_appdata.h"
