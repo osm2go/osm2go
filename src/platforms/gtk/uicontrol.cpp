osm2go/osm2go

commit 80758fade4847f6b049c7a4a58490f52a4519230

show changed objects without calling the upload dialog

src/platforms/gtk/uicontrol.cpp
@@ -131,6 +131,7 @@ MainUiGtk::MainUiGtk()
   menuitems[MENU_ITEM_TRACK_IMPORT] = createMenuItem(_("_Import"));
   menuitems[MENU_ITEM_MAP_UPLOAD] = createMenuItem(_("_Upload"), "upload.16");
   menuitems[MENU_ITEM_MAP_UNDO_CHANGES] = createMenuItem(_("Undo _all"), "edit-delete");
+  menuitems[MENU_ITEM_MAP_SHOW_CHANGES] = createMenuItem(_("Show &changes"));
 #ifndef FREMANTLE
   menuitems[MENU_ITEM_MAP_SAVE_CHANGES] = createMenuItem(_("_Save local changes"), "document-save");
 #endif

src/osm_api.h
@@ -19,12 +19,15 @@
 
 #pragma once
 
+#include "osm.h"
+
 #include <osm2go_platform.h>
 
 struct appdata_t;
-class osm_t;
 struct project_t;
 class settings_t;
 
 bool osm_download(osm2go_platform::Widget *parent, project_t *project);
 void osm_upload(appdata_t &appdata);
+
+void osm_modified_info(const osm_t::dirty_t &context, osm2go_platform::Widget *parent);

src/platforms/gtk/main.cpp
@@ -130,12 +130,13 @@ void appdata_t::main_ui_enable() {
 
   // those icons that get enabled or disabled depending on OSM data being loaded
 #ifndef FREMANTLE
-  std::array<MainUi::menu_items, 6> osm_active_items = { {
+  std::array<MainUi::menu_items, 7> osm_active_items = { {
     MainUi::MENU_ITEM_MAP_SAVE_CHANGES,
 #else
-  std::array<MainUi::menu_items, 5> osm_active_items = { {
+  std::array<MainUi::menu_items, 6> osm_active_items = { {
 #endif
     MainUi::MENU_ITEM_MAP_UNDO_CHANGES,
+    MainUi::MENU_ITEM_MAP_SHOW_CHANGES,
     MainUi::MENU_ITEM_MAP_RELATIONS,
     MainUi::SUBMENU_TRACK,
     MainUi::SUBMENU_VIEW,
@@ -344,6 +345,20 @@ cb_menu_undo_changes(appdata_t *appdata) {
   appdata->map->paint();
 
   appdata->uicontrol->showNotification(_("Undo all changes"), MainUi::Brief);
+}
+
+static void
+cb_menu_show_changes(appdata_t *appdata)
+{
+  project_t::ref project = appdata->project;
+  if (project->osm->is_clean(false)) {
+    appdata->uicontrol->showNotification(_("No changes present"), MainUi::Brief);
+    return;
+  }
+  const osm_t::dirty_t &dirty = project->osm->modified();
+  assert(!dirty.empty());
+
+  osm_modified_info(dirty, appdata_t::window);
 }
 
 static void
@@ -727,6 +742,10 @@ static void menu_create(appdata_internal &appdata, GtkBox *mainvbox) {
     appdata, submenu, G_CALLBACK(cb_menu_undo_changes), MainUi::MENU_ITEM_MAP_UNDO_CHANGES,
     "<OSM2Go-Main>/Map/UndoAll");
 
+  menu_append_new_item(
+    appdata, submenu, G_CALLBACK(cb_menu_show_changes), MainUi::MENU_ITEM_MAP_SHOW_CHANGES,
+    "<OSM2Go-Main>/Map/ShowChanges");
+
   gtk_menu_shell_append(GTK_MENU_SHELL(submenu), gtk_separator_menu_item_new());
 
   menu_append_new_item(
@@ -987,10 +1006,11 @@ static void menu_create(appdata_internal &appdata, GtkBox *) {
   } };
 
   /* -- the map submenu -- */
-  const std::array<menu_entry_t, 3> sm_map_entries = { {
+  const std::array<menu_entry_t, 4> sm_map_entries = { {
     menu_entry_t(MainUi::MENU_ITEM_MAP_UPLOAD,       G_CALLBACK(cb_menu_upload)),
     menu_entry_t(_("Download"),                      G_CALLBACK(cb_menu_download)),
     menu_entry_t(MainUi::MENU_ITEM_MAP_UNDO_CHANGES, G_CALLBACK(cb_menu_undo_changes)),
+    menu_entry_t(MainUi::MENU_ITEM_MAP_SHOW_CHANGES, G_CALLBACK(cb_menu_show_changes)),
   } };
 
   /* -- the wms submenu -- */

src/platforms/gtk/osm_upload_dialog.cpp
@@ -192,13 +192,14 @@ details_table(osm2go_platform::DialogGuard &dialog, const osm_t::dirty_t &dirty)
   gtk_box_pack_start(dialog.vbox(), table, FALSE, FALSE, 0);
 }
 
-#ifdef FREMANTLE
+} // namespace
+
 /* put additional infos into a seperate dialog for fremantle as */
 /* screen space is sparse there */
 void
-info_more(const osm_t::dirty_t &context, GtkWidget *parent)
+osm_modified_info(const osm_t::dirty_t &context, osm2go_platform::Widget *parent)
 {
-  osm2go_platform::DialogGuard dialog(gtk_dialog_new_with_buttons(_("Changeset details"),
+  osm2go_platform::DialogGuard dialog(gtk_dialog_new_with_buttons(static_cast<const gchar *>(_("Changeset details")),
                                       GTK_WINDOW(parent), GTK_DIALOG_MODAL,
                                       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, nullptr));
 
@@ -209,9 +210,6 @@ info_more(const osm_t::dirty_t &context, GtkWidget *parent)
   gtk_widget_show_all(dialog.get());
   gtk_dialog_run(dialog);
 }
-#endif
-
-} // namespace
 
 void osm_upload_dialog(appdata_t &appdata, const osm_t::dirty_t &dirty)
 {
@@ -293,7 +291,7 @@ void osm_upload_dialog(appdata_t &appdata, const osm_t::dirty_t &dirty)
     switch(gtk_dialog_run(dialog)) {
 #ifdef FREMANTLE
     case GTK_RESPONSE_HELP:
-      info_more(dirty, dialog.get());
+      osm_modified_info(dirty, dialog.get());
       break;
 #endif
     case GTK_RESPONSE_ACCEPT:

src/uicontrol.h
@@ -48,6 +48,7 @@ public:
     MENU_ITEM_TRACK_IMPORT,
     MENU_ITEM_MAP_UPLOAD,
     MENU_ITEM_MAP_UNDO_CHANGES,
+    MENU_ITEM_MAP_SHOW_CHANGES,
   #ifndef FREMANTLE
     MENU_ITEM_MAP_SAVE_CHANGES,
   #endif

commit 7efda181ba8772e7e8027c865b81f5d1891284bf

fix mnemonic marker for new "show changes"
Fixes: 80758fade4847f6b049c7a4a58490f52a4519230

src/platforms/gtk/uicontrol.cpp
@@ -131,7 +131,7 @@ MainUiGtk::MainUiGtk()
   menuitems[MENU_ITEM_TRACK_IMPORT] = createMenuItem(_("_Import"));
   menuitems[MENU_ITEM_MAP_UPLOAD] = createMenuItem(_("_Upload"), "upload.16");
   menuitems[MENU_ITEM_MAP_UNDO_CHANGES] = createMenuItem(_("Undo _all"), "edit-delete");
-  menuitems[MENU_ITEM_MAP_SHOW_CHANGES] = createMenuItem(_("Show &changes"));
+  menuitems[MENU_ITEM_MAP_SHOW_CHANGES] = createMenuItem(_("Show _changes"));
 #ifndef FREMANTLE
   menuitems[MENU_ITEM_MAP_SAVE_CHANGES] = createMenuItem(_("_Save local changes"), "document-save");
 #endif