osm2go/osm2go

commit 8566feb2ba450d09eb5bc4a3ad97c62a6ea37883

use trstring::native_type in menu construction

src/platforms/gtk/main.cpp
@@ -884,10 +884,10 @@ app_submenu_create(appdata_t &appdata, MainUi::menu_items submenu, const menu_en
 
     /* the "Style" menu entry is very special */
     /* and is being handled seperately */
-    if(menu_entries->label != nullptr && strcmp(_("Style"), menu_entries->label) == 0) {
+    if(!menu_entries->label.isEmpty() && strcmp(_("Style"), static_cast<const gchar *>(menu_entries->label)) == 0) {
       button = style_select_widget(settings_t::instance()->style);
       g_object_set_data(G_OBJECT(dialog), "style_widget", button);
-    } else if(menu_entries->label != nullptr && strcmp(_("Track visibility"), menu_entries->label) == 0) {
+    } else if(!menu_entries->label.isEmpty() && strcmp(_("Track visibility"), static_cast<const gchar *>(menu_entries->label)) == 0) {
       button = track_vis_select_widget(settings_t::instance()->trackVisibility);
       g_object_set_data(G_OBJECT(dialog), "track_widget", button);
     } else if(!menu_entries->toggle) {
@@ -968,11 +968,11 @@ on_submenu_track_clicked(appdata_internal *appdata)
 }
 
 struct main_menu_entry_t {
-  explicit main_menu_entry_t(const char *l, GCallback cb, void *cb_context)
+  explicit main_menu_entry_t(trstring:native_type_arg l, GCallback cb, void *cb_context)
     : label(l), menuindex(-1), activate_cb(cb), activate_context(cb_context) {}
   explicit main_menu_entry_t(MainUi::menu_items idx, GCallback cb, void *cb_context)
-    : label(nullptr), menuindex(idx), activate_cb(cb), activate_context(cb_context) {}
-  const char *label;
+    : menuindex(idx), activate_cb(cb), activate_context(cb_context) {}
+  trstring::native_type label;
   int menuindex;
   GCallback activate_cb;
   void * const activate_context;
@@ -999,7 +999,7 @@ app_menu_create(appdata_internal &appdata)
     const main_menu_entry_t &entry = main_menu[i];
     GtkWidget *button;
 
-    if (entry.label == nullptr)
+    if (entry.label.isEmpty())
       button = mainui->addMenu(static_cast<MainUi::menu_items>(entry.menuindex));
     else
       button = mainui->addMenu(entry.label);

src/platforms/gtk/MainUiGtk.h
@@ -32,6 +32,7 @@ typedef GtkMenuShell MenuBar;
 #endif
 
 #include <osm2go_cpp.h>
+#include "osm2go_i18n.h"
 #include <osm2go_stl.h>
 
 typedef struct _GtkWidget GtkWidget;
@@ -58,14 +59,14 @@ public:
   /**
    * @brief create a new submenu entry in the global menu bar
    */
-  GtkWidget *addMenu(const char *label);
+  GtkWidget *addMenu(trstring::native_type_arg label);
 
   /**
    * @brief add one of the predefined entries to the global menu bar
    */
   GtkWidget *addMenu(menu_items item);
 
-  static GtkWidget *createMenuItem(const char *label, const char *icon_name = nullptr) __attribute__((nonnull(1)));
+  static GtkWidget *createMenuItem(trstring::native_type_arg label, const char *icon_name = nullptr);
 
   inline statusbar_t *statusBar() const
   { return statusbar.get(); }

src/platforms/gtk/uicontrol.cpp
@@ -42,10 +42,10 @@ namespace {
 
 #ifdef FREMANTLE
 std::string
-strip_mnemonic(const char *label)
+strip_mnemonic(trstring::native_type_arg label)
 {
   // remove mnemonic marker
-  std::string hlabel = label;
+  std::string hlabel = static_cast<const gchar *>(label);
   std::string::size_type _pos = hlabel.find('_');
   if(likely(_pos != std::string::npos))
     hlabel.erase(_pos, 1);
@@ -54,33 +54,33 @@ strip_mnemonic(const char *label)
 #endif
 
 GtkWidget *
-create_submenu_item(const char *label)
+create_submenu_item(trstring::native_type_arg label)
 {
 #ifdef FREMANTLE
   return hildon_button_new_with_text(
                 static_cast<HildonSizeType>(HILDON_SIZE_FINGER_HEIGHT | HILDON_SIZE_AUTO_WIDTH),
                 HILDON_BUTTON_ARRANGEMENT_VERTICAL, strip_mnemonic(label).c_str(), nullptr);
 #else
-  return gtk_menu_item_new_with_mnemonic(label);
+  return gtk_menu_item_new_with_mnemonic(static_cast<const gchar *>(label));
 #endif
 }
 
 GtkWidget *
-create_checkbox_item(const char *label)
+create_checkbox_item(trstring::native_type_arg label)
 {
 #ifdef FREMANTLE
   GtkWidget *button = hildon_check_button_new(HILDON_SIZE_AUTO);
   gtk_button_set_label(GTK_BUTTON(button), strip_mnemonic(label).c_str());
   return button;
 #else
-  return gtk_check_menu_item_new_with_mnemonic(label);
+  return gtk_check_menu_item_new_with_mnemonic(static_cast<const gchar *>(label));
 #endif
 }
 
 } // namespace
 
 GtkWidget *
-MainUiGtk::createMenuItem(const char *label, const char *icon_name
+MainUiGtk::createMenuItem(trstring::native_type_arg label, const char *icon_name
 #ifdef FREMANTLE
                           __attribute__((unused))
 #endif
@@ -93,7 +93,7 @@ MainUiGtk::createMenuItem(const char *label, const char *icon_name
     if (image == nullptr)
       image = gtk_image_new_from_icon_name(icon_name, GTK_ICON_SIZE_MENU);
     assert(image != nullptr);
-    GtkWidget *item = gtk_image_menu_item_new_with_mnemonic(label);
+    GtkWidget *item = gtk_image_menu_item_new_with_mnemonic(static_cast<const gchar *>(label));
     gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(item), image);
     return item;
   }
@@ -182,7 +182,7 @@ GtkWidget *MainUiGtk::addMenu(GtkWidget *item)
 #endif
 }
 
-GtkWidget *MainUiGtk::addMenu(const char *label)
+GtkWidget *MainUiGtk::addMenu(trstring::native_type_arg label)
 {
   return addMenu(create_submenu_item(label));
 }

commit 36d54eae002adbcc76646ee7ce76d6c762c9bc39

Fremantle: fix build because of trstring conversion
Fixes: 8566feb2ba450d09eb5bc4a3ad97c62a6ea37883

src/platforms/gtk/main.cpp
@@ -825,13 +825,13 @@ menu_create(appdata_internal &appdata, GtkBox *mainvbox)
 
 struct menu_entry_t {
   typedef gboolean (*toggle_cb)();
-  explicit menu_entry_t(const char *l, GCallback cb = nullptr,
+  explicit menu_entry_t(trstring::native_type_arg l, GCallback cb = nullptr,
                         gboolean en = TRUE)
     : label(l), enabled(en), toggle(nullptr), menuindex(-1), activate_cb(cb) {}
   explicit menu_entry_t(MainUi::menu_items idx, GCallback cb = nullptr,
                         toggle_cb tg = nullptr)
     : label(nullptr), enabled(TRUE), toggle(tg), menuindex(idx), activate_cb(cb) {}
-  const char *label;
+  trstring::native_type label;
   gboolean enabled;
   toggle_cb toggle;
   int menuindex;
@@ -968,7 +968,7 @@ on_submenu_track_clicked(appdata_internal *appdata)
 }
 
 struct main_menu_entry_t {
-  explicit main_menu_entry_t(trstring:native_type_arg l, GCallback cb, void *cb_context)
+  explicit main_menu_entry_t(trstring::native_type_arg l, GCallback cb, void *cb_context)
     : label(l), menuindex(-1), activate_cb(cb), activate_context(cb_context) {}
   explicit main_menu_entry_t(MainUi::menu_items idx, GCallback cb, void *cb_context)
     : menuindex(idx), activate_cb(cb), activate_context(cb_context) {}