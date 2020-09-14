osm2go/osm2go

commit 792cdf0d5600cd201fc4067116de9987de2dd376

explicitely compare pointers against nullptr

src/platforms/gtk/osm_upload_dialog.cpp
@@ -62,7 +62,7 @@ osm_upload_context_t::osm_upload_context_t(appdata_t &a, project_t::ref p, const
   , project(p)
   , urlbasestr(p->server(settings_t::instance()->server) + "/")
   , comment(c)
-  , src(s ? s : std::string())
+  , src(s == nullptr ? s : std::string())
 {
 }
 
@@ -132,7 +132,7 @@ static void callback_buffer_modified(GtkTextBuffer *buffer, GtkDialog *dialog) {
   gtk_text_buffer_get_end_iter(buffer, &end);
   char *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
   gtk_dialog_set_response_sensitive(dialog, GTK_RESPONSE_ACCEPT,
-                                    (text && strlen(text) > 0) ? TRUE : FALSE);
+                                    (text != nullptr && strlen(text) > 0) ? TRUE : FALSE);
 }
 
 static gboolean cb_focus_in(GtkTextView *view, GdkEventFocus *, GtkTextBuffer *buffer) {

src/diff.cpp
@@ -375,12 +375,12 @@ static void diff_restore_way(xmlNodePtr node_way, osm_t::ref osm) {
   /* scan for nodes */
   node_chain_t new_chain;
   xmlNode *nd_node = nullptr;
-  for(nd_node = node_way->children; nd_node; nd_node = nd_node->next) {
+  for(nd_node = node_way->children; nd_node != nullptr; nd_node = nd_node->next) {
     if(nd_node->type == XML_ELEMENT_NODE) {
       if(likely(strcmp(reinterpret_cast<const char *>(nd_node->name), "nd") == 0)) {
 	/* attach node to node_chain */
 	node_t *tmp = osm->parse_way_nd(nd_node);
-	if(tmp)
+        if(tmp != nullptr)
 	  new_chain.push_back(tmp);
       }
     }
@@ -478,8 +478,7 @@ static void diff_restore_relation(xmlNodePtr node_rel, osm_t::ref osm) {
   /* scan for members */
   std::vector<member_t> members;
   xmlNode *member_node = nullptr;
-  for(member_node = node_rel->children; member_node;
-      member_node = member_node->next) {
+  for(member_node = node_rel->children; member_node != nullptr; member_node = member_node->next) {
     if(member_node->type == XML_ELEMENT_NODE) {
       if(likely(strcmp(reinterpret_cast<const char *>(member_node->name), "member") == 0)) {
 	/* attach member to member_chain */
@@ -538,7 +537,7 @@ unsigned int project_t::diff_restore() {
   root_element = xmlDocGetRootElement(doc.get());
 
   xmlNode *cur_node = nullptr;
-  for (cur_node = root_element; cur_node; cur_node = cur_node->next) {
+  for (cur_node = root_element; cur_node != nullptr; cur_node = cur_node->next) {
     if (cur_node->type == XML_ELEMENT_NODE) {
       if(strcmp(reinterpret_cast<const char *>(cur_node->name), "diff") == 0) {
         xmlString str(xmlGetProp(cur_node, BAD_CAST "name"));

src/fdguard.cpp
@@ -75,7 +75,7 @@ dirguard::dirguard(int fd)
   : d(fdopendir(dup(fd)))
 {
   // ignore the position of fd
-  if(d)
+  if(d != nullptr)
     rewinddir(d);
 }
 

src/josm_elemstyles.cpp
@@ -134,7 +134,7 @@ static bool parse_color(const char *col, color_t &color, ColorMap &colors) {
   /* if the color name contains a # it's a hex representation */
   const char * const hash = strchr(col, '#');
   std::string colname;
-  if(hash) {
+  if(hash != nullptr) {
     ret = osm2go_platform::parse_color_string(hash, color);
     if(ret && hash != col)
       colname.assign(col, hash - col);
@@ -154,7 +154,7 @@ static bool parse_color(const char *col, color_t &color, ColorMap &colors) {
       if(hash == col) {
         color = it->second;
         ret = true;
-      } else if(hash)
+      } else if(hash != nullptr)
         // check that the colors are the same if the key is specified multiple times
         assert_cmpnum(it->second, color);
     }
@@ -190,7 +190,7 @@ static const char *true_values[] = { "1", "yes", "true", nullptr };
 static const char *false_values[] = { "0", "no", "false", nullptr };
 
 static bool parse_boolean(const char *bool_str, const char **value_strings) {
-  for (int i = 0; value_strings[i]; ++i)
+  for (int i = 0; value_strings[i] != nullptr; ++i)
     if (strcasecmp(bool_str, value_strings[i]) == 0)
       return true;
   return false;
@@ -297,7 +297,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
     const char *k = nullptr, *v = nullptr;
     const char *b = nullptr;
 
-    for(unsigned int i = 0; attrs[i]; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
       if(strcmp(attrs[i], "k") == 0)
         k = attrs[i + 1];
       else if(strcmp(attrs[i], "v") == 0)
@@ -306,7 +306,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
         b = attrs[i + 1];
     }
     assert(k != nullptr);
-    elemstyle_condition_t cond = !b ? elemstyle_condition_t(k, v) :
+    elemstyle_condition_t cond = b == nullptr ? elemstyle_condition_t(k, v) :
                                  elemstyle_condition_t(k, parse_boolean(b, true_values));
     styles.back()->conditions.push_back(cond);
     break;
@@ -320,7 +320,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
     bool hasColor = false, hasWidth = false;
     elemstyle_line_t *line = elemstyle->line = new elemstyle_line_t();
 
-    for(unsigned int i = 0; attrs[i]; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
       if(strcmp(attrs[i], "colour") == 0) {
         hasColor = parse_color(attrs[i + 1], line->color, colors);
       } else if(strcmp(attrs[i], "width") == 0) {
@@ -376,7 +376,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
 
     elemstyle_line_mod_t &line_mod = elemstyle->line_mod;
 
-    for(unsigned int i = 0; attrs[i]; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
       if(strcmp(attrs[i], "colour") == 0) {
         color_t col;
         if(parse_color(attrs[i + 1], col, colors))
@@ -395,7 +395,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
     elemstyle->type |= ES_TYPE_AREA;
 
     bool hasColor = false;
-    for(unsigned int i = 0; attrs[i] && !hasColor; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr && !hasColor; i += 2) {
       if(strcmp(attrs[i], "colour") == 0)
         hasColor = parse_color(attrs[i + 1], elemstyle->area.color, colors);
       else if(strcmp(attrs[i], "priority") == 0)
@@ -407,7 +407,7 @@ void StyleSax::startElement(const xmlChar *name, const char **attrs)
     break;
   }
   case TagIcon:
-    for(unsigned int i = 0; attrs[i]; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
       if(strcmp(attrs[i], "annotate") == 0)
         elemstyle->icon.annotate = strcmp(attrs[i + 1], "true");
       else if(strcmp(attrs[i], "src") == 0)
@@ -472,17 +472,17 @@ void josm_elemstyles_free(std::vector<elemstyle_t *> &elemstyles) {
 #define WIDTH_SCALE (1.0)
 
 bool elemstyle_condition_t::matches(const base_object_t &obj) const {
-  if(key) {
+  if(key != nullptr) {
     const char *v = obj.tags.get_value(key);
     if(isBool) {
-      if(v) {
+      if(v != nullptr) {
          const char **value_strings = boolValue ? true_values : false_values;
          return parse_boolean(v, value_strings);
       } else {
         return false;
       }
     } else {
-      if(!v || (value && strcasecmp(v, value) != 0))
+      if(v == nullptr || (value != nullptr && strcasecmp(v, value) != 0))
         return false;
     }
   }
@@ -549,7 +549,7 @@ void colorize_node::operator()(const elemstyle_t *elemstyle)
    * avoiding needless image processing. */
   node_icon_unref(style, node, icons);
 
-  if(buf)
+  if(buf != nullptr)
     style->node_icons[node->id] = buf;
 
   if (elemstyle->zoom_max > 0)
@@ -702,7 +702,7 @@ void josm_elemstyles_colorize_way_functor::operator()(way_t *way) {
   }
 
   /* apply the last line mod entry that has been found during search */
-  if(line_mod) {
+  if(line_mod != nullptr) {
     way->draw.width = line_mod_apply_width(way->draw.width, &line_mod->line);
 
     /* special case: the way does not have a background, but it is to */

src/josm_presets_parser.cpp
@@ -103,7 +103,8 @@ static int josm_type_bit(const char *type, char sep) {
 /* parse a comma seperated list of types and set their bits */
 static unsigned int josm_type_parse(const char *type) {
   unsigned int type_mask = 0;
-  if(!type) return presets_item_t::TY_ALL;
+  if(type == nullptr)
+    return presets_item_t::TY_ALL;
 
   for(const char *ntype = strchr(type, ',');
       ntype != nullptr; ntype = strchr(type, ',')) {
@@ -336,7 +337,7 @@ const std::vector<std::string> &userLangs()
     const char *lcm = getenv("LC_MESSAGES");
     if(lcm == nullptr || *lcm == '\0')
       lcm = getenv("LANG");
-    if(lcm && *lcm) {
+    if(lcm != nullptr && *lcm != '\0') {
       std::string lc = lcm;
       std::string::size_type d = lc.find('.');
       if(d != std::string::npos)
@@ -446,7 +447,7 @@ const char *PresetSax::findAttribute(const char **attrs, const char *name, bool 
   // the defaut one.
   const char *c = nullptr;
 
-  for(unsigned int i = 0; attrs[i]; i += 2) {
+  for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
     // Check if the given attribute begins with one of the preferred language
     // codes. If yes, skip over the language code and check this one.
     const char *a = attrs[i];
@@ -476,7 +477,7 @@ PresetSax::AttrMap PresetSax::findAttributes(const char **attrs, const char **na
 {
   AttrMap ret;
 
-  for(unsigned int i = 0; attrs[i]; i += 2) {
+  for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
     // Check if the given attribute begins with one of the preferred language
     // codes. If yes, skip over the language code and check this one.
     const char *a = attrs[i];
@@ -683,7 +684,7 @@ void PresetSax::startElement(const char *name, const char **attrs)
     const char *key = nullptr;
     const char *value = nullptr;
     const char *match = nullptr;
-    for(unsigned int i = 0; attrs[i]; i += 2) {
+    for(unsigned int i = 0; attrs[i] != nullptr; i += 2) {
       if(strcmp(attrs[i], "key") == 0) {
         key = attrs[i + 1];
       } else if(strcmp(attrs[i], "value") == 0) {
@@ -743,7 +744,7 @@ void PresetSax::startElement(const char *name, const char **attrs)
     bool editable = ait == aitEnd || (strcmp(ait->second, "false") != 0);
 
     char delimiter = ',';
-    if(del) {
+    if(del != nullptr) {
       if(unlikely(strlen(del) != 1))
         dumpState("found", "combo with invalid delimiter ", del);
       else
@@ -844,7 +845,7 @@ void PresetSax::startElement(const char *name, const char **attrs)
       const char *tp = NULL_OR_MAP_VAL(a.find("type"));
       const char *cnt = NULL_OR_MAP_VAL(a.find("count"));
       unsigned int count = 0;
-      if(cnt) {
+      if(cnt != nullptr) {
         char *endp;
         count = strtoul(cnt, &endp, 10);
         if(unlikely(*endp != '\0')) {
@@ -1212,11 +1213,11 @@ std::vector<std::string> presets_element_selectable::split_string(const char *st
 {
   std::vector<std::string> ret;
 
-  if(!str)
+  if(str == nullptr)
     return ret;
 
   const char *c, *p = str;
-  while((c = strchr(p, delimiter))) {
+  while((c = strchr(p, delimiter)) != nullptr) {
     ret.push_back(std::string(p, c - p));
     p = c + 1;
   }

src/map.cpp
@@ -100,7 +100,7 @@ static void map_node_select(map_t *map, node_t *node) {
   map_item->highlight = false;
 
   /* node may not have any visible representation at all */
-  if(node->map_item_chain)
+  if(node->map_item_chain != nullptr)
     map_item->item = node->map_item_chain->firstCanvasItem();
   else
     map_item->item = nullptr;
@@ -133,7 +133,7 @@ static void map_node_select(map_t *map, node_t *node) {
   map->highlight.circle_new(map, CANVAS_GROUP_NODES_HL, new_map_item, x, y,
                             radius, map->style->highlight.color);
 
-  if(!map_item->item) {
+  if(map_item->item == nullptr) {
     /* and draw a fake node */
     new_map_item = new map_item_t(*map_item);
     new_map_item->highlight = true;
@@ -192,7 +192,7 @@ void draw_selected_way_functor::operator()(node_t* node)
   item.object = node;
 
   /* draw an arrow between every two nodes */
-  if(last) {
+  if(last != nullptr) {
     struct { float x, y;} center, diff;
     center.x = (last->lpos.x + node->lpos.x)/2;
     center.y = (last->lpos.y + node->lpos.y)/2;
@@ -315,7 +315,7 @@ void relation_select_functor::operator()(member_t& member)
   }
 
   /* attach item to item chain */
-  if(item)
+  if(item != nullptr)
     map->highlight.items.push_back(item);
 }
 
@@ -396,7 +396,7 @@ static void map_node_new(map_t *map, node_t *node, unsigned int radius,
   map_item->item->set_zoom_max(node->zoom_max / (2 * map->state.detail));
 
   /* attach map_item to nodes map_item_chain */
-  if(!node->map_item_chain)
+  if(node->map_item_chain == nullptr)
     node->map_item_chain = new map_item_chain_t();
   node->map_item_chain->map_items.push_back(map_item);
 
@@ -450,7 +450,7 @@ void map_way_draw_functor::operator()(way_t *way)
     return;
 
   /* attach map_item to ways map_item_chain */
-  if(!way->map_item_chain)
+  if(way->map_item_chain == nullptr)
     way->map_item_chain = new map_item_chain_t();
   std::vector<map_item_t *> &chain = way->map_item_chain->map_items;
   map_item_t *map_item;
@@ -657,7 +657,7 @@ static void map_free_map_item_chains(appdata_t &appdata) {
 map_item_t *map_t::item_at(lpos_t pos) {
   canvas_item_t *item = canvas->get_item_at(pos);
 
-  if(!item) {
+  if(item == nullptr) {
     printf("  there's no item\n");
     return nullptr;
   }
@@ -666,7 +666,7 @@ map_item_t *map_t::item_at(lpos_t pos) {
 
   map_item_t *map_item = item->get_user_data();
 
-  if(!map_item) {
+  if(map_item == nullptr) {
     printf("  item has no user data!\n");
     return nullptr;
   }
@@ -686,7 +686,7 @@ void map_t::pen_down_item() {
   pen_down.on_item = item_at(canvas->window2world(pen_down.at.x, pen_down.at.y));
 
   /* no item or already a real one */
-  if(!pen_down.on_item || !pen_down.on_item->highlight)
+  if(pen_down.on_item == nullptr || !pen_down.on_item->highlight)
     return;
 
   /* get the item (parent) this item is the highlight of */
@@ -694,10 +694,10 @@ void map_t::pen_down_item() {
   case object_t::NODE:
   case object_t::WAY: {
     visible_item_t * const vis = static_cast<visible_item_t *>(pen_down.on_item->object.obj);
-    if(vis->map_item_chain && !vis->map_item_chain->map_items.empty()) {
+    if(vis->map_item_chain != nullptr && !vis->map_item_chain->map_items.empty()) {
       map_item_t *parent = vis->map_item_chain->map_items.front();
 
-      if(parent) {
+      if(parent != nullptr) {
         printf("  using parent item %s #" ITEM_ID_FORMAT "\n", vis->apiString(), vis->id);
         pen_down.on_item = parent;
         return;
@@ -761,7 +761,7 @@ void map_t::set_zoom(double zoom, bool update_scroll_offsets) {
       map_limit_scroll(this, state.scroll_offset.x, state.scroll_offset.y);
   }
 
-  if(gps_item) {
+  if(gps_item != nullptr) {
     float radius = style->track.width / 2.0;
     if(zoom < GPS_RADIUS_LIMIT) {
       radius *= GPS_RADIUS_LIMIT;
@@ -792,7 +792,7 @@ void map_t::scroll_step(int x, int y) {
 bool map_t::item_is_selected_node(const map_item_t *map_item) const {
   printf("check if item is a selected node\n");
 
-  if(!map_item) {
+  if(map_item == nullptr) {
     printf("  no item requested\n");
     return false;
   }
@@ -822,7 +822,7 @@ bool map_t::item_is_selected_node(const map_item_t *map_item) const {
 bool map_t::item_is_selected_way(const map_item_t *map_item) const {
   printf("check if item is the selected way\n");
 
-  if(!map_item) {
+  if(map_item == nullptr) {
     printf("  no item requested\n");
     return false;
   }
@@ -850,7 +850,7 @@ static void map_handle_click(map_t *map) {
 
   /* problem: on_item may be the highlight itself! So store it! */
   map_item_t map_item;
-  if(map->pen_down.on_item)
+  if(map->pen_down.on_item != nullptr)
     map_item = *map->pen_down.on_item;
 
   /* if we aready have something selected, then de-select it */
@@ -1048,7 +1048,7 @@ void map_t::button_release(int x, int y) {
 
     item_deselect();
 
-    if(node) {
+    if(node != nullptr) {
       map_node_select(this, node);
 
       /* let the user specify some tags for the new node */
@@ -1318,7 +1318,7 @@ void map_t::action_ok() {
 
     item_deselect();
 
-    if(node) {
+    if(node != nullptr) {
       map_node_select(this, node);
 
       /* let the user specify some tags for the new node */
@@ -1683,7 +1683,7 @@ void map_t::show_all() {
 }
 
 void map_t::detail_change(float detail, const char *banner_msg) {
-  if(banner_msg)
+  if(banner_msg != nullptr)
     appdata.uicontrol->showNotification(banner_msg, MainUi::Busy);
   /* deselecting anything allows us not to care about automatic deselection */
   /* as well as items becoming invisible by the detail change */
@@ -1694,7 +1694,7 @@ void map_t::detail_change(float detail, const char *banner_msg) {
 
   clear(MAP_LAYER_OBJECTS_ONLY);
   paint();
-  if(banner_msg)
+  if(banner_msg != nullptr)
     appdata.uicontrol->showNotification(nullptr, MainUi::Busy);
 }
 

src/map_edit.cpp
@@ -59,8 +59,8 @@ void map_t::way_add_segment(lpos_t pos) {
   /* the last node placed is less than 5 pixels from the current */
   /* position */
   const node_t *lnode = action.way->last_node();
-  if(lnode && (state.zoom * std::sqrt((lnode->lpos.x - pos.x) * (lnode->lpos.x - pos.x) +
-                                      (lnode->lpos.y - pos.y) * (lnode->lpos.y - pos.y))) < 5) {
+  if(lnode != nullptr && (state.zoom * std::sqrt((lnode->lpos.x - pos.x) * (lnode->lpos.x - pos.x) +
+                                                 (lnode->lpos.y - pos.y) * (lnode->lpos.y - pos.y))) < 5) {
 #if 0
     printf("detected double click -> simulate ok click\n");
     touchnode_clear();
@@ -90,7 +90,7 @@ void map_t::way_add_segment(lpos_t pos) {
       if(action.way->node_chain.empty()) {
         action.extending = touch_way;
 
-        if(action.extending) {
+        if(action.extending != nullptr) {
           if(!yes_no_f(nullptr, MISC_AGAIN_ID_EXTEND_WAY, _("Extend way?"),
              _("Do you want to extend the way present at this location?")))
             action.extending = nullptr;
@@ -111,7 +111,7 @@ void map_t::way_add_segment(lpos_t pos) {
         node = osm->node_new(pos);
     }
 
-    if(node) {
+    if(node != nullptr) {
       assert(action.way != nullptr);
       action.way->append_node(node);
 
@@ -215,7 +215,7 @@ void map_t::way_add_ok() {
   std::for_each(chain.begin(), chain.end(), map_draw_nodes(this));
 
   /* attach to existing way if the user requested so */
-  if(action.extending) {
+  if(action.extending != nullptr) {
     // this is triggered when the user started with extending an existing way
     // since the merged way is a temporary one there are no relation memberships
     action.extending->merge(action.way, osm);
@@ -228,14 +228,13 @@ void map_t::way_add_ok() {
 
   /* we might already be working on the "ends_on" way as we may */
   /* be extending it. Joining the same way doesn't make sense. */
-  if(action.ends_on && (action.ends_on == action.way)) {
+  if(action.ends_on == action.way) {
     printf("  the new way ends on itself -> don't join itself\n");
     action.ends_on = nullptr;
   }
 
-  if(action.ends_on &&
-     yes_no_f(nullptr, MISC_AGAIN_ID_EXTEND_WAY_END, _("Join way?"),
-              _("Do you want to join the way present at this location?"))) {
+  if(action.ends_on != nullptr && yes_no_f(nullptr, MISC_AGAIN_ID_EXTEND_WAY_END, _("Join way?"),
+                                           _("Do you want to join the way present at this location?"))) {
     printf("  this new way ends on another way\n");
     // this is triggered when the new way ends on an existing way, this can
     // happen even if an existing way was extended before
@@ -413,9 +412,9 @@ void map_t::way_cut(lpos_t pos) {
   set_action(MAP_ACTION_IDLE);
 
   /* and redo selection if way still exists */
-  if(item)
+  if(item != nullptr)
     select_way(way);
-  else if(neww)
+  else if(neww != nullptr)
     select_way(neww);
 }
 

src/map_hl.cpp
@@ -61,7 +61,7 @@ bool find_highlighted::operator()(canvas_item_t* c)
 {
   map_item_t *hl_item = c->get_user_data();
 
-  return hl_item && hl_item->object == item.object;
+  return hl_item != nullptr && hl_item->object == item.object;
 }
 
 bool map_highlight_t::isHighlighted(const map_item_t& item) const

commit 49ee5ab497268935b652926d6289a4d8a283663b

add "source" tag again on changesets
Fixes: 792cdf0d5600cd201fc4067116de9987de2dd376

src/platforms/gtk/osm_upload_dialog.cpp
@@ -75,7 +75,7 @@ osm_upload_context_t::osm_upload_context_t(appdata_t &a, project_t::ref p, const
   , project(p)
   , urlbasestr(p->server(settings_t::instance()->server) + '/')
   , comment(c)
-  , src(s == nullptr ? s : std::string())
+  , src(s != nullptr ? s : std::string())
 {
 }
 