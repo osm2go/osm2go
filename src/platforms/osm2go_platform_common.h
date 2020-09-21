osm2go/osm2go

commit 3e85e9a3b6ef1f5deb80a41618ebbf57b6a9958a

provide only one overload for osm2go_platform::yes_no()

src/platforms/osm2go_platform_common.h
@@ -80,7 +80,7 @@ namespace osm2go_platform {
    * @brief a dialog asking for yes or no
    * @retval true the user clicked yes
    */
-  bool yes_no(const trstring &title, const trstring &msg,
+  bool yes_no(trstring::arg_type title, trstring::arg_type msg,
               unsigned int again_flags = 0, Widget *parent = nullptr) __attribute__((warn_unused_result));
 
   const std::vector<dirguard> &base_paths() __attribute__((warn_unused_result));

src/platforms/gtk/osm2go_platform.h
@@ -89,11 +89,6 @@ namespace osm2go_platform {
     value_type m_x, m_y;
 
   };
-
-  bool yes_no(trstring::native_type_arg title, trstring::native_type_arg msg,
-              unsigned int again_flags = 0,Widget *parent = nullptr) __attribute__((warn_unused_result));
-  bool yes_no(trstring::native_type_arg title, const trstring &msg,
-              unsigned int again_flags = 0, Widget *parent = nullptr) __attribute__((warn_unused_result));
 };
 
 #include "../osm2go_platform_common.h"

src/platforms/gtk/platform.cpp
@@ -275,8 +275,7 @@ static void on_toggled(GtkWidget *button, const int *flags)
     gtk_dialog_set_response_sensitive(dialog, RESPONSE_YES, not_active);
 }
 
-static bool yes_no(trstring::arg_type title, const char *msg, unsigned int again_flags,
-                             osm2go_platform::Widget *parent)
+bool osm2go_platform::yes_no(trstring::arg_type title, trstring::arg_type msg, unsigned int again_flags, osm2go_platform::Widget *parent)
 {
   /* flags used to prevent re-appearence of dialogs */
   static struct {
@@ -288,7 +287,8 @@ static bool yes_no(trstring::arg_type title, const char *msg, unsigned int again
   if(dialog_again.not_again & again_bit)
     return ((dialog_again.reply & again_bit) != 0);
 
-  printf("%s: \"%s\"\n", static_cast<const gchar *>(static_cast<trstring::native_type>(title)), msg);
+  printf("%s: \"%s\"\n", static_cast<const gchar *>(static_cast<trstring::native_type>(title)),
+                         static_cast<const gchar *>(static_cast<trstring::native_type>(msg)));
 
   GtkWindow *p = GTK_WINDOW(parent ? parent : appdata_t::window);
 
@@ -296,11 +296,11 @@ static bool yes_no(trstring::arg_type title, const char *msg, unsigned int again
 #ifndef FREMANTLE
                   gtk_message_dialog_new(p, GTK_DIALOG_DESTROY_WITH_PARENT,
                                          GTK_MESSAGE_QUESTION, GTK_BUTTONS_YES_NO,
-                                         "%s", msg));
+                                         "%s", static_cast<const gchar *>(static_cast<trstring::native_type>(msg))));
 
   gtk_window_set_title(dialog, title);
 #else
-                  hildon_note_new_confirmation(p, msg));
+                  hildon_note_new_confirmation(p, static_cast<const gchar *>(static_cast<trstring::native_type>(msg))));
 #endif
 
   GtkWidget *cbut = nullptr;
@@ -334,24 +334,6 @@ static bool yes_no(trstring::arg_type title, const char *msg, unsigned int again
   }
 
   return yes;
-}
-
-bool osm2go_platform::yes_no(trstring::native_type_arg title, trstring::native_type_arg msg, unsigned int again_flags,
-                             osm2go_platform::Widget *parent)
-{
-  return yes_no(trstring::any_type(title), static_cast<const char *>(msg), again_flags, parent);
-}
-
-bool osm2go_platform::yes_no(trstring::native_type_arg title, const trstring &msg, unsigned int again_flags,
-                             osm2go_platform::Widget *parent)
-{
-  return yes_no(trstring::any_type(title), static_cast<const gchar *>(msg), again_flags, parent);
-}
-
-bool osm2go_platform::yes_no(const trstring &title, const trstring &msg, unsigned int again_flags,
-                             osm2go_platform::Widget *parent)
-{
-  return yes_no(title, static_cast<const gchar *>(msg), again_flags, parent);
 }
 
 const std::vector<dirguard> &osm2go_platform::base_paths()

src/platforms/gtk/relation_edit.cpp
@@ -736,10 +736,10 @@ on_relation_remove(relation_context_t *context)
   g_debug("remove relation #" ITEM_ID_FORMAT, sel->id);
 
   if(!sel->members.empty()) {
-    g_string msg(g_strdup_printf(ngettext("This relation still has %zu member. Delete it anyway?",
-                                          "This relation still has %zu members. Delete it anyway?",
-                                          sel->members.size()), sel->members.size()));
-    if(!osm2go_platform::yes_no(_("Delete non-empty relation?"), msg.get(), 0, context->dialog.get()))
+    trstring msg(ngettext("This relation still has %zu member. Delete it anyway?",
+                          "This relation still has %zu members. Delete it anyway?",
+                          sel->members.size()), nullptr, sel->members.size());
+    if(!osm2go_platform::yes_no(_("Delete non-empty relation?"), msg, 0, context->dialog.get()))
       return;
   }
 

commit 20b16c5233462f82c1574c86c580bd537efbf013

add missing include
Fixes: 3e85e9a3b6ef1f5deb80a41618ebbf57b6a9958a

src/platforms/osm2go_platform_common.h
@@ -48,6 +48,8 @@ enum {
 #include <string>
 #include <vector>
 
+#include <osm2go_i18n.h>
+
 namespace osm2go_platform {
   /**
    * @brief process all pending GUI events