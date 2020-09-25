osm2go/osm2go

commit 00a3f9e82155fd8b60651fb3f1f05056b4e71ca2

require non-empty name passed to icon_t::load() and icon_t::widget_load()

src/platforms/gtk/josm_presets_button.cpp
@@ -1066,11 +1066,13 @@ presets_element_t::attach_key *presets_element_link::attach(preset_attach_contex
 {
   const trstring label = trstring("[Preset] %1").arg(item->name);
   GtkWidget *button = osm2go_platform::button_new_with_label(label);
-  GtkWidget *img = icon_t::instance().widget_load(item->icon, 16);
-  if(img != nullptr) {
-    gtk_button_set_image(GTK_BUTTON(button), img);
-    // make sure the image is always shown, Hildon seems to hide it by default
-    gtk_widget_show(img);
+  if (!item->icon.empty()) {
+    GtkWidget *img = icon_t::instance().widget_load(item->icon, 16);
+    if(img != nullptr) {
+      gtk_button_set_image(GTK_BUTTON(button), img);
+      // make sure the image is always shown, Hildon seems to hide it by default
+      gtk_widget_show(img);
+    }
   }
   g_signal_connect_swapped(button, "clicked", G_CALLBACK(item_link_clicked), item);
   attach_both(attctx, button);

src/icon.h
@@ -39,7 +39,7 @@ public:
 
   /**
    * @brief load an icon from disk, limited to given dimensions
-   * @param sname the name of the icon
+   * @param sname the name of the icon, must not be empty
    * @param limit the maximum dimensions of the image
    * @return the scaled pixbuf
    *

src/platforms/gtk/icon.cpp
@@ -114,8 +114,7 @@ icon_file_exists(const std::string &file)
 
 icon_item *icon_t::load(const std::string &sname, int limit)
 {
-  if(sname.empty())
-    return nullptr;
+  assert(!sname.empty());
 
   icon_buffer::BufferMap &entries = static_cast<icon_buffer *>(this)->entries;
 

src/platforms/gtk/iconbar.cpp
@@ -227,7 +227,7 @@ bool iconbar_t::isTrashEnabled() const
 namespace {
 
 #ifndef FINGER_UI
-GtkWidget *
+GtkWidget * __attribute__((nonnull(1,3,4)))
 icon_add(GtkWidget *vbox, appdata_t &appdata, const char *icon_str, void(*func)(map_t *))
 {
   GtkWidget *but = gtk_button_new();
@@ -240,7 +240,7 @@ icon_add(GtkWidget *vbox, appdata_t &appdata, const char *icon_str, void(*func)(
 }
 #endif
 
-GtkWidget *
+GtkWidget * __attribute__((nonnull(2,4)))
 tool_button_label(icon_t &icons, GtkToolbar *toolbar, trstring::native_type_arg label_str, const char *icon_str)
 {
   PangoAttrList *attrs = pango_attr_list_new();
@@ -262,7 +262,7 @@ tool_button_label(icon_t &icons, GtkToolbar *toolbar, trstring::native_type_arg 
   return GTK_WIDGET(item);
 }
 
-GtkWidget *  __attribute__((nonnull(1,3,5)))
+GtkWidget * __attribute__((nonnull(1,3,5)))
 tool_add(GtkToolbar *toolbar, icon_t &icons, const char *icon_str, trstring::native_type_arg tooltip_str,
          GCallback func, gpointer context, bool separator = false)
 {

commit 529348f70ee59eb06d9f728121d43ec1b640aee2

fix violated assertion when picker menu entries have no icon
Fixes: 00a3f9e82155fd8b60651fb3f1f05056b4e71ca2

src/platforms/gtk/josm_presets_button.cpp
@@ -636,7 +636,7 @@ GtkTreeIter
 preset_insert_item(const presets_item_named *item, icon_t &icons, GtkListStore *store)
 {
   /* icon load can cope with empty string as name (returns nullptr then) */
-  icon_item *icon = icons.load(item->icon, 16);
+  icon_item *icon = item->icon.empty() ? nullptr : icons.load(item->icon, 16);
 
   /* Append a row and fill in some data */
   GtkTreeIter iter;