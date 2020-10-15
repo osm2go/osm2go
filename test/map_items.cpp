13192abeb10ad05c3e5aa133b0d59710a4b2e7ee
Rolf Eike Beer <eike@sf-mail.de>
2020-10-15 22:40:00 +0200

    tests: check some more things that are not drawn

--- test/map_items.cpp
+++ test/map_items.cpp
@@ -15,18 +15,27 @@
 #include <memory>
 #include <unistd.h>
 
-static void set_bounds(osm_t::ref o) {
+void test_map::test_function()
+{
+  way_add_begin();
+  way_add_cancel();
+}
+
+namespace {
+
+void set_bounds(osm_t::ref o)
+{
   bool b = o->bounds.init(pos_area(pos_t(52.2692786, 9.5750497), pos_t(52.2695463, 9.5755)));
   assert(b);
 }
 
-static void test_map_delete()
+void test_map_delete()
 {
   appdata_t a;
   std::unique_ptr<map_t> m(std::make_unique<test_map>(a));
 }
 
-static void test_map_delete_items()
+void test_map_delete_items()
 {
   appdata_t a;
   std::unique_ptr<map_t> m(std::make_unique<test_map>(a));
@@ -49,7 +58,7 @@ static void test_map_delete_items()
   o->node_delete(n);
 }
 
-static void test_draw_deleted(const std::string &tmpdir)
+void test_draw_deleted(const std::string &tmpdir)
 {
   appdata_t a;
   a.project.reset(new project_t("foo", tmpdir));
@@ -81,27 +90,56 @@ static void test_draw_deleted(const std::string &tmpdir)
 
   // deleted ways are not drawn
   m->draw(w);
+}
 
-  ba.id = 1235;
-  w = new way_t(ba);
+void test_draw_hidden(const std::string &tmpdir)
+{
+  appdata_t a;
+  a.project.reset(new project_t("foo", tmpdir));
+  std::unique_ptr<map_t> m(std::make_unique<test_map>(a));
+  m->style.reset(new style_t());
+  a.project->osm.reset(new osm_t());
+  osm_t::ref o = a.project->osm;
+  set_bounds(o);
+  MainUiDummy * const ui = static_cast<MainUiDummy *>(a.uicontrol.get());
+
+  base_attributes ba(123);
+  ba.version = 1;
+  way_t *w = new way_t(ba);
   o->insert(w);
   assert(!w->isDeleted());
   assert_cmpnum(w->flags, 0);
-  assert(!o->wayIsHidden(w));
+
+  for (int i = 0; i < 4; i++) {
+    lpos_t p(10, 10 + i);
+    node_t *n = o->node_new(p);
+    o->attach(n);
+    assert(!n->isDeleted());
+    assert_cmpnum(n->flags, OSM_FLAG_DIRTY);
+    w->append_node(n);
+  }
+
   o->waySetHidden(w);
   assert(o->wayIsHidden(w));
 
   // hidden ways are not drawn
   m->draw(w);
+
+  // trick the way to become unhidden bit still not drawn: also set deleted marker
+  w->flags |= OSM_FLAG_DELETED;
+
+  ui->m_actions[MainUi::MENU_ITEM_MAP_SHOW_ALL] = false;
+  m->show_all();
+
+  assert_cmpnum(o->hiddenWays.size(), 0);
+  w->flags = 0;
+
+  // delete a node from a hidden way: this should trigger a redraw, but again it's not actually drawn
+  o->waySetHidden(w);
+  o->node_delete(w->node_chain.front(), m.get());
 }
 
-void test_map::test_function()
-{
-  way_add_begin();
-  way_add_cancel();
-}
-
-static void test_way_add_cancel(const std::string &tmpdir)
+void test_way_add_cancel(const std::string &tmpdir)
 {
   appdata_t a;
   std::unique_ptr<test_map> m(std::make_unique<test_map>(a));
@@ -113,7 +151,7 @@ static void test_way_add_cancel(const std::string &tmpdir)
   m->test_function();
 }
 
-static void test_map_item_deleter(const std::string &tmpdir)
+void test_map_item_deleter(const std::string &tmpdir)
 {
   appdata_t a;
   a.project.reset(new project_t("foo", tmpdir));
@@ -135,6 +173,8 @@ static void test_map_item_deleter(const std::string &tmpdir)
   mid.run(nullptr);
 }
 
+} // namespace
+
 int main()
 {
   char tmpdir[] = "/tmp/osm2go-project-XXXXXX";
@@ -150,6 +190,7 @@ int main()
   test_map_delete();
   test_map_delete_items();
   test_draw_deleted(osm_path);
+  test_draw_hidden(osm_path);
   test_way_add_cancel(osm_path);
   test_map_item_deleter(osm_path);
 
