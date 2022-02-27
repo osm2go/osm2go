e94e95f6dee56207075355452137ad8a82e1d3d1
Rolf Eike Beer <eike@sf-mail.de>
2022-02-27 10:58:39 +0100

    update location of discardable tag list

--- data/discarded.py
+++ data/discarded.py
@@ -3,7 +3,7 @@
 import json
 import urllib.request
 
-with urllib.request.urlopen("https://github.com/openstreetmap/iD/raw/develop/data/discarded.json") as url:
+with urllib.request.urlopen("https://cdn.jsdelivr.net/npm/@openstreetmap/id-tagging-schema@3/dist/discarded.min.json") as url:
 	data = json.loads(url.read().decode())
 
 	print("#pragma once\n")
